3eed3de4603c72fe41ecd62556d8270bfc0c060b
briadam <briadam@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2011-07-01 22:52:22 +0000

    Attention: Those doing MPI builds will need to change/set CMake cache defines!
    Several CMake changes:
    
    * Collect specification maintenance (NIDR) code into a separate helper file
    
    * Move all Boost library probes to top level, reduce min version to 1.37, and consolidate library logic for Acro.  (Affects Pecos too.)
    
    * Extend MPI option processing and collect into separate helper file.  Specifically run the middle ground between the CMake macro which is too limited and the Trilinos one, which we may move to later (more complex).  Main motivation is flexiblity and better interoperation with Trilinos.
    
    New guidance on MPI (updated on Wiki as well):  Recommended practice is to set DAKOTA_HAVE_MPI and set CMAKE_CXX_COMPILER to a compiler wrapper.  However, we try to do introspection as well; alternates:
     * set MPI_CXX_COMPILER in cache to a specific MPI C++ wrapper (Trilinos)
     * set PATH in environment to include MPI C++ wrapper
     * set MPI_LIBRARY and MPI_INCLUDE_PATH in cache
     * set MPI_BASE_DIR in cache (Trilinos)
     * set MPIHOME in environment
    
    Will likely require adjustment after more testing with TriKota.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@537 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ config/DakotaEnableSpecMaint.cmake
@@ -0,0 +1,129 @@
+# Helper function to process DAKOTA specification maintenance options:
+# * whether to build NIDR-generated files into the source tree
+# * whether to build options JAGUAR-related files like nidrgen
+function(DakotaEnableSpecMaint)
+
+  if (ENABLE_SPEC_MAINT)
+  
+    # generate dakota.input.summary using nidrgen
+    add_custom_command(
+      OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.summary"
+      DEPENDS nidrgen 
+              ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec
+      COMMAND ${NIDR_BINARY_DIR}/nidrgen
+      ARGS    -efp ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec > ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.summary
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+  
+    # generate dakota.input.desc (depends on dakota.tags.desc)
+    find_package(Perl REQUIRED)
+    file(GLOB ref_dox_files ${CMAKE_CURRENT_SOURCE_DIR}/docs Ref_*.dox)
+    add_custom_command(
+      OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.desc"
+      DEPENDS nidrgen 
+              ${CMAKE_CURRENT_SOURCE_DIR}/generate_desc.pl
+  	    ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec
+  	    ${ref_dox_files}
+      COMMAND ${NIDR_BINARY_DIR}/nidrgen
+      ARGS    -T ${CMAKE_CURRENT_SOURCE_DIR}/dakota.tags.desc ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec
+      COMMAND ${PERL_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/generate_desc.pl
+      COMMAND ${CMAKE_COMMAND}
+      ARGS    -E remove ${CMAKE_CURRENT_SOURCE_DIR}/dakota.tags.desc
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+  
+    # generate NIDR_guikeywds.h: could move to jaguar-files target once
+    # not checked in
+    add_custom_command(
+      OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/NIDR_guikeywds.h"
+      DEPENDS nidrgen 
+              ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec
+  	    ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.desc
+      COMMAND ${NIDR_BINARY_DIR}/nidrgen
+      ARGS    -egG ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.desc > ${CMAKE_CURRENT_SOURCE_DIR}/NIDR_guikeywds.h
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+  
+    # generate NIDR_keywds.H
+    add_custom_command(
+      OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/NIDR_keywds.H"
+      DEPENDS nidrgen 
+              ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec
+  	    ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.desc
+  	    ${CMAKE_CURRENT_SOURCE_DIR}/NIDR_initdefs.h
+      COMMAND ${NIDR_BINARY_DIR}/nidrgen
+      ARGS    dakota.input.nspec dakota.input.desc NIDR_initdefs.h NIDR_keywds.H
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+  
+    # create a special target and add to "all" target
+    add_custom_target(dakota-spec-files 
+      ALL
+      DEPENDS nidrgen
+              ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.summary
+  	    ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.desc
+  	    ${CMAKE_CURRENT_SOURCE_DIR}/NIDR_guikeywds.h
+  	    ${CMAKE_CURRENT_SOURCE_DIR}/NIDR_keywds.H
+      VERBATIM
+      )
+  
+    # Target jaguar-files builds targets needed for jaguar that are not
+    # built as part of the all target
+    # TODO: move guikeywds to this target as not needed in repo
+    add_custom_target(jaguar-files DEPENDS dakreorder dakreord VERBATIM)
+  
+    # generate NIDR_keywds0.h (only needed by dakreorder.c) in two steps
+    # (1) generate NIDR_keywds0.h.tmp
+    # nidrgen generates the initial version of the file
+    # Dt -8 generateds YYYMMDD for the newest file
+    set(dakreorder_date_files 
+      ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec 
+      ${CMAKE_CURRENT_SOURCE_DIR}/dakreorder.c 
+      ${NIDR_SOURCE_DIR}/nidr.c 
+      ${NIDR_SOURCE_DIR}/nidr-parser.c 
+      ${NIDR_SOURCE_DIR}/nidr-scanner.c
+      ${NIDR_SOURCE_DIR}/avltree.c 
+      ${CMAKE_CURRENT_BINARY_DIR}/NIDR_keywds0.h.tmp 
+      )
+    ADD_CUSTOM_COMMAND(
+      OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/NIDR_keywds0.h.tmp"
+             date.txt
+      DEPENDS nidrgen Dt ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec
+      COMMAND ${NIDR_BINARY_DIR}/nidrgen
+      ARGS -ftn- ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec > NIDR_keywds0.h.tmp
+      COMMAND ${NIDR_BINARY_DIR}/Dt
+      ARGS -8 ${CMAKE_CURRENT_SOURCE_DIR}/dakota.input.nspec ${dakreorder_date_files} > date.txt
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      )
+    # (2) generate NIDR_keywds0.h
+    ADD_CUSTOM_COMMAND(
+      OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/NIDR_keywds0.h"
+      DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/NIDR_keywds0.h.tmp" date.txt
+      COMMAND ${CMAKE_COMMAND}
+      ARGS
+        -Dinfile:FILEPATH=${CMAKE_CURRENT_BINARY_DIR}/NIDR_keywds0.h.tmp
+        -Ddatefile:FILEPATH=${CMAKE_CURRENT_BINARY_DIR}/date.txt
+        -Doutfile:FILEPATH=${CMAKE_CURRENT_BINARY_DIR}/NIDR_keywds0.h
+        -P ${Dakota_SOURCE_DIR}/config/finish_keywds0.cmake
+      VERBATIM)
+  
+    # TODO: make -ldl optional if possible
+    set(dakreorder_libs nidr)
+    find_library(libdl dl) 
+    if(libdl)
+      list(APPEND dakreorder_libs ${libdl})
+    endif()
+  
+    add_executable(dakreorder EXCLUDE_FROM_ALL dakreorder.c NIDR_keywds0.h)
+    target_link_libraries(dakreorder ${dakreorder_libs})
+  
+    add_executable(dakreord EXCLUDE_FROM_ALL dakreorder.c)
+    set_target_properties(dakreord PROPERTIES
+      COMPILE_DEFINITIONS NO_NIDR_keywds0)
+    target_link_libraries(dakreord ${dakreorder_libs})
+  
+    # TODO: Ensure DAKOTA sources depend on NIDR_keywds.H
+  
+  endif()
+
+endfunction()
