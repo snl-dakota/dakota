70da420a5e1c30cec0261b18219b1948b35ad0b3
wjbohnh <wjbohnh@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2011-07-20 16:57:02 +0000

    Added a trivial, cache initialization BuildSetup.cmake file to demonstrate "typical" DAKOTA build settings on RHEL6 (GCC 4.4.4).
    Associate with Trac ticket #4053.
    
    Request review by Brian Adams.  (E.g. any added value in committing cache init files like
    this to SVN?)
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@555 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ examples/platforms/BuildSetupRHEL6.cmake
@@ -0,0 +1,20 @@
+# Assume a "comparably equipped" RHEL6 COE build host
+#   We compile with the stock gcc-4.4.x 
+#   Install linear algebra RPMS:  blas blas-devel lapack lapack-devel 
+#   (Optionally) install MPI RPMS:  openmpi openmpi-devel 
+#   (Optionally) install graphics RPMS:  lesstif lesstif-devel libX11 libXau libXext libXmu libXmu-devel libXp libXp-devel libXpm libXpm-devel 
+
+##############################################################################
+#
+# EXAMPLE BuildSetup.cmake file for DAKOTA (GNU-4.4.4/RHEL6)
+#
+##############################################################################
+# Enforce shared library build for DAKOTA and all of its TPLs
+##############################################################################
+
+set(BUILD_STATIC_LIBS OFF CACHE BOOL "Set to ON to build static libraries" FORCE)
+set(BUILD_SHARED_LIBS ON CACHE BOOL "Set to ON to build DSO libraries" FORCE)
+
+# Disable optional X graphics
+set(HAVE_X_GRAPHICS OFF CACHE BOOL "Disable dependency on X libraries" FORCE)
+
