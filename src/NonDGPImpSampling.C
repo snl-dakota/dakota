e36ee44d7c73f2590d0e9c85d18752a25254a73d
lpswile <lpswile@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2012-01-14 16:35:08 +0000

    Additions to the GP Importance Sampling code. Mike:  many thanks to re-factoring the vary pattern in the LHS constructors.   There still is more to be added to this code, but this commit adds much of the structure.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@903 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- src/NonDGPImpSampling.C
+++ src/NonDGPImpSampling.C
@@ -37,7 +37,7 @@ namespace Dakota {
 NonDGPImpSampling::NonDGPImpSampling(Model& model): NonDSampling(model)
 { 
   samplingVarsMode = ACTIVE_UNIFORM;
-  String sample_reuse, approx_type("global_gaussian");/*("global_kriging");*/
+  String sample_reuse, approx_type("global_kriging");/*("global_kriging");*/
   UShortArray approx_order; // not used by GP/kriging
   short corr_order = -1, data_order = 1, corr_type = NO_CORRECTION;
   if (probDescDB.get_bool("method.derivative_usage")) {
@@ -51,6 +51,8 @@ NonDGPImpSampling::NonDGPImpSampling(Model& model): NonDSampling(model)
   gpModel.assign_rep(new DataFitSurrModel(gpBuild, iteratedModel, approx_type,
     approx_order, corr_type, corr_order, data_order, sample_reuse), false);
   vary_pattern = true; // allow seed to run among multiple approx sample sets
+  // need to add to input spec
+  numEmulEval = 100;
   construct_lhs(gpEval, gpModel, sample_type, numEmulEval, randomSeed,
 		rngName, vary_pattern);
 
@@ -81,74 +83,139 @@ NonDGPImpSampling::~NonDGPImpSampling()
 void NonDGPImpSampling::quantify_uncertainty()
 {
 
-  numPtsAdd = 400; // need to include this in the input spec
+  numPtsAdd = 200; // need to include this in the input spec
   numPtsTotal = numSamples + numPtsAdd;
-  numEmulEval = 10; // perhaps add to input spect?
+  numEmulEval = 100; // perhaps add to input spect?
  
-  // Build initial GP model using truth model evals.  This will be
-  // over the initial LHS sample set defined in the constructor.
-  gpModel.build_approximation(); // runs gpBuild on iteratedModel
-
-  // Evaluate approximate samples on the GP
-  gpEval.run_iterator(Cout);
-
-  //KEITH:  Currently I have gpCvars, gpVar, and gpMeans as variables 
-  //holding the GP surrogate results.  We should discuss if these 
-  //should be class variables or local variables.
- 
-  const RealMatrix&  all_samples = gpEval.all_samples();
-  RealVectorArray  gpCvars(numEmulEval);
+  // Build initial GP model.  This will be built over the initial LHS sample set
+  // defined in the constructor.
+  gpModel.build_approximation();
+  
+  gpCvars.resize(numEmulEval);
   RealVector temp_cvars;
-  RealVectorArray gpVar(numEmulEval);
-  RealVectorArray gpMeans(numEmulEval);
-  const IntResponseMap& all_resp = gpEval.all_responses();
+  gpVar.resize(numEmulEval);
+  gpMeans.resize(numEmulEval);
+  indicator.resize(numPtsTotal);
+  expIndicator.resize(numEmulEval);
+  normConst.resize(numPtsAdd);
+  //RealVector rhoEmul0(numEmulEval);
+  //RealVector rhoEmul2(numEmulEval);
   int num_problem_vars=iteratedModel.acv();
-  int i,j;
+
+  int i,j,k;
  
 // KEITH:  You will probably not need to use this.  This piece of 
 // code prints out the build points, not the approximation points.
   const Pecos::SurrogateData& gp_data = gpModel.approximation_data(0);
-  for (j = 0; j < numSamples; j++) {
-    Cout << " Surrogate Vars " << gp_data.continuous_variables(j) << '\n';
-    Cout << " Surrogate Response " << gp_data.response_function(j); 
-  }
+//  for (j = 0; j < numSamples; j++) {
+//    Cout << " Surrogate Vars " << gp_data.continuous_variables(j) << '\n';
+//    Cout << " Surrogate Response " << gp_data.response_function(j); 
+//  }
 
-// The following two loops obtain the variables, the variances, and the 
-// means of the GP prediction.  Probably the best way to handle this is 
-// to calculate these and hand them to an expected indicator function. 
-//
-  for (i = 0; i< numEmulEval; i++) {
-    temp_cvars = RealVector(Teuchos::View, (Real*)all_samples[i], num_problem_vars);
-    gpCvars[i]=temp_cvars;
-    Cout << "input is " << gpCvars[i] << '\n'; 
-    gpVar[i] = gpModel.approximation_variances(temp_cvars);
-    Cout << "variance is " << gpVar[i];
-  }
-
-  IntRespMCIter resp_it = all_resp.begin();
-  for (j=0, resp_it=all_resp.begin(); j<numEmulEval; ++j, ++resp_it) {
-    RealVector temp_resp(numFunctions);
-    for (i=0; i<numFunctions; i++) 
-       temp_resp(i) = resp_it->second.function_value(i);
-     gpMeans[j]=temp_resp;
-     Cout << "output is " << gpMeans[j] << '\n';
-  }
-
-// We have built the initial GP.  Now we need to go through, per response function// and response level and calculate the failure probability. 
+// We have built the initial GP.  Now we need to go through, per response function
+// and response level and calculate the failure probability. 
 // We will need to add error handling:  we will only be calculating 
 // results per response level, not probability level or reliability index.
-   int respFnCount, levelCount;
+   
+   int respFnCount, levelCount, iter;
 
    for (respFnCount=0; respFnCount<numFunctions; respFnCount++) {
      size_t num_levels = requestedRespLevels[respFnCount].length();
      for (levelCount=0; levelCount<num_levels; levelCount++) {
-       // Here we will have a loop over the number of points added, 
-       // where each time you calculate the expected indicator function 
+       Cout << "Starting calculations for response function " << respFnCount+1 << '\n';
+       Cout << "Starting calculations for level  " << levelCount+1 << '\n';
+       Cout << "Threshold level is " << requestedRespLevels[respFnCount][levelCount] << '\n';
+       Real z = requestedRespLevels[respFnCount][levelCount];
+       // Calculate indicator over the true function evaluations
+       for (j = 0; j < numSamples; j++) {
+         if (gp_data.response_function(j)<z) 
+	   indicator(j)=1;
+         else indicator(j)=0;
+         Cout << "indicator j " << indicator(j) << '\n';
+       }
+       
+       // Here we loop over the number of points added, 
+       // where each time we calculate the expected indicator function 
        // and add to the approximation. 
-       // it should be something like: 
-       // for (k = 0; k < numPtsAdd; k++) { 
-       //   calculate expected indicator function;
-       //   calculate rhoDraw, normalization constants, etc. 
+       for (k = 0; k < numPtsAdd; k++) { 
+         // generate new set of emulator samples.  Note this will have a different seed 
+         // each time.
+         gpEval.run_iterator(Cout);
+         // obtain results 
+         const RealMatrix&  all_samples = gpEval.all_samples();
+         const IntResponseMap& all_resp  = gpEval.all_responses();
+         for (i = 0; i< numEmulEval; i++) {
+           temp_cvars = RealVector(Teuchos::View, (Real*)all_samples[i], num_problem_vars);
+           gpCvars[i]=temp_cvars;
+           Cout << "input is " << gpCvars[i] << '\n';
+           gpVar[i] = gpModel.approximation_variances(temp_cvars);
+           Cout << "variance is " << gpVar[i];
+         }
+
+         IntRespMCIter resp_it = all_resp.begin();
+         for (j=0, resp_it=all_resp.begin(); j<numEmulEval; ++j, ++resp_it) {
+           RealVector temp_resp(numFunctions);
+             for (i=0; i<numFunctions; i++)
+               temp_resp(i) = resp_it->second.function_value(i);
+               gpMeans[j]=temp_resp;
+              Cout << "output is " << gpMeans[j] << '\n';
+         }
+          
+       // calculate expected indicator function;
+         expIndicator = calcExpIndicator(respFnCount,z);
+       // calculate distribution pdfs required to calculate the draw distribution
+       // distribution_pdf(rhoEmul2);
+       // distribution_pdf(rhoEmul0);
+       // FOR NOW, we assume distribution 0 and 2 are the same, 
+       // but in general rhoDrawThis = expIndicator * rhoEmul0/rhoEmul2;
+         rhoDrawThis = expIndicator; 
+      	
+       // calculate the normalization constant
+         normConst(k)=0.0;
+         for (j = 0; j < numEmulEval; j++) 
+	   normConst(k)+=expIndicator(j);
+         normConst(k)/=numEmulEval;
+         Real temp_norm_const = normConst(k);
+       // calculate the draw distribution
+         rhoDrawThis.resize(0);
+         xDrawThis.resize(0);
+         expIndThis.resize(0);
+         calcRhoDraw();
+          
+         iter = 1;
+         while ((iter<3) && (normConst(k)*numEmulEval<16)) {
+	   iter = iter+1;
+           gpEval.run_iterator(Cout);
+           // obtain results 
+           const RealMatrix&  this_samples = gpEval.all_samples();
+           const IntResponseMap& this_resp  = gpEval.all_responses();
+           for (i = 0; i< numEmulEval; i++) {
+             temp_cvars = RealVector(Teuchos::View, (Real*)this_samples[i], num_problem_vars);
+             gpCvars[i]=temp_cvars;
+             Cout << "input is " << gpCvars[i] << '\n';
+             gpVar[i] = gpModel.approximation_variances(temp_cvars);
+             Cout << "variance is " << gpVar[i];
+           }
+
+           resp_it = this_resp.begin();
+           for (j=0, resp_it=this_resp.begin(); j<numEmulEval; ++j, ++resp_it) {
+             RealVector temp_resp(numFunctions);
+             for (i=0; i<numFunctions; i++)
+               temp_resp(i) = resp_it->second.function_value(i);
+               gpMeans[j]=temp_resp;
+              Cout << "output is " << gpMeans[j] << '\n';
+           }
+          
+       // calculate expected indicator function;
+           expIndicator = calcExpIndicator(respFnCount,z);
+           Real temp_norm_this=0.0;
+           for (j = 0; j < numEmulEval; j++) 
+	     temp_norm_this+=expIndicator(j);
+           temp_norm_this/=numEmulEval;
+           temp_norm_const = temp_norm_const+temp_norm_this;
+           calcRhoDraw();
+
+         }     
        //   calculate importance density and probability of hitting failure reg
        //   calculate mixture model estimate
        //   NOTE:  when you need to append to the gpModel, use: 
@@ -159,11 +226,59 @@ void NonDGPImpSampling::quantify_uncertainty()
        //   iteratedModel.compute_response();
        //   IntResponsePair resp_truth(iteratedModel.evaluation_id(),
        //                            iteratedModel.current_response());
-       //                                                        
-       //
+       }
      }
    }
 }
 
+
+void NonDGPImpSampling::calcRhoDraw()
+{ 
+  int i, templength;
+  templength = xDrawThis.size();
+
+  for (i = 0; i< numEmulEval; i++) {
+    if (expIndicator(i)!=0.0) {
+      xDrawThis.resize(templength+1);
+      expIndThis.resize(templength+1);
+      rhoDrawThis.resize(templength+1);
+      xDrawThis[templength]=gpCvars[i];
+      expIndThis(templength)=expIndicator(i);
+      rhoDrawThis(templength)=expIndicator(i);
+      templength=templength+1;
+    }
+  }    
+  
+  for (i = 0; i< templength; i++) {
+    Cout << "xDrawThis  " << i << xDrawThis[i] << '\n';
+    Cout << "rhoDrawThis  "  << rhoDrawThis[i] << '\n';
+  }
+}
+
+RealVector NonDGPImpSampling::calcExpIndicator(const int respFnCount, const Real respThresh)
+{
+  int i, j;
+  RealVector ei(numEmulEval);
+
+  Real cdf,snv,stdv;
+  for (i = 0; i< numEmulEval; i++) {
+    //Cout << "GPmean  " << gpMeans[i][respFnCount];
+    //Cout << "GPvar  " << gpVar[i][respFnCount];
+    snv = respThresh-gpMeans[i][respFnCount];
+    stdv = std::sqrt(gpVar[i][respFnCount]); 
+    if(std::fabs(snv)>=std::fabs(stdv)*50.0) {
+    //this will trap the denominator=0.0 case even if numerator=0.0
+      cdf=(snv>0.0)?1.0:0.0;
+    }
+    else{
+      snv/=stdv;
+      cdf = Pecos::Phi(snv);
+    }
+    ei(i)=cdf;
+    Cout << "EI " << ei(i) << " respThresh= " << respThresh << " mu= " << gpMeans[i][respFnCount] << " stdv= " << stdv << '\n';
+  }    
+  return ei;
+
+}
 
 } // namespace Dakota
