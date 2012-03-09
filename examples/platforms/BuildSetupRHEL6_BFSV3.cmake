c69b183270cb6d0a48a52d1c261815825fe0a612
wjbohnh <wjbohnh@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2012-03-09 18:06:39 +0000

    Removed restriction with respect to Boost.Filesystem version support. Previously, on Version 2 was supported -- NOW, Version 3 is supported IF Dakota is configured with Boost 1.46 or later.
    Build tests performed on RHEL6 with Dakota linked with a CMakeified version
    of Boost-1.48:
    http://gitorious.org/~denisarnaud/boost/denisarnauds-cmake/trees/cmake-1.48.0
    
    ToDo:
    - Perform runtime tests (via Jenkins/CTest)
    - Close Trac #4132
    - Remove BFSV3 warning prior to Rel5.3
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@955 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ examples/platforms/BuildSetupRHEL6_BFSV3.cmake
@@ -0,0 +1,50 @@
+# Assume a "comparably equipped" RHEL6 COE build host
+#   - We compile with the stock gcc-4.4.6 
+#   - Install linear algebra RPMS:  blas blas-devel lapack lapack-devel 
+#
+# Refer to wiki for more info:
+#   - https://software.sandia.gov/trac/dakota/wiki/CMakeFAQ
+#
+# Use a CMakeified version of Boost with Filesystem VERSION 3 default
+#   - http://gitorious.org/~denisarnaud/boost/denisarnauds-cmake/trees/cmake-1.48.0
+#
+##############################################################################
+#
+# EXAMPLE BuildSetup.cmake file for DAKOTA (GNU-4.4.6/RHEL6/Boost.FS_V3)
+#
+##############################################################################
+
+# CMake Boost probes in ACRO result in "multiple targets with same name" error
+set(HAVE_ACRO OFF CACHE BOOL
+    "Do NOT use ACRO for when DakBld with Boost.Filesytem V3" FORCE)
+
+set(Boost_NO_SYSTEM_PATHS ON CACHE BOOL
+    "Do NOT use system boost for when linking with Boost.Filesytem V3" FORCE)
+ 
+set(BOOST_INCLUDEDIR
+    "/Net/dakota/utils/Linux_x86_64/boost_1.48.gnu-4.4.6/include/boost-1.48.0"
+    CACHE PATH "CMakeified Boost-1.48" FORCE)
+
+set(BOOST_LIBRARYDIR
+    "/Net/dakota/utils/Linux_x86_64/boost_1.48.gnu-4.4.6/lib/boost-1.48.0"
+    CACHE PATH "CMakeified Boost-1.48" FORCE)
+
+set(Boost_USE_MULTITHREADED OFF CACHE BOOL
+    "Do NOT use -mt variant when linking with Boost.Filesytem V3" FORCE)
+
+set(Boost_USE_STATIC OFF CACHE BOOL
+    "Do NOT use -static variant when linking with Boost.Filesytem V3" FORCE)
+
+set(Boost_DEBUG OFF CACHE BOOL
+    "Do NOT use -d variant when linking with Boost.Filesytem V3" FORCE)
+
+##############################################################################
+# Enforce shared library build for DAKOTA and all of its TPLs
+##############################################################################
+
+set(BUILD_SHARED_LIBS ON CACHE BOOL "Set to ON to build DSO libraries" FORCE)
+set(BUILD_STATIC_LIBS OFF CACHE BOOL "Save time/space -- NO static libs" FORCE)
+
+# Disable optional X graphics
+set(HAVE_X_GRAPHICS OFF CACHE BOOL "Disable dependency on X libraries" FORCE)
+
