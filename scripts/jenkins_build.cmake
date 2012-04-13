d78c9581acd085190742d3dc46676ca7206dd0d1
wjbohnh <wjbohnh@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2012-04-13 19:55:34 +0000

    Removed some unnecessary comments and performed minor cleanup.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@966 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- scripts/jenkins_build.cmake
+++ scripts/jenkins_build.cmake
@@ -1,4 +1,4 @@
-# Setup the build with desirable build settings from a jenkins ExecuteShell step
+# Setup the cmake build with desirable settings from a jenkins ExecuteShell step
 
 set(CTEST_SITE "$ENV{NODE_NAME}")
 set(CTEST_DASHBOARD_ROOT "$ENV{WORKSPACE}")
@@ -6,10 +6,14 @@ set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/source")
 set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/build")
 
 set(CTEST_BUILD_NAME "$ENV{JOB_NAME}")
+set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
 
+# WJB - ToDo:  Generalize for nmake on Windows
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_BUILD_CONFIGURATION RelWithDebInfo)
 set(CTEST_BUILD_COMMAND "make -i -j8" )
+
+set(CTEST_BUILD_CONFIGURATION RelWithDebInfo)
+set(CTEST_DAK_DISTRO "${CTEST_SOURCE_DIRECTORY}/config/DakotaDistro.cmake")
 
 set(CTEST_CONFIGURE_COMMAND
   "${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}")
@@ -18,20 +22,17 @@ set(CTEST_CONFIGURE_COMMAND
 set(CTEST_CONFIGURE_COMMAND
   "${CTEST_CONFIGURE_COMMAND} \"-C${CTEST_DASHBOARD_ROOT}/BuildSetup.cmake\"")
 set(CTEST_CONFIGURE_COMMAND
-  "${CTEST_CONFIGURE_COMMAND} \"-C${CTEST_SOURCE_DIRECTORY}/config/DakotaDistro.cmake\"")
+  "${CTEST_CONFIGURE_COMMAND} \"-C${CTEST_DAK_DISTRO}\"")
 set(CTEST_CONFIGURE_COMMAND
   "${CTEST_CONFIGURE_COMMAND} \"${CTEST_SOURCE_DIRECTORY}\"")
 
 
-# Now execute the "configure/make/make install" steps
+# Now execute the "configure and make" steps
 
-ctest_start(Experimental)
+ctest_start(Continuous)
 
 ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}")
 ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}")
 
-# ToDo:  Verify "install" step is combined with build when using ctest
-
-# ToDo:  Confirm the submit step is the only thing giving BMA heartburn
-####ctest_submit()
+#ctest_submit()
 
