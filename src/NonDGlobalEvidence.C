b0e8962a4d9ad462cf65676c0392c8c438f99707
mseldre <mseldre@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2012-08-24 23:26:09 +0000

    Index update omission.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1107 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- src/NonDGlobalEvidence.C
+++ src/NonDGlobalEvidence.C
@@ -41,26 +41,27 @@ void NonDGlobalEvidence::set_cell_bounds()
 {
   // TO DO: av_cntr assumes all active variables are interval variables
   size_t j, v_cntr, av_cntr; 
-  for (j=0, v_cntr=0, av_cntr=0; j<numContIntervalVars; ++j, ++v_cntr) {
+  for (j=0, v_cntr=0, av_cntr=0; j<numContIntervalVars;
+       ++j, ++v_cntr, ++av_cntr) {
     intervalOptModel.continuous_lower_bound(cellLowerBounds[cellCntr][v_cntr],
 					    av_cntr);
     intervalOptModel.continuous_upper_bound(cellLowerBounds[cellCntr][v_cntr],
 					    av_cntr);
   }
    
-  for (j=0, av_cntr=0; j<numDiscIntervalVars; ++j, ++v_cntr) {
+  for (j=0, av_cntr=0; j<numDiscIntervalVars; ++j, ++v_cntr, ++av_cntr) {
     intervalOptModel.discrete_int_lower_bound(cellLowerBounds[cellCntr][v_cntr],
 					      av_cntr);
     intervalOptModel.discrete_int_upper_bound(cellLowerBounds[cellCntr][v_cntr],
 					      av_cntr);
   } 
 
-  for (j=0; j<numDiscSetIntUncVars; ++j, ++v_cntr) {
+  for (j=0; j<numDiscSetIntUncVars; ++j, ++v_cntr, ++av_cntr) {
     intervalOptModel.discrete_int_variable(cellLowerBounds[cellCntr][v_cntr],
 					   av_cntr);
   } 
 
-  for (j=0, av_cntr=0; j<numDiscSetRealUncVars; ++j, ++v_cntr) {
+  for (j=0, av_cntr=0; j<numDiscSetRealUncVars; ++j, ++v_cntr, ++av_cntr) {
     intervalOptModel.discrete_real_variable(cellLowerBounds[cellCntr][v_cntr],
 					    av_cntr);
   } 
