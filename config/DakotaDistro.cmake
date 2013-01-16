4b59ca5d07970f592f5019f6ad5abeb3b290ed26
briadam <briadam@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-01-16 06:38:24 +0000

    Attempt to restore the DakotaDistro macro, triggered by DevDistro for some developers.  Newer Trilinos rejects custom CMAKE_BUILD_TYPES (at least the way we're invoking it), so we can't define DISTRIBUTION.  Instead, try to force the use of -O2 for C, CXX, and Fortran flags, including in Teuchos.
    Note that we probably want to change continuous and deployment builds accordingly to use the right macros, else you end up with a mix of -O2 and -O3.
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1345 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- config/DakotaDistro.cmake
+++ config/DakotaDistro.cmake
@@ -1,8 +1,20 @@
 
 # CMake options for DAKOTA testing builds
 
+# This no longer works as Trilinos CMake rejects custom CMAKE_BUILD_TYPE
 # Use distribution build options (-O2)
-set(CMAKE_BUILD_TYPE "DISTRIBUTION" CACHE STRING "Type of build")
+#set(CMAKE_BUILD_TYPE "DISTRIBUTION" CACHE STRING "Type of build")
+
+# CMake build flags to match historical baselines and releases
+set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Dakota Dev Release")
+set(CMAKE_C_FLAGS_RELEASE "-O2" CACHE STRING "Dakota Dev Release Flags")
+set(CMAKE_CXX_FLAGS_RELEASE "-O2" CACHE STRING "Dakota Dev Release Flags")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O2" CACHE STRING "Dakota Dev Release Flags")
+
+# New Trilinos seems to want to override those above.  Try to force.
+set(CMAKE_C_FLAGS_RELEASE_OVERRIDE "-O2" CACHE STRING "Dakota Dev Release Flags")
+set(CMAKE_CXX_FLAGS_RELEASE_OVERRIDE "-O2" CACHE STRING "Dakota Dev Release Flags")
+set(CMAKE_Fortran_FLAGS_RELEASE_OVERRIDE "-O2" CACHE STRING "Dakota Dev Release Flags")
 
 # Use shared libraries and don't use static
 set(BUILD_STATIC_LIBS FALSE CACHE BOOL "Build static libs?")
