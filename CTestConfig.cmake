8fc4ff4900097550f4f20029f46372ce5de78763
wjbohnh <wjbohnh@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-01-20 20:28:22 +0000

    Rename top-level cmake module directory: config -> cmake
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1387 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,7 +9,8 @@ set(CTEST_PROJECT_NAME "Dakota")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "cdash.sandia.gov")
+#set(CTEST_DROP_SITE "cdash.sandia.gov")
+set(CTEST_DROP_SITE "dakota.sandia.gov")
 set(CTEST_DROP_LOCATION "/cdash/submit.php?project=Dakota")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
