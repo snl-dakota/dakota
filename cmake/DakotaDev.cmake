02f57d5d4a84703175a7cf920860746aae82a6c3
mseldre <mseldre@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-02-15 20:34:06 +0000

    Turn on PECOS_ENABLE_TESTS within developer builds as a first step to restoring Pecos unit tests as first-class citizens.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1544 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- cmake/DakotaDev.cmake
+++ cmake/DakotaDev.cmake
@@ -5,6 +5,7 @@ set(DAKOTA_HAVE_MPI TRUE CACHE BOOL "Enable MPI in DAKOTA?")
 set(ENABLE_DAKOTA_DOCS TRUE CACHE BOOL "Enable DAKOTA documentation build")
 set(ENABLE_SPEC_MAINT TRUE CACHE BOOL 
   "Enable DAKOTA specification maintenance mode?")
+set(PECOS_ENABLE_TESTS TRUE CACHE BOOL "Enable Pecos-specific tests?")
 
 # Not included from Mike's configs, but may help some
 
@@ -15,5 +16,3 @@ set(ENABLE_SPEC_MAINT TRUE CACHE BOOL
 # CMake 2.8.6 has problems with RHEL6/Boost -- the following is a workaround
 #-DBoost_NO_BOOST_CMAKE=ON
 #set(Boost_NO_BOOST_CMAKE ON CACHE BOOL "Obtain desired behavior on RHEL6" FORCE)
-
-
