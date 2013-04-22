c15b5e1d83f5133e158065c61c8c1783560d5e6d
wjbohnh <wjbohnh@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-04-22 23:21:47 +0000

    Add path to LAPACK.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1580 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- cmake/platforms/msvc_ifort_ia32.cmake
+++ cmake/platforms/msvc_ifort_ia32.cmake
@@ -4,6 +4,16 @@
 #
 ############################################################################
 
+# BMA: Not sure this is the right place to make these settings...
+set( DAKOTA_HAVE_MPI OFF
+     CACHE BOOL "MSVC is a serial build" FORCE )
+
+# WJB - ToDo:  move these settings to a proper location
+set( LAPACK_DIR "C:/Dashboards/Support/lapack-3.3.1"
+     CACHE PATH "LAPACK spec should be in host-specific file" FORCE )
+
+############################################################################
+
 # DO NOT use shared libraries on Windows (yet)
 set(BUILD_STATIC_LIBS ON  CACHE BOOL "Build static libs")
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "DO NOT Build shared libs!")
