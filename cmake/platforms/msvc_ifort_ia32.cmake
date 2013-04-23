9476b5ba7d70aeefab97607331aa72320ba7eaee
wjbohnh <wjbohnh@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-04-23 01:17:16 +0000

    Correct the Boost path for native, Windows builds on the jenkins build-host aday.
    Should work in a "common" msvc_ifort build fragment since the path to Boost
    happens to be the same whether using steinman OR aday.
    
    Would be better to move the Boost path to a host fragment file, i.e. aday.cmake
    BUT, that would cause the cygwin builds on aday to fail.
    
    ToDo:  After the 5.3.1 release -- generalize the ctest infrastructure so that
    is is painless to configure a windows host to support BOTH cywin and msvc builds.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1582 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- cmake/platforms/msvc_ifort_ia32.cmake
+++ cmake/platforms/msvc_ifort_ia32.cmake
@@ -9,6 +9,14 @@ set( DAKOTA_HAVE_MPI OFF
      CACHE BOOL "MSVC is a serial build" FORCE )
 
 # WJB - ToDo:  move these settings to a proper location
+
+set( Boost_INCLUDE_DIR "C:/Dashboards/Support/boost_1_42"
+     CACHE PATH "Boost spec should be in host-specific file" FORCE )
+
+set( Boost_LIBRARY_DIRS "C:/Dashboards/Support/boost_1_42/lib"
+     CACHE FILEPATH "Boost spec should be in host-specific file" FORCE )
+
+#set( LAPACK_DIR "C:/Dashboards/Support/lapack-3.4.2/build"
 set( LAPACK_DIR "C:/Dashboards/Support/lapack-3.3.1"
      CACHE PATH "LAPACK spec should be in host-specific file" FORCE )
 
