6e33589eb77ae45fde0c22d8aa3215435756b449
briadam <briadam@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-07-09 17:52:48 +0000

    Add CMake code to install Queso's libraries to support runtime linkage.  Also let Dakota be flexible and try to find Queso libraries in $prefix/lib or $prefix/lib64 for greater operating system suport (thanks for jmhite, NCSU for reporting).
    Note that this will not resolve issues with QUESO finding GRVY on a platform with separate 64-bit lib dirs, so just turn off GRVY as not needed any longer.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1655 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ cmake/InstallQuesoLibs.cmake
@@ -0,0 +1,30 @@
+# Find the QUESO (and possibly GRVY) libs and install to
+# ${CMAKE_INSTALL_PREFIX}/lib
+
+# NOTE: This script will only work for make install from top of build tree
+# TODO: Review string quoting conventions and test with spaces in filename
+
+message( "CMAKE_CURRENT_BINARY_DIR (1): ${CMAKE_CURRENT_BINARY_DIR}" ) 
+if ( DAKOTA_JENKINS_BUILD OR DEFINED ENV{WORKSPACE} )
+  # By convention, all Dakota, jenkins-driven build jobs use a 'build'
+  # subdir for clear separation of source and build trees in the WORKSPACE
+  set( CMAKE_CURRENT_BINARY_DIR $ENV{WORKSPACE}/build )
+elseif ( NOT CMAKE_CURRENT_BINARY_DIR )
+  set( CMAKE_CURRENT_BINARY_DIR $ENV{PWD} )
+endif()
+message( "CMAKE_CURRENT_BINARY_DIR (2): ${CMAKE_CURRENT_BINARY_DIR}" ) 
+
+# On some platforms, these libraries get installed to $prefix/lib64
+file(GLOB queso_libs "${CMAKE_CURRENT_BINARY_DIR}/packages/queso_ext/lib/*")
+file(GLOB grvy_libs "${CMAKE_CURRENT_BINARY_DIR}/packages/grvy_ext/lib/*")
+file(GLOB queso64_libs "${CMAKE_CURRENT_BINARY_DIR}/packages/queso_ext/lib64/*")
+file(GLOB grvy64_libs "${CMAKE_CURRENT_BINARY_DIR}/packages/grvy_ext/lib64/*")
+
+# Process each library and install
+foreach(dakota_lib ${queso_libs} ${grvy_libs})
+  file(INSTALL "${dakota_lib}" DESTINATION "${CMAKE_INSTALL_PREFIX}/lib")
+endforeach()
+
+foreach(dakota_lib ${queso64_libs} ${grvy64_libs})
+  file(INSTALL "${dakota_lib}" DESTINATION "${CMAKE_INSTALL_PREFIX}/lib64")
+endforeach()
