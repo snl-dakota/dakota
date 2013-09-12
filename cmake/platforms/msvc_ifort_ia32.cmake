0bc37a3e41fd7993148aedabb9e130d478c84c93
dmvigi <dmvigi@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-09-12 20:24:24 +0000

    installed Boost 1.49; changed CMake Boost_INCLUDE_DIR and Boost_LIBRARY_DIRS variables
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1757 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- cmake/platforms/msvc_ifort_ia32.cmake
+++ cmake/platforms/msvc_ifort_ia32.cmake
@@ -10,10 +10,10 @@ set( DAKOTA_HAVE_MPI OFF
 
 # WJB - ToDo:  move these settings to a proper location
 
-set( Boost_INCLUDE_DIR "C:/Dashboards/Support/boost_1_42"
+set( Boost_INCLUDE_DIR "C:/Dashboards/Support/boost_1_49_0"
      CACHE PATH "Boost spec should be in host-specific file" FORCE )
 
-set( Boost_LIBRARY_DIRS "C:/Dashboards/Support/boost_1_42/lib"
+set( Boost_LIBRARY_DIRS "C:/Dashboards/Support/boost_1_49_0/libs"
      CACHE FILEPATH "Boost spec should be in host-specific file" FORCE )
 
 #set( LAPACK_DIR "C:/Dashboards/Support/lapack-3.4.2/build"
