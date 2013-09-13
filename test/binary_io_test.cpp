57174e2f82284b1e04199296fb2bab5d0b131cef
wjbohnh <wjbohnh@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-09-13 18:40:39 +0000

    Check-pointing a version that dumps a couple of new data_types, even though it is "broken".  Need to refresh my memory about Teuchos SDM matrix initialization (and possibly debug what I am doing wrong).
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1761 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- test/binary_io_test.cpp
+++ test/binary_io_test.cpp
@@ -6,52 +6,123 @@
     For more information, see the README file in the top Dakota directory.
     _______________________________________________________________________ */
 
+#include "BinaryIO_Helper.hpp"
+
+#include <boost/assign/std/vector.hpp>
+
 #include <cassert>
 #include <iostream>
 #include <string>
 
-//#include "hdf5.h"
-//#include "netcdf.h"
-// This header not currently in the repo:
-#include "NetcdfFiles.hh"
+using namespace boost::assign;
+using namespace Dakota;
 
-// Note: may need newer Cmake for NetCDF probe, particularly CMakePushCheckState
-
-using radlib_seacasio::SimpleNetcdfFile;
+#define RANK 2
 
 int main()
 {
+  double rval_out = 3.14159, rval_in = 0.0;
+  int ival_out = 314159, ival_in = 0;
+  std::string pi_tag("/DakPi");
+  std::string file_name("binary_io_test.h5");
 
-  double val_out = 3.14159, val_in = 0.0;
-  std::string fileName("binary_io_test.h5");
+  std::vector<hsize_t> rdims;
+  rdims += 2, 2;
+  std::vector<hsize_t> idims;
+  idims += 3;
+
+  RealMatrix rmatrix_out(rdims[0], rdims[1]);
+  rmatrix_out(0, 0) = rval_out;
+  rmatrix_out(0, 1) = 0.23;
+  rmatrix_out(1, 0) = 0.23;
+  rmatrix_out(1, 1) = rval_out;
+
+  std::vector<int> ivec_out;
+  ivec_out += ival_out, 23, 333;
+  hsize_t isize = ivec_out.size();
+
+  assert(rdims.size() == RANK);
+  assert(rmatrix_out.numRows() == 2);
+  assert(rmatrix_out.numCols() == 2);
+  assert(rval_out == rmatrix_out(0, 0));
+  assert(rval_out == rmatrix_out(1, 1));
+  assert(ival_out == ivec_out[0]);
+  assert(isize == 3);
+
+  bool file_exist = false;
+  bool read_only = false;
+  herr_t status;
+
+  // WJB - ToDo: split out into functions
 
   // scope within which file write takes place
   {
     // open/create file
-    const bool fileExist = false;
-    const bool readOnly = false;
-    SimpleNetcdfFile netcdfFile(fileName, fileExist, readOnly);
+    SimpleBinaryStream binary_file(file_name, file_exist, read_only);
     
+    //binary_File.addVar(pi_tag, rval_out);
+
     // write data 
-    netcdfFile.addVar("adouble", val_out);
-    
-    // close file
-    // goes out of scope...
+    // single value
+#if 1
+    // WJB: hack to make a single value look like an array of len==1
+    std::vector<double> dummy_buf;
+    dummy_buf += rval_out;
+
+    std::vector<hsize_t> dummy_dims;
+    dummy_dims += 1;
+    // WJB: assertions should be replaced with "exceptions" in store_data
+    assert(dummy_dims.size() == 1);
+    assert(dummy_dims[0]     == 1);
+
+    status = binary_file.store_data<double,1>(pi_tag.c_str(), dummy_dims,
+               dummy_buf);
+    assert(status >= 0);
+#else
+    herr_t status = binary_file.store_data(pi_tag.c_str(), rval_out);
+#endif
+
+    // RealMatrix
+    status = binary_file.store_data("/RealMatrixData", rmatrix_out);
+    assert(status >= 0);
+
+    // std::vector<int>
+    status = binary_file.store_data<int, 1>("/IntVectorData", idims, ivec_out);
+    assert(status >= 0);
+
+    // binary stream goes out of scope... (file close)
   }
 
-  // scope within which file write takes place
+  // WJB - ToDo: split out into functions
+
+  // scope within which file read takes place
   {
     // open/read file
-    const bool fileExist = true;
-    const bool readOnly = true;
-    SimpleNetcdfFile netcdfFile(fileName, fileExist, readOnly);
+    file_exist = true;
+    read_only = true;
+    SimpleBinaryStream binary_file(file_name, file_exist, read_only);
   
     // read data 
-    netcdfFile.getVar("adouble", val_in);
+    // WJB:  Easy via NetCDF4! binary_File.getVar(pi_tag.c_str(), rval_in);
+
+    //RealMatrix rmatrix_in;
+
+    //herr_t status = binary_file.read_data<double, RANK>("/RealMatrixData",
+                      //rmatrix_in);
+    //assert(status >= 0);
+    //assert(rmatrix_in(0, 0) == rmatrix_out(0, 0));
+
+    std::vector<int> ivec_in;
+    status = binary_file.read_data<int, 1>("/IntVectorData", ivec_in);
+    assert(status >= 0);
+
+    assert( ivec_in.size()   == ivec_out.size() );
+    assert( ivec_in[isize-1] == ivec_out[isize-1] );
+    assert( ivec_in[0]       == ivec_out[0] );
   }
 
-  assert(val_in == val_out);
+  //assert(rval_in == rval_out);
 
-  return(0);
+  return status;
 }
 
