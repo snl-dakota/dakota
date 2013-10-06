90b205cadcdf58acb413df17f237bde82d2848cf
lpswile <lpswile@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-10-06 20:13:42 +0000

    Committing Mohamed's initial implementation of POF Darts. It is set up to allow for probability estimation of different response levels and also different response functions. We anticipate some further development: this is a working version that can handle some problems very well.
    Mohamed and I will include a test for nightly testing next week. 
     
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1828 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- src/NonDPOFDarts.hpp
+++ src/NonDPOFDarts.hpp
@@ -15,6 +15,7 @@
 #ifndef NOND_POFDARTS_H
 #define NOND_POFDARTS_H
 
+
 #include "dakota_data_types.hpp"
 #include "DakotaNonD.hpp"
 
@@ -57,14 +58,133 @@ protected:
   //
   //- Heading: Convenience functions
   //
+    
+    
+    void initiate_random_number_generator(unsigned long x);
+    
+    double generate_a_random_number();
+    
+    void init_pof_darts();
+    
+    void exit_pof_darts();
   
+    void execute(size_t kd);
+    
+    void print_POF_results(double lower, double upper);
+    /// print the final statistics
+    void print_results(std::ostream& s);
+    
+    //////////////////////////////////////////////////////////////
+    // MPS METHODS
+    //////////////////////////////////////////////////////////////
+    
+    void classical_dart_throwing_games(size_t game_index);
+    
+    void line_dart_throwing_games(size_t game_index);
+
+    bool valid_dart(double* x);
+    
+    bool valid_line_flat(size_t flat_dim, double* flat_dart);
+    
+    void add_point(double* x);
+    
+    ////////////////////////////////////////////////////////////////
+    // OPT / POF METHODS
+    ////////////////////////////////////////////////////////////////
+    void  shrink_all_spheres(); // shrink all disks by 90% to allow more sampling
+    
+    void adjust_radius_of_previous_samples_due_to_new_threshold();
+    
+    void assign_sphere_radius_POF(double* x, size_t isample);
+    
+    void assign_sphere_radius_OPT(double* x, size_t isample);
+    
+    void resolve_overlap_POF();
+    
+    double f_true(double* x);
+    
+    double f_grad_FD(double* x, double f, double &curv);
+    
+    double get_dart_radius(double f, double fgrad, double fcurv);
+    
+    ////////////////////////////////////////////////////////////////
+    // VOLUME OF SPHERE UNION METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    void retrieve_POF_bounds(double &lower, double &upper);
+    
+    double estimate_spheres_volume_0d(double** spheres, size_t num_spheres, size_t num_dim, double* xmin, double* xmax);
+    
+    double get_sphere_volume(double r, size_t num_dim);
+    
+    void sample_uniformly_from_unit_sphere(double* dart, size_t num_dim);
+    
+    double area_triangle(double x1, double y1, double x2, double y2, double x3, double y3);
+    
+    
+    ////////////////////////////////////////////////////////////////
+    // OUTPUT METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    void plot_vertices_2d();
+    
   //
   //- Heading: Data
   //
-  // number of samples of true function
-  int samples; 
-  // user-specified seed 
-  int seed;
+    
+    // number of samples of true function
+    int samples;
+    
+    // user-specified seed
+    int seed;
+    
+    
+    // variables for Random number generator
+    double Q[1220];
+    int indx;
+    double cc;
+    double c; /* current CSWB */
+    double zc;	/* current SWB `borrow` */
+    double zx;	/* SWB seed1 */
+    double zy;	/* SWB seed2 */
+    size_t qlen;/* length of Q array */
+    
+    
+    // Variables of the POF dart algorithm
+    
+    size_t _n_dim; // dimension of the problem
+    double* _xmin; // lower left corner of the domain
+    double* _xmax; // Upper right corner of the domain
+    
+    bool   _global_optimization;
+    double _failure_threshold;
+    double _global_minima;
+    
+    // Input
+    double _num_darts;
+    double _num_successive_misses_p;
+    double _num_successive_misses_m;
+    double _max_num_successive_misses;
+    
+    
+    double _max_radius; // maximum radius of a sphere
+    double _accepted_void_ratio; // termination criterion for a maximal sample
+    
+    // Output
+    size_t _num_inserted_points, _total_budget;
+    double** _sample_points; // store radius as a last coordinate
+    
+    // Darts
+    double* _dart; // a dart for inserting a new sample point
+    double* _grad_vec; // Function gradient at a given point
+    
+    size_t _flat_dim;
+    size_t* _line_flat;
+    size_t _num_flat_segments;
+    double* _line_flat_start;
+    double* _line_flat_end;
+    double* _line_flat_length;
+    
 
 };
 
