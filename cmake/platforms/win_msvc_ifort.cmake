e8f89cc72f5e686b3d789fe5dbc32aa7ef2ae720
briadam <briadam@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-11-01 01:30:19 +0000

    Migrate MSVS build to new VM with MSVS 2010 and Intel Composer XE 2011.  Initial results are promising, but planning to increase test coverage next: https://jenkins.sandia.gov/view/DAKOTA/job/dakota_core_win_msvc_ifort/4/
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/trunk@1989 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ cmake/platforms/win_msvc_ifort.cmake
@@ -0,0 +1,17 @@
+############################################################################
+#
+# CMake configuration file for Native Windows, 32-bit MSVC/ifort platform
+#
+############################################################################
+
+# BMA: Not sure this is the right place to make these settings...
+set( DAKOTA_HAVE_MPI OFF
+     CACHE BOOL "MSVC is a serial build" FORCE )
+
+
+############################################################################
+
+# DO NOT use shared libraries on Windows (yet)
+set(BUILD_STATIC_LIBS ON  CACHE BOOL "Build static libs")
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "DO NOT Build shared libs!")
+
