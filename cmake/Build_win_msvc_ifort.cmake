a19a6dc7788be2ee0d1575075537a324ba63a7c4
dmvigi <dmvigi@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2013-11-04 22:52:38 +0000

    Merged revisions 1989-1991,1993,1996 via svnmerge from https://software.sandia.gov/svn/public/dakota/core/trunk
    ........
      r1989 | briadam | 2013-10-31 19:30:19 -0600 (Thu, 31 Oct 2013) | 1 line
      
      Migrate MSVS build to new VM with MSVS 2010 and Intel Composer XE 2011.  Initial results are promising, but planning to increase test coverage next: https://jenkins.sandia.gov/view/DAKOTA/job/dakota_core_win_msvc_ifort/4/
    ........
      r1990 | briadam | 2013-11-01 09:47:40 -0600 (Fri, 01 Nov 2013) | 3 lines
      
      Remove testing artifacts before initiating testing to avoid stale results.
    ........
      r1991 | briadam | 2013-11-01 11:39:45 -0600 (Fri, 01 Nov 2013) | 1 line
      
      Update list of Windows tests to include based on new build host.  All tests included either fail gracefully or run to completion.
    ........
      r1993 | briadam | 2013-11-01 16:02:36 -0600 (Fri, 01 Nov 2013) | 1 line
      
      Use correct way to glob for files and remove them.
    ........
      r1996 | dmvigi | 2013-11-04 12:47:21 -0700 (Mon, 04 Nov 2013) | 1 line
      
      Updating author information and version information for Dakota 5.4
    ........
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/core/branches/5.4@1999 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ cmake/Build_win_msvc_ifort.cmake
@@ -0,0 +1,34 @@
+##############################################################################
+#
+# CTest Automated Build Variables for Native Windows MSVC/ifort, 32-bit
+#
+##############################################################################
+
+set( CTEST_BUILD_NAME "dakota_win_msvc_ifort" )
+
+set( DAKOTA_CMAKE_PLATFORM "win_msvc_ifort.cmake")
+set( DAKOTA_CMAKE_BUILD_TYPE "DakotaDistro.cmake")
+
+set( CTEST_DASHBOARD_ROOT 
+     $ENV{WORKSPACE} )
+set( CTEST_SOURCE_DIRECTORY
+     "${CTEST_DASHBOARD_ROOT}/source")
+
+set( CTEST_CMAKE_GENERATOR "Visual Studio 10" )
+
+set( DAKOTA_CTEST_PROJECT_TAG "Continuous" )
+set( CTEST_BUILD_CONFIGURATION Release )
+
+set( DAKOTA_CTEST_REGEXP "dakota_*" )
+set( DAKOTA_DEBUG ON )
+
+##############################################################################
+
+##########################################################################
+# Set up Internal CMake paths first. Then call automated build file.
+# DO NOT CHANGE!!
+##########################################################################
+include( ${CTEST_SCRIPT_DIRECTORY}/utilities/DakotaSetupBuildEnv.cmake )
+include( common_build )
+##########################################################################
+
