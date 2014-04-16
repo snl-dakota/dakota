e8156cc708889b8e178793dc8b50da2955a1c85e
briadam <briadam@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2014-04-16 07:45:24 +0000

    Make new reference manual generation process a first-class part of the documentation build process.  Should only be necessary to put doxygen 1.8.4 on PATH and make docs-newref in build/docs.  Includes significant rearrangement of directories and filenames to more closely follow existing conventions in hopes of making life easier.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@2343 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ docs/newref_concat_dox.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ $# -lt 3 ]; then 
+  echo "Usage: $0 dakota/src/docs input_summary output_dox"
+  exit 1;
+fi
+
+src_docs=${1}
+input_summary=${2}
+output_dox=${3}
+
+# Generate verbatim spec summary in intermediate file
+echo "\\verbatim" > DakotaInputSpec.dox
+cat ${input_summary} >> DakotaInputSpec.dox
+echo "\\endverbatim" >> DakotaInputSpec.dox
+
+## Concatenate the Doxygen files
+# Some are header files, plus the collected topics and keyword pages, 
+# and the input spec summary
+cat $src_docs/NewRef_Main.dox \
+  $src_docs/NewRef_Sample_Inputs.dox \
+  $src_docs/NewRef_Input_Spec.dox \
+  $src_docs/NewRef_Spec_Summary.dox DakotaInputSpec.dox \
+  DakotaTopics.dox \
+  $src_docs/NewRef_Keywords_Intro.dox DakotaKeywords.dox > ${output_dox}
+
+# Don't need this as the Java incorporates the topics header
+#  $src_docs/NewRef_TopicsIntro.dox 
+
+# mark the end of doxygen file
+echo '*/' >> ${output_dox}
