a4960d159cde08d6b91db2c7afbc0f9901a3ccd6
lpswile <lpswile@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2014-07-24 23:27:46 +0000

    Additions of a test file to test the string variable capability. At this point, the file only tests the strings with LHS and computes the correlations including the string variables. The parameter study works, but the correlation on the parameter study results does not work.  Per discussion with Brian, we identified a few paths to proceed and will discuss with Mike when he returns.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@2686 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ test/gerstner_string.cpp
@@ -0,0 +1,127 @@
+/*  _______________________________________________________________________
+
+    DAKOTA: Design Analysis Kit for Optimization and Terascale Applications
+    Copyright 2014 Sandia Corporation.
+    This software is distributed under the GNU Lesser General Public License.
+    For more information, see the README file in the top Dakota directory.
+    _______________________________________________________________________ */
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+
+int main(int argc, char** argv)
+{
+  using namespace std;
+
+  ifstream fin(argv[1]);
+  if (!fin) {
+    cerr << "\nError: failure opening " << argv[1] << endl;
+    exit(-1);
+  }
+  size_t i, num_vars, num_fns;
+  string vars_text, fns_text;
+
+  // Get the parameter vector and ignore the labels
+  fin >> num_vars >> vars_text;
+  vector<double> x(num_vars-1);
+  for (i=0; i<num_vars-1; i++) {
+    fin >> x[i];
+    fin.ignore(256, '\n');
+  }
+
+  string thisstring; 
+  fin >> thisstring;
+  fin.ignore(256, '\n');
+  cout << thisstring << "this string";
+
+  // Get the ASV vector and ignore the labels
+  fin >> num_fns >> fns_text;
+  vector<int> ASV(num_fns);
+  for (i=0; i<num_fns; i++) {
+    fin >> ASV[i];
+    fin.ignore(256, '\n');
+  }
+
+  if (num_vars != 3) {
+    cerr << "Wrong number of variables for the gerstner problem\n";
+    exit(-1);
+  }
+  if (num_fns != 1) {
+    cerr << "Wrong number of functions for the gerstner problem\n";
+    exit(-1);
+  }
+ 
+  ofstream fout(argv[2]);
+  if (!fout) {
+    cerr << "\nError: failure creating " << argv[2] << endl;
+    exit(-1);
+  }
+  fout.precision(15); // 16 total digits
+  fout.setf(ios::scientific);
+  fout.setf(ios::right);
+
+  short test_fn; double even_coeff, odd_coeff, inter_coeff;
+  if (thisstring        == "iso1")
+    { test_fn = 1; even_coeff = odd_coeff = 10.; }
+  else if (thisstring   == "iso2")
+    { test_fn = 2; even_coeff = odd_coeff = inter_coeff = 1.; }
+  else if (thisstring   == "iso3")
+    { test_fn = 3; even_coeff = odd_coeff = 10.; }
+  else if (thisstring == "aniso1")
+    { test_fn = 1; even_coeff =  1.; odd_coeff = 10.; }
+  else if (thisstring == "aniso2")
+    { test_fn = 2; even_coeff =  1.; odd_coeff = inter_coeff = 10.; }
+  else if (thisstring == "aniso3")
+    { test_fn = 3; even_coeff = 10.; odd_coeff = 5.; }
+  else {
+    cerr << "Error: analysis component specification required in gerstner "
+         << "direct fn." << std::endl;
+    exit(-1);
+  }
+  cout << even_coeff << "even_coeff" << "\n";
+  cout << odd_coeff << "odd_coeff" << "\n";
+  vector<double> xC;
+  xC.resize(2);
+  xC[0]=x[0];
+  xC[1]=x[1];
+
+  double fnVal = 0.; 
+  // ***** f
+  if (ASV[0] & 1) {
+    switch (test_fn) {
+    case 1:
+      fnVal = 0.;
+      for (size_t i=0; i<num_vars; ++i)
+        fnVal += (i%2) ? odd_coeff*exp(-xC[i]*xC[i]) :
+                         even_coeff*exp(-xC[i]*xC[i]); break;
+    case 2:
+      fnVal = 0.;
+      for (size_t i=0; i<num_vars; ++i)
+        if (i%2)
+          fnVal +=  odd_coeff*exp(xC[i])
+                      + inter_coeff*exp(xC[i]*xC[i-1]);
+        else
+          fnVal += even_coeff*exp(xC[i]);
+      break;
+    case 3: {
+      double sum = 0;
+      for (size_t i=0; i<num_vars; ++i)
+        sum -= (i%2) ? odd_coeff*xC[i]*xC[i] : even_coeff*xC[i]*xC[i];
+      fnVal = exp(sum); break;
+    }
+    }
+  }
+
+  fout << "                     " << fnVal << " f\n";
+
+  fout.flush();
+  fout.close();
+  return 0;
+}
+
+  
