9b633589446df3af9cd9df0b3a9d5991126a6643
wjbohnh <wjbohnh@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2014-08-08 19:12:54 +0000

    Add "framework" for more granular testing of workdir utils/features. Just a stub for now.
    ToDo:  Move into the test subdir when the time is right.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@2713 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ src/test_workdir_utils.cpp
@@ -0,0 +1,75 @@
+/*  _______________________________________________________________________
+
+    DAKOTA: Design Analysis Kit for Optimization and Terascale Applications
+    2opyright (c) 2010, Sandia National Laboratories.
+    This software is distributed under the GNU Lesser General Public License.
+    For more information, see the README file in the top Dakota directory.
+    _______________________________________________________________________ */
+
+#include "WorkdirHelper.hpp"
+#include "dakota_data_util.hpp"  // for strcontains
+#include "dakota_global_defs.hpp"
+//#include "dakota_filesystem_utils.hpp"
+
+//#include <boost/assign/std/vector.hpp>
+//#include <boost/foreach.hpp>
+
+#include <cassert>
+#include <iostream>
+
+using namespace Dakota;
+
+namespace Dakota {
+}
+
+
+namespace TestWorkdir {
+
+// ToDo:  Populate a handful of workdir util "unit" tests
+
+} // end namespace TestWorkdir
+
+
+int main()
+{
+#if 0
+  // argc/argv??
+
+  //bfs::path fq_search(argv[1]);
+  bfs::path fq_search( std::getenv("HOME") );
+
+  // TODO: better way to do this (trailing slash yields filename of .)
+  // want to allow matching /tmp/foo/ as /tmp with entry foo so foo
+  // gets copied or linked
+
+  // BUT if fq_search is a symlink, perhaps want to resolve it...
+  // In Posix, trailing slash on symlink means follow it
+
+  if ( boost::equals( (--fq_search.end())->native(), ".") )
+    ;//fq_search.remove_filename();
+
+  bfs::path search_dir = fq_search.parent_path();
+
+  // could instead consider the Dakota runtime PWD
+    //search_dir = std::getenv(PWD);
+  if (search_dir.empty())
+    search_dir = ".";
+
+  std::string wild_card = fq_search.filename().native();
+
+  std::cout << "Searching " << search_dir << " for " << wild_card << std::endl;
+
+  bfs::directory_iterator dir_it(search_dir);
+  bfs::directory_iterator dir_end;
+  for ( ; dir_it != dir_end; ++dir_it) {
+    ;//if (matches_wildcard(dir_it->path().filename(), wild_card))
+      std::cout << "in wildcard: " << dir_it->path() << std::endl;
+    //else
+    //  std::cout << "NOT in wildcard: " << dir_it->path() << std::endl;
+  }
+
+  //return status;
+#endif
+return 0;
+}
+
