aa88f13232dc9806aed04c04386e614a9bd2dbf5
wjbohnh <wjbohnh@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2014-12-10 00:01:22 +0000

    Add test to demonstrate linkage of "unit" tests with Dakota. It would be preferable to split-up dakota into re-useable components for unit testing, but c'est la vie.
    The test still doesn't much of anything useful, but the extra lines
    specifying the linkage spec in unit_test/CMakeLists.txt may aleviate
    some burden for those wanting to author unit tests ASAP.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@2989 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- unit_test/min_unit_test.cpp
+++ unit_test/min_unit_test.cpp
@@ -9,6 +9,12 @@
 #include <boost/test/minimal.hpp>     // header-only minimal testing
 //#include <boost/test/unit_test.hpp> // requires linkage -lboost_unit_test_framework
 
+#include "DakotaBuildInfo.hpp"
+
+#include <boost/lexical_cast.hpp>
+#include <exception>
+
+
 namespace DakotaUnitTest {
 namespace MinTest {
 
@@ -16,6 +22,25 @@ void my_test0()
 {
   BOOST_CHECK(0 == 0);
   BOOST_CHECK(1+1 == 2);
+}
+
+
+void rev_test()
+{
+  using boost::lexical_cast;
+
+  try {
+    // It is desirable to split-up dakota into packages of re-useable components
+    // but that is a significant re-factoring effort in its own right, so
+    // link with the whole dakota library (for now)
+
+    std::string rev_str = Dakota::DakotaBuildInfo::get_rev_number();
+    short rev = lexical_cast<short>(rev_str);
+    BOOST_CHECK(rev > 2852);
+  }
+  catch (const boost::bad_lexical_cast &) {
+    boost::exit_failure;
+  }
 }
 
 } // end of MinTest namespace
@@ -28,6 +53,8 @@ void my_test0()
 int test_main( int argc, char* argv[] )      // note the name!
 {
   DakotaUnitTest::MinTest::my_test0();
+  DakotaUnitTest::MinTest::rev_test();
+
   return boost::exit_success;
 }
 
