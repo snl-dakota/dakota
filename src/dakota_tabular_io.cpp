66b79aa43e8f4668d802688109b88ef114e4ec79
jasteph <jasteph@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2015-02-05 20:15:15 +0000

    After discussion with Brian, left-aligning the eval_id column instead of prepending a space to the header row. This avoids adding a superfluous column when converting to csv or importing into Excel.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@3069 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- src/dakota_tabular_io.cpp
+++ src/dakota_tabular_io.cpp
@@ -61,7 +61,7 @@ void write_header_tabular(std::ostream& tabular_ostream,
 			  const std::string& counter_label)
 {
   // headers use Matlab comment syntax
-  tabular_ostream << " %" << counter_label << ' ';
+  tabular_ostream << "%" << counter_label << ' ';
   if (TABULAR_IFACE_ID)
     tabular_ostream << "interface ";
   vars.write_tabular_labels(tabular_ostream);
@@ -72,7 +72,10 @@ void write_header_tabular(std::ostream& tabular_ostream,
 void write_leading_columns(std::ostream& tabular_ostream, size_t eval_id, 
 			   const String& iface_id)
 {
-  tabular_ostream << std::setw(9) << eval_id << ' ';
+  // align left to make eval_id consistent with whitespace-delimited header row
+  std::ios_base::fmtflags before_left_align = tabular_ostream.flags();
+  tabular_ostream << std::setw(8) << std::left << eval_id << ' ';
+  tabular_ostream.flags(before_left_align);
   if (TABULAR_IFACE_ID) {
     // write the interface ID string, NO_ID for empty
     // (Dakota 6.1 used EMPTY for missing ID)
