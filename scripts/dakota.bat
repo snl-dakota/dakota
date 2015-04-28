966fcdb8bcb8de82259a068096644ea52ce3f1cb
briadam <briadam@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2015-04-28 23:04:46 +0000

    Allow dakota.bat to work when the installation path has spaces in it.
    Patch created by Kevin Olson and refined by Ernest (Foss) Friedman-Hill (DART Team), submitted March 19, 2015.  Includes considerable simplification and cleanup.  Also suppress messages to stderr during PATH query.
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@3301 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- scripts/dakota.bat
+++ scripts/dakota.bat
@@ -1,5 +1,6 @@
 @echo off
-
+setlocal enabledelayedexpansion
+ 
 rem  _______________________________________________________________________
 rem
 rem  DAKOTA: Design Analysis Kit for Optimization and Terascale Applications
@@ -7,35 +8,31 @@ rem  Copyright 2014 Sandia Corporation.
 rem  This software is distributed under the GNU Lesser General Public License.
 rem  For more information, see the README file in the top Dakota directory.
 rem  _______________________________________________________________________
-
-rem Wrapper script dakota.bat to help manage binary and library paths
-rem when running dakota.  Assume dakota is installed alongside this
-rem script and libraries are in the same directory and/or ..\lib.
-
-rem get the name and extension of this script
-set script_name=%~nx0
-
-rem get the path (drive letter and path) to this wrapper script
-rem assume DAKOTA lives in same directory
-set execpath=%~dp0
-
-if not exist "%execpath%\dakota.exe" (
-  echo Error in %script_name%
-  echo   Could not find dakota binary in %execpath%
-  exit /B 1
-) 
-
-rem Getting the quotes correct here is really tricky
-rem Seems better to minimally quote, though might break with 
-rem spaces in PATH or Dakota location...
-
-set libpaths=%execpath%;%execpath%\..\lib
-
-rem Append PATH if not already done (not reliably though)
-rem Do in a single statement without quotes due to possible
-rem existence of both parens and space in PATH
-
-echo %PATH% | find "%libpaths%" > NUL || set PATH=%libpaths%;%PATH%;%execpath%\..\test 
-
-rem echo "Launching %execpath%\dakota.exe with args: %*"
-"%execpath%\dakota.exe" %*
+ 
+rem  _______________________________________________________________________
+rem
+rem see if the path where this .bat was launched is already in the PATH; if so
+rem do not add
+rem  _______________________________________________________________________
+echo "%PATH%" | find "%~dp0" > NUL 2>&1 || set PATH=%~dp0;%~dp0\..\lib;%PATH%;%~dp0\..\test
+ 
+rem  _______________________________________________________________________
+rem
+rem see if we can now find dakota.exe on the path; this approach handles
+rem issues with spaces and/or parentheses in the path
+rem  _______________________________________________________________________
+for %%i in (dakota.exe) do set chk=%%~$PATH:i
+ 
+if "%chk%" == "" (
+   echo "**Failed to find Dakota binary"
+   exit /B 1
+)
+ 
+rem  _______________________________________________________________________
+rem
+rem launch the found program, which will include the full path; this launch
+rem   will handle spaces and parentheses in the path; pass along all of the
+rem   other parameters specified to this file
+rem  _______________________________________________________________________
+ 
+"%chk%" %*
