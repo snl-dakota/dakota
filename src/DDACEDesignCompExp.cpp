snl-dakota/dakota

commit 0d7fc1d444843cd2777137b100a222baf0190631

Properly support post-run for DDACE, including main effects and quality metrics.
Address user-reported bug.  Symbol adjustments weren't applied in post-run mode, nor was the symbolMapping intialized.  Refactored DDACE wrapper to modularly create the sampler object and use it as needed in both pre-run and post-run contexts.

Added tests for post-run with PSUADE, FSUDace, and DDACE (already have LHS coverage in dakota_tabular_io.in.



git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@3196 aa0779c3-6ec2-4b84-a212-a405c134cd8d

src/DDACEDesignCompExp.cpp
@@ -13,6 +13,7 @@
 #include "DDACEDesignCompExp.hpp"
 #include "dakota_system_defs.hpp"
 #include "ProblemDescDB.hpp"
+#include "DDaceSampler.h"
 #include "DDaceRandomSampler.h"
 #include "DDaceOASampler.h"
 #include "DDaceOALHSampler.h"
@@ -112,6 +113,12 @@ void DDACEDesignCompExp::extract_trends()
 
 void DDACEDesignCompExp::post_input()
 {
+  if (!seedSpec) {
+    Cerr << "\nError (DACE): post_run mode requires user-specified seed.\n";
+    abort_handler(-1);
+  }
+  // apply any corrections to user spec to update numSamples before post input
+  resolve_samples_symbols();
   // call convenience function from Analyzer
   read_variables_responses(numSamples, numContinuousVars);
 }
@@ -119,6 +126,12 @@ void DDACEDesignCompExp::post_input()
 
 void DDACEDesignCompExp::post_run(std::ostream& s)
 {
+  // main effects require generating the symbolMapping for post-run
+  if (mainEffectsFlag && symbolMapping.empty()) {
+    boost::shared_ptr<DDaceSamplerBase> ddace_sampler = 
+      create_sampler(iteratedModel);
+    symbolMapping = ddace_sampler->getP();
+  }
   // In VBD case, stats are managed in the run phase
   if (!varBasedDecompFlag) {
     if (mainEffectsFlag) // need allResponses
@@ -143,6 +156,9 @@ void DDACEDesignCompExp::get_parameter_sets(Model& model)
   // keep track of number of DACE executions for this object
   numDACERuns++;
 
+  Cout << "\nDACE method = " << submethod_enum_to_string(daceMethod) 
+       << " Samples = " << numSamples << " Symbols = " << numSymbols;
+
   // If a seed is specified, use it to get repeatable behavior, else allow DDACE
   // to generate different samples each time (seeded from a system clock).  For
   // the case where extract_trends() may be called multiple times for the same
@@ -159,8 +175,6 @@ void DDACEDesignCompExp::get_parameter_sets(Model& model)
   }
   else if (!varyPattern) // force same sample pattern: reset to previous value
     DistributionBase::setSeed(randomSeed);
-  Cout << "\nDACE method = " << daceMethod << " Samples = " << numSamples
-       << " Symbols = " << numSymbols;
   if (varyPattern && numDACERuns > 1)
     Cout << " Seed not reset from previous DACE execution\n";
   else if (seedSpec)
@@ -168,6 +182,47 @@ void DDACEDesignCompExp::get_parameter_sets(Model& model)
   else
     Cout << " Seed (system-generated) = " << randomSeed << '\n';
 
+  // vector used for DDace getSamples
+  std::vector<DDaceSamplePoint> sample_points(numSamples);
+
+  // in get_parameter_sets, generate the samples; could omit the symbolMapping
+  boost::shared_ptr<DDaceSamplerBase> ddace_sampler = 
+    create_sampler(iteratedModel);
+  ddace_sampler->getSamples(sample_points);
+  if (mainEffectsFlag)
+    symbolMapping = ddace_sampler->getP();
+
+  // copy the DDace sample array to allSamples
+  if (allSamples.numRows() != numContinuousVars ||
+      allSamples.numCols() != numSamples)
+    allSamples.shapeUninitialized(numContinuousVars, numSamples);
+  for (int i=0; i<numSamples; ++i) {
+    Real* all_samp_i = allSamples[i];
+    const DDaceSamplePoint& sample_pt_i = sample_points[i];
+    for (int j=0; j<numContinuousVars; ++j)
+      all_samp_i[j] = sample_pt_i[j];
+  }
+
+  if (volQualityFlag) {
+    double* dace_points = new double [numContinuousVars*numSamples];
+    copy_data(sample_points, dace_points, numContinuousVars*numSamples);
+    const RealVector& c_l_bnds = model.continuous_lower_bounds();
+    const RealVector& c_u_bnds = model.continuous_upper_bounds();
+    for (int i=0; i<numContinuousVars; i++) {
+      const double& offset = c_l_bnds[i];
+      double norm = 1. / (c_u_bnds[i] - c_l_bnds[i]);
+      for (int j=0; j<numSamples; j++)
+        dace_points[i+j*numContinuousVars]
+	  = (dace_points[i+j*numContinuousVars] - offset) * norm;
+    }
+    volumetric_quality(numContinuousVars, numSamples, dace_points);
+    delete [] dace_points;
+  }
+}
+
+boost::shared_ptr<DDaceSamplerBase>
+DDACEDesignCompExp::create_sampler(Model& model)
+{
   // Get bounded region and check that (1) the lengths of bounds arrays are 
   // consistent with numContinuousVars, and (2) the bounds are not default 
   // bounds (upper/lower = +/-DBL_MAX) since this results in Infinity in the 
@@ -183,8 +238,7 @@ void DDACEDesignCompExp::get_parameter_sets(Model& model)
          << "\n       bounds arrays in DDACEDesignCompExp." << std::endl;
     abort_handler(-1);
   }
-  int i, j;
-  for (i=0; i<numContinuousVars; i++) {
+  for (int i=0; i<numContinuousVars; i++) {
     if (c_l_bnds[i] <= -DBL_MAX || c_u_bnds[i] >= DBL_MAX) {
       Cerr << "\nError: DDACEDesignCompExp requires specification of variable "
 	   << "bounds for all active variables." << std::endl;
@@ -195,96 +249,61 @@ void DDACEDesignCompExp::get_parameter_sets(Model& model)
   // Construct a uniform distribution vector
   // An alternative option is a normal distribution 
   std::vector<Distribution> ddace_distribution(numContinuousVars);
-  for (i=0; i<numContinuousVars; i++)
+  for (int i=0; i<numContinuousVars; i++)
     ddace_distribution[i]= UniformDistribution(c_l_bnds[i], c_u_bnds[i]);
-
-  // vector used for DDace getSamples
-  std::vector<DDaceSamplePoint> sample_points(numSamples);
 
   bool noise = true;
   switch (daceMethod) {
   case SUBMETHOD_OAS: {
-    DDaceOASampler ddace_sampler(numSamples, noise, ddace_distribution);
-    ddace_sampler.getSamples(sample_points);
-    if (mainEffectsFlag)
-      symbolMapping = ddace_sampler.getP();
+    return boost::shared_ptr<DDaceSamplerBase>
+      (new DDaceOASampler(numSamples, noise, ddace_distribution));
     break;
   }
   case SUBMETHOD_OA_LHS: {
     int strength = 2;
     bool randomize = true;
-    DDaceOALHSampler ddace_sampler(numSamples, numContinuousVars, strength,
-				   randomize, ddace_distribution);
-    ddace_sampler.getSamples(sample_points);
-    if (mainEffectsFlag)
-      symbolMapping = ddace_sampler.getP();
+    return boost::shared_ptr<DDaceSamplerBase>
+      (new DDaceOALHSampler(numSamples, numContinuousVars, strength, randomize,
+			    ddace_distribution));
     break;
   }
   case SUBMETHOD_LHS: {
     int replications = numSamples/numSymbols;
-    DDaceLHSampler ddace_sampler(numSamples, replications, noise,
-				 ddace_distribution);
-    ddace_sampler.getSamples(sample_points);
-    if (mainEffectsFlag)
-      symbolMapping = ddace_sampler.getP();
+    return boost::shared_ptr<DDaceSamplerBase>
+      (new DDaceLHSampler(numSamples, replications, noise, ddace_distribution));
     break;
   }
   case SUBMETHOD_RANDOM: {
-    DDaceRandomSampler ddace_sampler(numSamples, ddace_distribution);
-    ddace_sampler.getSamples(sample_points);
+      return boost::shared_ptr<DDaceSamplerBase>
+	(new DDaceRandomSampler(numSamples, ddace_distribution));
     break;
   }
   case SUBMETHOD_GRID: {
-    DDaceFactorialSampler ddace_sampler(numSamples, numSymbols, noise,
-					ddace_distribution);
-    ddace_sampler.getSamples(sample_points);
-    if (mainEffectsFlag)
-      symbolMapping = ddace_sampler.getP();
+    return boost::shared_ptr<DDaceSamplerBase>
+      (new DDaceFactorialSampler(numSamples, numSymbols, noise, 
+				 ddace_distribution));
     break;
   }
   case SUBMETHOD_CENTRAL_COMPOSITE: {
-    DDaceCentralCompositeSampler ddace_sampler(numSamples, numContinuousVars,
-					       ddace_distribution);
-    ddace_sampler.getSamples(sample_points);
+    return boost::shared_ptr<DDaceSamplerBase>
+      (new DDaceCentralCompositeSampler(numSamples, numContinuousVars,
+					ddace_distribution));
     break;
   }
   case SUBMETHOD_BOX_BEHNKEN: {
-    DDaceBoxBehnkenSampler ddace_sampler(numSamples, numContinuousVars,
-					 ddace_distribution);
-    ddace_sampler.getSamples(sample_points);
+    return boost::shared_ptr<DDaceSamplerBase>
+      (new DDaceBoxBehnkenSampler(numSamples, numContinuousVars,
+				  ddace_distribution));
     break;
   }
   default:
-    Cerr << "DDACE method \"" << daceMethod << "\" is not available at this "
-    	 << "time\nplease choose another sampling method." << std::endl;
+    Cerr << "DDACE method \"" << submethod_enum_to_string(daceMethod) 
+	 << "\" is not available at this time\n"
+	 << "please choose another sampling method." << std::endl;
     abort_handler(-1);
     break;
   }
 
-  // copy the DDace sample array to allSamples
-  if (allSamples.numRows() != numContinuousVars ||
-      allSamples.numCols() != numSamples)
-    allSamples.shapeUninitialized(numContinuousVars, numSamples);
-  for (i=0; i<numSamples; ++i) {
-    Real* all_samp_i = allSamples[i];
-    const DDaceSamplePoint& sample_pt_i = sample_points[i];
-    for (j=0; j<numContinuousVars; ++j)
-      all_samp_i[j] = sample_pt_i[j];
-  }
-
-  if (volQualityFlag) {
-    double* dace_points = new double [numContinuousVars*numSamples];
-    copy_data(sample_points, dace_points, numContinuousVars*numSamples);
-    for (i=0; i<numContinuousVars; i++) {
-      const double& offset = c_l_bnds[i];
-      double norm = 1. / (c_u_bnds[i] - c_l_bnds[i]);
-      for (j=0; j<numSamples; j++)
-        dace_points[i+j*numContinuousVars]
-	  = (dace_points[i+j*numContinuousVars] - offset) * norm;
-    }
-    volumetric_quality(numContinuousVars, numSamples, dace_points);
-    delete [] dace_points;
-  }
 }
 
 
@@ -427,8 +446,8 @@ void DDACEDesignCompExp::resolve_samples_symbols()
     break;
   }
   default:
-    Cerr << "Error: DDACE method \"" << daceMethod << "\" is not an option."
-	 << std::endl;
+    Cerr << "Error: DDACE method \"" << submethod_enum_to_string(daceMethod) 
+	 << "\" is not an option." << std::endl;
     abort_handler(-1); break;
   }
 

src/DDACEDesignCompExp.hpp
@@ -19,6 +19,7 @@
 
 #include "DakotaPStudyDACE.hpp"
 
+class DDaceSamplerBase;
 class DDaceSamplePoint;
 
 namespace Dakota {
@@ -74,6 +75,9 @@ private:
 
   /// builds a DDaceMainEffects::OneWayANOVA if mainEffectsFlag is set
   void compute_main_effects();
+
+  /// create a DDACE sampler
+  boost::shared_ptr<DDaceSamplerBase> create_sampler(Model& model);
 
   /// convenience function for resolving number of samples and
   /// number of symbols from input.

test/dakota_base.test
@@ -5026,6 +5026,75 @@ Partial Rank Correlation Matrix between input and output:
        cdv_2 -7.28695e-02 -8.56853e-01  9.88141e-01
        cdv_3 -5.40213e-02  1.10211e-01  1.53494e-01
        csv_1  9.86310e-01 -1.14127e-01 -2.68944e-01
+Test Number 10 succeeded
+<<<<< Function evaluation summary (I1): 49 total (49 new, 0 duplicate)
+<<<<< Best parameters          =
+                      3.0625565391e-01 cdv_1
+                      1.7966183297e-01 cdv_2
+                      1.9281970958e-01 cdv_3
+                      2.1916471235e+00 csv_1
+<<<<< Best objective function  =
+                      3.1254689919e+00
+<<<<< Best constraint values   =
+                      3.9616090681e-03
+                     -1.2084945273e-01
+<<<<< Best data captured at function evaluation 20
+Test Number 11 succeeded
+<<<<< Function evaluation summary (I1): 0 total (0 new, 0 duplicate)
+<<<<< Best parameters          =
+                      3.0625565390e-01 cdv_1
+                      1.7966183300e-01 cdv_2
+                      1.9281970960e-01 cdv_3
+                      2.1916471240e+00 csv_1
+<<<<< Best objective function  =
+                      3.1254689920e+00
+<<<<< Best constraint values   =
+                      3.9616090680e-03
+                     -1.2084945270e-01
+<<<<< Best data captured at function evaluation 20
+Test Number 12 succeeded
+<<<<< Function evaluation summary (I1): 0 total (0 new, 0 duplicate)
+<<<<< Best parameters          =
+                      1.0000000000e-02 cdv_1
+                      1.0000000000e-02 cdv_2
+                      1.0000000000e-02 cdv_3
+                      4.0000000000e-02 csv_1
+<<<<< Best objective function  =
+                      3.7311345900e+00
+<<<<< Best constraint values   =
+                     -4.9000000000e-03
+                     -4.9000000000e-03
+<<<<< Best data captured at function evaluation 37
+Simple Correlation Matrix among all inputs and outputs:
+                    cdv_1        cdv_2        cdv_3        csv_1       obj_fn nln_ineq_con_1 nln_ineq_con_2 
+       cdv_1  1.00000e+00 
+       cdv_2 -9.19292e-03  1.00000e+00 
+       cdv_3  1.47807e-01  7.68677e-02  1.00000e+00 
+       csv_1  1.01530e-01 -6.43744e-02 -1.02346e-01  1.00000e+00 
+      obj_fn  1.05126e-01  1.78107e-02 -1.94660e-01  8.31953e-01  1.00000e+00 
+nln_ineq_con_1  9.42558e-01 -2.38183e-01  1.55801e-01  6.67084e-02  7.84669e-02  1.00000e+00 
+nln_ineq_con_2 -2.32008e-01  9.44008e-01  2.28649e-02 -1.21357e-01 -5.76912e-02 -4.31677e-01  1.00000e+00 
+Partial Correlation Matrix between input and output:
+                   obj_fn nln_ineq_con_1 nln_ineq_con_2 
+       cdv_1  7.43488e-02  9.68632e-01 -6.63956e-01 
+       cdv_2  1.47614e-01 -7.02022e-01  9.68993e-01 
+       cdv_3 -2.18918e-01  1.26077e-01 -8.88495e-02 
+       csv_1  8.33444e-01 -1.67055e-01 -1.67226e-01 
+Simple Rank Correlation Matrix among all inputs and outputs:
+                    cdv_1        cdv_2        cdv_3        csv_1       obj_fn nln_ineq_con_1 nln_ineq_con_2 
+       cdv_1  1.00000e+00 
+       cdv_2 -9.19292e-03  1.00000e+00 
+       cdv_3  1.47807e-01  7.68677e-02  1.00000e+00 
+       csv_1  1.01530e-01 -6.43744e-02 -1.02346e-01  1.00000e+00 
+      obj_fn  1.06883e-01 -5.76298e-02 -1.04986e-01  9.82118e-01  1.00000e+00 
+nln_ineq_con_1  9.52943e-01 -2.64518e-01  1.40582e-01  8.60366e-02  1.00978e-01  1.00000e+00 
+nln_ineq_con_2 -2.70891e-01  9.51755e-01  2.25983e-02 -9.48215e-02 -8.27003e-02 -4.90765e-01  1.00000e+00 
+Partial Rank Correlation Matrix between input and output:
+                   obj_fn nln_ineq_con_1 nln_ineq_con_2 
+       cdv_1  4.31227e-02  9.85704e-01 -8.48519e-01 
+       cdv_2  3.21895e-02 -8.50089e-01  9.86179e-01 
+       cdv_3 -3.26585e-02  1.06062e-01 -7.96832e-02 
+       csv_1  9.81651e-01 -1.54832e-01 -5.37690e-02 
 dakota_dll.in
 Test Number 0 succeeded
 <<<<< Function evaluation summary: 81 total (81 new, 0 duplicate)
@@ -10646,6 +10715,32 @@ Test Number 0 succeeded
                       0.0000000000e+00 cdv_20
 <<<<< Best objective function  =
                      -1.8137481543e+02
+<<<<< Best data captured at function evaluation 19
+Test Number 1 succeeded
+<<<<< Function evaluation summary: 0 total (0 new, 0 duplicate)
+<<<<< Best parameters          =
+                      6.6666666670e-01 cdv_1
+                      3.3333333330e-01 cdv_2
+                      0.0000000000e+00 cdv_3
+                      0.0000000000e+00 cdv_4
+                      0.0000000000e+00 cdv_5
+                      3.3333333330e-01 cdv_6
+                      0.0000000000e+00 cdv_7
+                      0.0000000000e+00 cdv_8
+                      6.6666666670e-01 cdv_9
+                      0.0000000000e+00 cdv_10
+                      1.0000000000e+00 cdv_11
+                      6.6666666670e-01 cdv_12
+                      3.3333333330e-01 cdv_13
+                      0.0000000000e+00 cdv_14
+                      1.0000000000e+00 cdv_15
+                      0.0000000000e+00 cdv_16
+                      3.3333333330e-01 cdv_17
+                      6.6666666670e-01 cdv_18
+                      3.3333333330e-01 cdv_19
+                      0.0000000000e+00 cdv_20
+<<<<< Best objective function  =
+                     -1.8137481540e+02
 <<<<< Best data captured at function evaluation 19
 dakota_rbdo_cantilever.in 
 Test Number 0 succeeded

test/dakota_dace.in
@@ -1,3 +1,7 @@
+#@ s11: DependsOn=s10
+#@ s11: ExecArgs='-post_run dakota_dace.10.dat::'
+#@ s12: DependsOn=s7
+#@ s12: ExecArgs='-post_run dakota_dace.7.dat::'
 #@ p0: MPIProcs=4
 #@ p1: MPIProcs=4
 #@ p2: MPIProcs=4
@@ -8,27 +12,36 @@
 
 environment,
 	  graphics
-	  tabular_graphics_data
+	  tabular_data
+#	    tabular_data_file 'dakota_dace.10.dat'	#s10
+#	    tabular_data_file 'dakota_dace.7.dat'	#s7
 
 method,
-	dace oas seed = 5		#s0,#s8,#p0,#p1,#p2,#p3,#p4
+	dace oas seed = 5		#s0,#s8,#s10,#s11,#p0,#p1,#p2,#p3,#p4
+	  quality_metrics		#s0,#s10,#s11
 #       dace oa_lhs seed = 5            #s9
 	  samples = 49 symbols = 7 	#s0,#s8,#s9,#p0,#p1,#p2,#p3,#p4
 #	dace lhs seed = 5		#s1
 #	  samples = 50 symbols = 50	#s1
+# Test post-run with automatic samples/symbols adjustment, main effects, and 
+# symbols to exercise dependencies in pre vs post run
+#          main_effects	       		#s10,#s11
+#	   samples = 42 symbols = 6  	#s10,#s11
 #	fsu_quasi_mc hammersley		#s2,#s3
 #	fsu_quasi_mc halton		#s4,#s5
-#	fsu_cvt				#s6,#s7
-#	  samples = 100			#s2,#s3,#s4,#s5,#s6,#s7
-#	  latinize			#s3,#s5,#s7
-	  quality_metrics		#s0,#s3,#s5,#s7
+#	fsu_cvt				#s6,#s7,#s12
+#	  samples = 100			#s2,#s3,#s4, #s5,#s6,#s7,#s12
+#	  latinize			#s3,#s5,#s7,#s12
+#	  quality_metrics		#s3,#s5,#s7,#s12
 #	  sequence_start 0 0 0 0	#s2,#s3,#s4,#s5
 #	  sequence_leap  1 1 1 1	#s2,#s3,#s4,#s5
 #	  prime_base   3 5 7		#s2
 #	  prime_base 2 3 5 7  		#s4
-#  	  seed = 50374 fixed_seed	#s6,#s7
-#	  num_trials = 10000		#s6,#s7
-#	  max_iterations = 100		#s6,#s7
+#  	  seed = 50374 fixed_seed	#s6,#s7,#s12
+#	  num_trials = 10000		#s6,#s7,#s12
+#	  max_iterations = 100		#s6,#s7,#s12
+# Test post-run for FSUDace
+# 
 #	  output quiet	   		#s1,#s2,#s3,#s4,#s5,#s6,#s7,#s8,#s9,#p0,#p1,#p2,#p3,#p4
 
 variables,
@@ -55,8 +68,8 @@ interface,
 #	evaluation_servers = 4	   		#p4
 #	processors_per_evaluation = 1		#p4
 	system
-	  asynchronous			#s0,#s1,#s2,#s3,#s4,#s5,#s6,#s7,#s8,#s9,#p2,#p3
-	    evaluation_concurrency = 5	#s0,#s1,#s2,#s3,#s4,#s5,#s6,#s7,#s8,#s9,#p2
+	  asynchronous			#s0,#s1,#s2,#s3,#s4,#s5,#s6,#s7,#s8,#s9,#s10,#s11,#s12,#p2,#p3
+	    evaluation_concurrency = 5	#s0,#s1,#s2,#s3,#s4,#s5,#s6,#s7,#s8,#s9,#s10,#s11,#s12,#p2
 #	    evaluation_concurrency = 10	#p3
 	  analysis_driver = 'text_book'
 

test/dakota_psuade.in
@@ -1,4 +1,9 @@
-# DAKOTA INPUT FILE : dakota_moat.in
+#@ s1: DependsOn=s0
+#@ s1: ExecArgs='-post_run dakota_psuade.0.dat::'
+# DAKOTA INPUT FILE : dakota_psuade.in
+environment					#s0,#s1
+  tabular_data					#s0
+    tabular_data_file 'dakota_psuade.0.dat'	#s0
 
 method,
 	psuade_moat

commit 1d27971142f20ace237b920a6814013621eed20f

Correct DDACE bug I introduced in r3196; logic for requiring a user-specified seed with post-run was wrong and too strict, resulting in error when running DDACE in normal run mode.  Now fixed seed is only required when the symbol mapping is uninitialized in the post-run phase.
In general, need a better way to distinguish specific run modes from running all modes.



git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@3494 aa0779c3-6ec2-4b84-a212-a405c134cd8d

src/DDACEDesignCompExp.cpp
@@ -13,6 +13,7 @@
 #include "DDACEDesignCompExp.hpp"
 #include "dakota_system_defs.hpp"
 #include "ProblemDescDB.hpp"
+#include "ParallelLibrary.hpp"
 #include "DDaceSampler.h"
 #include "DDaceRandomSampler.h"
 #include "DDaceOASampler.h"
@@ -113,12 +114,11 @@ void DDACEDesignCompExp::extract_trends()
 
 void DDACEDesignCompExp::post_input()
 {
-  if (!seedSpec) {
-    Cerr << "\nError (DACE): post_run mode requires user-specified seed.\n";
-    abort_handler(-1);
+  if (parallelLib.command_line_user_modes() && 
+      !parallelLib.command_line_post_run_input().empty()) {
+    // apply any corrections to user spec to update numSamples before post input
+    resolve_samples_symbols();
   }
-  // apply any corrections to user spec to update numSamples before post input
-  resolve_samples_symbols();
   // call convenience function from Analyzer
   read_variables_responses(numSamples, numContinuousVars);
 }
@@ -126,8 +126,15 @@ void DDACEDesignCompExp::post_input()
 
 void DDACEDesignCompExp::post_run(std::ostream& s)
 {
-  // main effects require generating the symbolMapping for post-run
+  // Main effects require the symbolMapping.  It should only be empty
+  // if pre-run get_parameter_sets wasn't called in this execution, in
+  // which case, we need a fixed seed to properly regenerate it.
   if (mainEffectsFlag && symbolMapping.empty()) {
+    if (!seedSpec) {
+      Cerr << "\nError (DACE): calculating main effects in post_run mode "
+	   << "requires user-specified seed.\n";
+      abort_handler(-1);
+    }
     boost::shared_ptr<DDaceSamplerBase> ddace_sampler = 
       create_sampler(iteratedModel);
     symbolMapping = ddace_sampler->getP();