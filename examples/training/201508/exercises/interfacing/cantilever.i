19025d8b596b8876abd51b847cb703ab117a7412
jasteph <jasteph@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2015-08-09 23:44:27 +0000

    Interfacing exercise.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@3554 aa0779c3-6ec2-4b84-a212-a405c134cd8d

---
+++ examples/training/201508/exercises/interfacing/cantilever.i
@@ -0,0 +1,13 @@
+# cantilever.i
+# One parameter per line. Whitespace otherwise ignored.
+# Comments start with # and continue to the end of the line
+# All parameters below are required
+
+width = 1.0            # beam width (horizontal dimension), inches
+thickness = 1.0        # beam thickness (vertical dimension), inches
+length = 10.0          # beam length, inches
+youngs_modulus = 29e6  # lb/in^2
+horizontal_load = 5    # lb
+
+vertical_load = 10     # lb
+density = 500          # density, lb/ft^3
---
+++ examples/training/201508/exercises/interfacing/driver.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# $1 and $2 are special variables in bash that contain the 1st and 2nd 
+# command line arguments to the script, which are the names of the
+# Dakota parameters and results files, respectively.
+
+params=$1
+results=$2
+
+############################################################################### 
+##
+## Pre-processing Phase -- Generate/configure an input file for your simulation 
+##  by substiting in parameter values from the Dakota paramters file.
+##
+###############################################################################
+
+# TODO
+
+
+############################################################################### 
+##
+## Execution Phase -- Run your simulation
+##
+###############################################################################
+
+
+./cantilever cantilever.i > cantilever.log
+
+############################################################################### 
+##
+## Post-processing Phase -- Extract (or calculate) quantities of interest
+##  from your simulation's output and write them to a properly-formatted
+##  Dakota results file.
+##
+###############################################################################
+
+#TODO
+
