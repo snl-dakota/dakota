b6d5e86e6df2fa899bd960aeabf8795dd59b9c0f
arushdi <arushdi@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2015-09-08 23:36:38 +0000

    Updating NonDRKDDarts files to reflect the final stable versions of the adaptive sampling and numerical integration algorithms. Instead of using the evaluated samples, RKD constructs a global surrogate after the sample budget is exhausted. The surrogate is then evaluated to return the value of the high-d integration. As the team discussed and agreed in the BG meeting, RKD will remain in stealth mode, available to developers for experimental purposes, but not documented or exposed to Dakota users in the upcoming release. This is undertaken until further discussions and decisions on how to better incorporate it into Dakota as a utility helping existing UQ methods.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@3607 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- src/NonDRKDDarts.hpp
+++ src/NonDRKDDarts.hpp
@@ -68,22 +68,63 @@ protected:
     
     void init_rkd_darts();
     
+    void create_rkd_containers(size_t expected_num_samples);
+        
     /// -----
     /// perform the evaluate parameter sets portion of run
     /// void quantify_uncertainty();
     
-    double execute(size_t num_darts);
+    void execute();
     
-    double evaluate_function(double* x);
+    void create_initial_children(size_t parent);
+    
+    void create_new_sample(size_t parent, size_t left, size_t right, double position);
+    
+    void improve_parent_evaluation(size_t parent);
+    
+    void evaluate_1d_surrogate(size_t parent);
+    
+    double get_surrogate_interp_error(size_t parent);
+    
+    double estimate_surrogate_evaluation_err(size_t parent);
+    
+    void get_children(size_t parent, size_t* children);
+    
+    void get_neighbors(size_t sample, size_t &num_neighbors, size_t* neighbors);
+    
+    double interpolate_lagrange(size_t num_data_points, double* data_x, double* data_f, double x);
+    
+    double integrate_legendre_gauss(double xmin, double xmax, size_t num_data_points, double* data_x, double* data_f, double &err_est);
+    
+    double f_true(double* x); // for debuging only
+    
+    // ----------------------
+    // Dakota - related
+    // ----------------------
+    
+    void initialize_surrogates();
+
+    void compute_response(double* x);
+    
+    void add_surrogate_data(const Variables& vars, const Response& resp);
+
+    void build_surrogate();
+    
+    double eval_surrogate(size_t fn_index, double *vin);
+    
+    void estimate_rkd_surrogate();
     
     /// -----
     
     /// generate statistics
+    
     void post_run(std::ostream& s);
     
-    void print_integration_results();
+    void print_integration_results(std::ostream& s);
     
     void exit_rkd_darts();
+    
+    void destroy_rkd_containers();
     
     // ----------------------
     // - Heading: Data
@@ -108,12 +149,43 @@ protected:
     double zy;	/* SWB seed2 */
     size_t qlen;/* length of Q array */
     
-    // Variables of the RKD algorithm
-    size_t _num_dim; // dimension of the problem
-    double* _xmin; // lower left corner of the domain
-    double* _xmax; // Upper right corner of the domain
+private:
     
-    double _I_RKD; // Numerical Integration value
+     // Variables of the RKD algorithm
+    
+    double* _I_RKD;
+    
+    bool _eval_error;
+    
+    size_t _test_function;
+    
+    size_t _num_inserted_points;
+
+    size_t _num_dim, _num_samples, _max_num_samples, _num_evaluations, _evaluation_budget; // numberof dimensions, size of samples containers, _number of function evaluations
+    
+    size_t _max_num_neighbors; // maximum number of neighbor to conside in building lagrange interpolant in each cell
+    
+    double _bounding_box_volume;
+    
+    double _discont_jump_threshold; // discontinuity jump threshold for disconnecting neighbors across a discontinuity
+    
+    double* _xmin;
+    double* _xmax;
+    
+    double** _fval;
+    
+    size_t* _sample_dim;
+    size_t* _sample_parent;
+    size_t* _sample_first_child;
+    size_t* _sample_num_children;
+    size_t* _sample_left;
+    size_t* _sample_right;
+    double* _sample_coord;              // Sample one dimensional coordinate
+    double* _sample_value;              // Sample value (either through integration of children or direct function evaluation)
+    double* _sample_left_interp_err;
+    double* _sample_right_interp_err;
+    double* _sample_left_ev_err;
+    double* _sample_right_ev_err;
     
    };
 
