344b7a32fdd1904b9250088e282364d78ee7fee7
lpswile <lpswile@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2015-10-16 19:47:46 +0000

    Changes to support discrete variables in incremental LHS sampling. This commit addresses all types of integer discrete variables, including distributions such as the binomial, histogram variables, and discrete uncertain set variables.  It also supports discrete real variables.  Discrete string variables are not supported with the incremental capability at this time.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@3701 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- src/NonDIncremLHSSampling.cpp
+++ src/NonDIncremLHSSampling.cpp
@@ -34,6 +34,7 @@
 
 static const char rcsId[]="@(#) $Id: NonDIncremLHSSampling.cpp 6715 2010-04-02 21:58:15Z wjbohnh $";
 
+
 namespace Dakota {
   extern PRPCache data_pairs;
 
@@ -85,8 +86,8 @@ void NonDIncremLHSSampling::quantify_uncertainty()
     abort_handler(-1);
   }
 
-  if (numDiscreteIntVars || numDiscreteStringVars || numDiscreteRealVars) {
-    Cout << "Error: Discrete distribution types are not "
+  if ( numDiscreteStringVars ) {
+    Cout << "Error: Discrete distribution string types are not "
  	 << "supported in incremental LHS sampling.  " << std::endl;
     abort_handler(-1);
   }
@@ -113,14 +114,25 @@ void NonDIncremLHSSampling::quantify_uncertainty()
 
   size_t v, s, cntr;
   RealVectorArray sample1_values(numSamples); // views OK
+  IntVectorArray sample1int_values(numSamples);
+  RealVectorArray sample1real_values(numSamples);
+  //std::vector<StringMultiArrayConstView> sample1string_values(numSamples);
+
   // Restart records have negative eval IDs in the cache, so walk them
   // in reverse eval ID order (-1 to -numSamples).  May not work in
   // context of partial or concatenated restart.
   PRPCacheCRevIter prp_rev_iter = data_pairs.rbegin();
-  for (s=0; s<numSamples; ++s, ++prp_rev_iter)
+  for (s=0; s<numSamples; ++s, ++prp_rev_iter) {
     sample1_values[s] = prp_rev_iter->variables().continuous_variables();
+    sample1int_values[s] = prp_rev_iter->variables().discrete_int_variables();
+    sample1real_values[s] = prp_rev_iter->variables().discrete_real_variables();
+    //sample1string_values[s] = prp_rev_iter->variables().discrete_string_variables();
+  }
 #ifdef DEBUG
   Cout << "\nsample1\n" << sample1_values << '\n';
+  Cout << "\nsample1int\n" << sample1int_values << '\n';
+  Cout << "\nsample1real\n" << sample1real_values << '\n';
+  //Cout << "\nsample1string\n" << sample1string_values << '\n';
 #endif
 
   if (sampleType == SUBMETHOD_INCREMENTAL_RANDOM) {
@@ -134,7 +146,7 @@ void NonDIncremLHSSampling::quantify_uncertainty()
     // sample, and ranks of combined sample
     IntArray rank_col(numSamples), final_rank(numSamples);
     rawData.resize(numSamples);
-    IntMatrix sample1_ranks(numContinuousVars, numSamples, false);
+    IntMatrix sample1_ranks(numContinuousVars + numDiscreteIntVars + numDiscreteRealVars, numSamples, false);
 
     // store sample values and ranks returned by 1st get_parameter_sets() call
     for (v=0; v<numContinuousVars; ++v) {
@@ -148,13 +160,35 @@ void NonDIncremLHSSampling::quantify_uncertainty()
       for (s=0; s<numSamples; ++s) // can't be combined with loop above
         sample1_ranks(v, s) = final_rank[s] + 1;
     }
+    for (v=0; v<numDiscreteIntVars; ++v) {
+      for (size_t rank_count = 0; rank_count < numSamples; rank_count++){
+        rank_col[rank_count] = rank_count;
+        rawData[rank_count] = sample1int_values[rank_count][v];
+      }
+      std::sort(rank_col.begin(), rank_col.end(), rank_sort);
+      for (s=0; s<numSamples; ++s)
+        final_rank[rank_col[s]] = s;
+      for (s=0; s<numSamples; ++s) // can't be combined with loop above
+        sample1_ranks(v+numContinuousVars, s) = final_rank[s] + 1;
+    }
+    for (v=0; v<numDiscreteRealVars; ++v) {
+      for (size_t rank_count = 0; rank_count < numSamples; rank_count++){
+        rank_col[rank_count] = rank_count;
+        rawData[rank_count] = sample1real_values[rank_count][v];
+      }
+      std::sort(rank_col.begin(), rank_col.end(), rank_sort);
+      for (s=0; s<numSamples; ++s)
+        final_rank[rank_col[s]] = s;
+      for (s=0; s<numSamples; ++s) // can't be combined with loop above
+        sample1_ranks(v+numContinuousVars+numDiscreteIntVars, s) = final_rank[s] + 1;
+    }
 
 #ifdef DEBUG
     Cout << "rank1\n" << sample1_ranks << '\n';
 #endif //DEBUG
 
     // initialize the matrix which identifies whether the sample 2 rank should
-    // be switched or not
+    // be switched or not.  The switch matrix is only for continuous variables
     BoolDequeArray switch_ranks(numContinuousVars);
     for (v=0; v<numContinuousVars; ++v)
       switch_ranks[v] = BoolDeque(numSamples, false);
@@ -170,9 +204,9 @@ void NonDIncremLHSSampling::quantify_uncertainty()
 	  switch_ranks[v][index] = true;
       }
     }
-#ifdef DEBUG
-    Cout << "switch rank1\n" << switch_ranks << '\n';
-#endif //DEBUG
+//#ifdef DEBUG
+//    Cout << "switch rank1\n" << switch_ranks << '\n';
+//#endif //DEBUG
 
     varyPattern = true;
     //numSamples = previousSamples;
@@ -181,11 +215,34 @@ void NonDIncremLHSSampling::quantify_uncertainty()
     get_parameter_sets(iteratedModel);
     // store sample ranks returned by 2nd get_parameter_sets() call
     int num_samp2 = samplesRef - numSamples;
-    IntMatrix sample2_ranks(numContinuousVars, num_samp2, false);
+
+    IntMatrix sample2_ranks(numContinuousVars+numDiscreteIntVars+numDiscreteRealVars, num_samp2, false);
     for (s=0; s<num_samp2; ++s) {
       int* s2r_s = sample2_ranks[s]; Real* sR_s = sampleRanks[s];
       for (v=0; v<numContinuousVars; ++v)
 	s2r_s[v] = (int)std::floor(sR_s[v]+.5); // round to nearest integer
+    }
+    for (v=0; v<numDiscreteIntVars; ++v) {
+      for (size_t rank_count = 0; rank_count < num_samp2; rank_count++){
+        rank_col[rank_count] = rank_count;
+        rawData[rank_count] = allSamples[rank_count][v+numContinuousVars];
+      }
+      std::sort(rank_col.begin(), rank_col.end(), rank_sort);
+      for (s=0; s<num_samp2; ++s)
+        final_rank[rank_col[s]] = s;
+      for (s=0; s<num_samp2; ++s) // can't be combined with loop above
+        sample2_ranks(v+numContinuousVars, s) = final_rank[s] + 1;
+    }
+    for (v=0; v<numDiscreteRealVars; ++v) {
+      for (size_t rank_count = 0; rank_count < num_samp2; rank_count++){
+        rank_col[rank_count] = rank_count;
+        rawData[rank_count] = allSamples[rank_count][v+numContinuousVars+numDiscreteIntVars];
+      }
+      std::sort(rank_col.begin(), rank_col.end(), rank_sort);
+      for (s=0; s<num_samp2; ++s)
+        final_rank[rank_col[s]] = s;
+      for (s=0; s<num_samp2; ++s) // can't be combined with loop above
+        sample2_ranks(v+numContinuousVars+numDiscreteIntVars, s) = final_rank[s] + 1;
     }
 
 #ifdef DEBUG
@@ -194,7 +251,7 @@ void NonDIncremLHSSampling::quantify_uncertainty()
 #endif // DEBUG
 
     // calculate the combined ranks  
-    sampleRanks.shapeUninitialized(numContinuousVars, samplesRef);
+    sampleRanks.shapeUninitialized(numContinuousVars+numDiscreteIntVars+numDiscreteRealVars, samplesRef);
     for (s=0; s<numSamples; ++s)
       for (v=0; v<numContinuousVars; ++v) {
 	int rank1 = sample1_ranks(v,s), index = rank1 - 1,
@@ -207,7 +264,52 @@ void NonDIncremLHSSampling::quantify_uncertainty()
 	  comb_rank = (switch_ranks[v][index]) ? 2*rank2 : 2*rank2-1;
 	sampleRanks(v,s) = (Real)comb_rank;
       }
-
+    // Ranks for discrete variables.  In the discrete case, we don't switch, we just 
+    // calculate the overall ranking of the combined set 
+    rank_col.resize(samplesRef), final_rank.resize(samplesRef);
+    rawData.resize(samplesRef); 
+    if (numDiscreteIntVars > 0) {
+      for (v=0; v<numDiscreteIntVars; ++v) {
+        for (size_t rank_count = 0; rank_count < numSamples; rank_count++){
+          rank_col[rank_count] = rank_count;
+          rawData[rank_count] = sample1int_values[rank_count][v];
+        }
+        for (size_t rank_count = numSamples ; rank_count < samplesRef; rank_count++){
+          rank_col[rank_count] = rank_count;
+          rawData[rank_count] = allSamples[rank_count-numSamples][v+numContinuousVars];
+        }
+        std::sort(rank_col.begin(), rank_col.end(), rank_sort);
+        for (s=0; s<samplesRef; ++s)
+          final_rank[rank_col[s]] = s+1;
+        Cout << "final ranks " << final_rank << '\n';
+        Cout << "rawData " << rawData << '\n'; 
+        //combine_ranks_discrete(ave_ranks, sample1int_values);
+        for (s=0; s<samplesRef; ++s) // can't be combined with loop above
+          sampleRanks(v+numContinuousVars, s) = final_rank[s];
+      }
+    }
+    if (numDiscreteRealVars > 0) {
+      for (v=0; v<numDiscreteRealVars; ++v) {
+        for (size_t rank_count = 0; rank_count < numSamples; rank_count++){
+          rank_col[rank_count] = rank_count;
+          rawData[rank_count] = sample1real_values[rank_count][v];
+        }
+        for (size_t rank_count = numSamples ; rank_count < samplesRef; rank_count++){
+          rank_col[rank_count] = rank_count;
+          rawData[rank_count] = allSamples[rank_count-numSamples][v+numContinuousVars+numDiscreteIntVars];
+        }
+        std::sort(rank_col.begin(), rank_col.end(), rank_sort);
+        for (s=0; s<samplesRef; ++s)
+          final_rank[rank_col[s]] = s+1;
+        Cout << "final ranks " << final_rank << '\n';
+        Cout << "rawData " << rawData << '\n'; 
+        //combine_ranks_discrete(ave_ranks, sample1int_values);
+        for (s=0; s<samplesRef; ++s) // can't be combined with loop above
+          sampleRanks(v+numContinuousVars+numDiscreteIntVars, s) = final_rank[s];
+      }
+    }
+            
+            
 #ifdef DEBUG
     Cout << "\ncombined ranks\n" << sampleRanks;
 #endif //DEBUG
@@ -224,13 +326,22 @@ void NonDIncremLHSSampling::quantify_uncertainty()
 
 #ifdef DEBUG
     Cout << "rank_combined\n" << sampleRanks << '\n';// updated by SET_GET_RANKS
+    Cout << "First cut full sample set\n" ; 
+    write_data(Cout,allSamples,false,true,true); 
 #endif //DEBUG
+
   }
 
   // need to put the first set back  
   for (s=0; s<previousSamples; ++s)
     copy_data(sample1_values[s], allSamples[s], (int)numContinuousVars);
-  
+  for (s=0; s<previousSamples; ++s) {
+    for (size_t k=0; k<numDiscreteIntVars; ++k)
+      allSamples[s][numContinuousVars+k]=sample1int_values[s][k];
+    for (size_t k=0; k<numDiscreteRealVars; ++k)
+      allSamples[s][numContinuousVars+numDiscreteIntVars+k]=sample1real_values[s][k];
+  }
+
 #ifdef DEBUG
   numSamples = samplesRef;
   Cout <<"\nallSamples\n"; write_data(Cout, allSamples, false, true, true);
@@ -241,7 +352,6 @@ void NonDIncremLHSSampling::quantify_uncertainty()
   evaluate_parameter_sets(iteratedModel, true, false);
   compute_statistics(allSamples, allResponses);
 }
-
 
 void NonDIncremLHSSampling::print_results(std::ostream& s)
 {
