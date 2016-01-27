2b69bfe81b911a5286a3632fb2e1006e097893fb
Russell Hooper <rhoope@sandia.gov>
2016-01-26 17:08:40 -0700

    Fix CmakeLists.txt for teuchos and snapshot src/DakotaBuildInfo.cpp
    ... using a configure for a svn checkout of Dakota.

---
+++ src/DakotaBuildInfo.cpp
@@ -0,0 +1,8 @@
+// -- AUTO GENERATED FILE; DO NOT EDIT --
+        #include "DakotaBuildInfo.hpp"
+        std::string Dakota::DakotaBuildInfo::releaseNum="6.3+";
+        std::string Dakota::DakotaBuildInfo::subversionRev="3989";
+        std::string Dakota::DakotaBuildInfo::releaseDate="";
+        // ToDo: Gather SVN rev for key TPLs, e.g. pecos, surfpack, etc.
+        //       Lower priority - a post-5.4 release task
+        
