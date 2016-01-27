369d5548005cabab67b26cb3bd0feaa1da17c153
jasteph <jasteph@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2016-01-27 21:35:38 +0000

    Adding a couple of error levels to make convergence a bit clearer. Convergence wrt number of experiences still hard to see.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@3993 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- examples/training/201508/solutions/calibration/cantilever/gendata_cantilever.m
+++ examples/training/201508/solutions/calibration/cantilever/gendata_cantilever.m
@@ -1,24 +1,29 @@
 dnominal = [ 2.2786458300000001e+02 2.6133333299999998e+04 1.8042803500000000e+00 ];
 
-num_data = [1 5 10 50];
-rel_std_dev = 0.10;
-nl_str = '10';
+num_data = [1 5 10 50 100];
+rel_std_dev = [0.10 0.05 0.02 0.01 0.005 0.001];
+% padding needed b/c strings in an ordinary array must be the same length
+% cellstr will strip the spaces when it converts to a cell array
+nl_str = cellstr(['10 ';'5  ';'2  ';'1  ';'0.5';'0.1']);
 
 rng(201508);
 
-for exp_ind = 1:length(num_data),
+for err_ind = 1:length(nl_str),
 
-  num_exp = num_data(exp_ind);
-  data_mult = 1.0 + rel_std_dev*randn(num_exp, 3);
+	for exp_ind = 1:length(num_data),
 
-  noisy_data = data_mult .* repmat(dnominal, num_exp, 1);
+	  num_exp = num_data(exp_ind);
+	  data_mult = 1.0 + rel_std_dev(err_ind)*randn(num_exp, 3);
 
-save(['cant_exp_data.', nl_str, 'perc.', int2str(num_exp), '.dat'], 'noisy_data', '-ascii');
+	  noisy_data = data_mult .* repmat(dnominal, num_exp, 1);
 
-  %figure(1)
-  %for i=1:3,
-  %  subplot(1,3,i);
-  %  plot(1,dnominal(:,i),'o',1,noisy_data(:,i),'x');
-  %end
+	save(['cant_exp_data-', nl_str{err_ind}, 'perc-', int2str(num_exp), '.dat'], 'noisy_data', '-ascii');
 
+	  %figure(1)
+	  %for i=1:3,
+	  %  subplot(1,3,i);
+	  %  plot(1,dnominal(:,i),'o',1,noisy_data(:,i),'x');
+	  %end
+
+	end
 end
