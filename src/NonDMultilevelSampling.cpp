d7bbd8e408304c7846ff437b26c1844413a4ce8b
mseldre <mseldre@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2016-02-12 23:37:33 +0000

    Complete higher order moments for control variate Monte Carlo and add baseline.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@4038 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- src/NonDMultilevelSampling.cpp
+++ src/NonDMultilevelSampling.cpp
@@ -129,7 +129,6 @@ control_variate_mc(size_t lf_model_form, size_t hf_model_form,
 {
   iteratedModel.surrogate_model(lf_model_form, soln_level);
   iteratedModel.truth_model(hf_model_form, soln_level);
-
   Model& truth_model = iteratedModel.truth_model();
   Model& surr_model  = iteratedModel.surrogate_model();
 
@@ -138,14 +137,11 @@ control_variate_mc(size_t lf_model_form, size_t hf_model_form,
        hf_cost    = truth_model.solution_level_cost()[soln_level],
        cost_ratio = hf_cost / lf_cost;
   size_t qoi, iter = 0, N_lf, N_hf, delta_N_lf, delta_N_hf;
-  RealVector sum_L(numFunctions), sum_H(numFunctions),
-    sum_L2(numFunctions), sum_H2(numFunctions), sum_LH(numFunctions),
-    mean_L(numFunctions, false), mean_H(numFunctions, false),
-    var_L(numFunctions, false),  var_H(numFunctions, false),
-    cov_LH(numFunctions, false), rho2_LH(numFunctions, false);
-  Real lf_fn, hf_fn, mu_L, mu_H, cov, rho_sq, avg_eval_ratio, bias_corr;
-  IntRespMCIter r_it;
   
+  // ---------------------
+  // Process pilot Samples
+  // ---------------------
+
   // Initialize for pilot sample
   if      (pilotSamples.empty())     delta_N_lf = delta_N_hf = 100; // default
   else if (pilotSamples.size() == 1) delta_N_lf = delta_N_hf = pilotSamples[0];
@@ -157,8 +153,6 @@ control_variate_mc(size_t lf_model_form, size_t hf_model_form,
   }
   Cout << "\nCVMC pilot sample: LF = " << delta_N_lf << " HF = " << delta_N_hf
        << std::endl;
-
-  /////////////////////// SIMPLER ONE PASS TO START ////////////////////////////
 
   // set the number of current samples from the defined increment
   numSamples = std::min(delta_N_lf, delta_N_hf);// ignore excess for initial set
@@ -172,57 +166,91 @@ control_variate_mc(size_t lf_model_form, size_t hf_model_form,
   evaluate_parameter_sets(iteratedModel, true, false);
 
   // process allResponses: accumulate new samples for each qoi
+  Real lf_fn, hf_fn, lf_prod, hf_prod;
+  RealVector sum_L(numFunctions), sum_H(numFunctions), sum_LH(numFunctions),
+    sum_L2(numFunctions), sum_H2(numFunctions), sum_L2H2(numFunctions),
+    sum_L3(numFunctions), sum_H3(numFunctions), sum_L3H3(numFunctions),
+    sum_L4(numFunctions), sum_H4(numFunctions), sum_L4H4(numFunctions),
+    sum_L6(numFunctions), sum_L8(numFunctions);
+  IntRespMCIter r_it;
   for (r_it=allResponses.begin(); r_it!=allResponses.end(); ++r_it) {
     const RealVector& fn_vals = r_it->second.function_values();
     for (qoi=0; qoi<numFunctions; ++qoi) {
       // sum_* are running sums across all increments
-      lf_fn = fn_vals[qoi]; hf_fn = fn_vals[qoi+numFunctions];
-      sum_L[qoi]  += lf_fn; sum_L2[qoi] += lf_fn * lf_fn;
-      sum_H[qoi]  += hf_fn; sum_H2[qoi] += hf_fn * hf_fn;
-      sum_LH[qoi] += lf_fn * hf_fn;
+      lf_prod = lf_fn = fn_vals[qoi];
+      hf_prod = hf_fn = fn_vals[qoi+numFunctions];
+      sum_L[qoi] += lf_fn; sum_H[qoi] += hf_fn; sum_LH[qoi] += lf_fn * hf_fn;
+
+      lf_prod *= lf_fn; hf_prod *= hf_fn;
+      sum_L2[qoi] += lf_prod; sum_H2[qoi] += hf_prod;
+      sum_L2H2[qoi] += lf_prod * hf_prod;
+
+      lf_prod *= lf_fn; hf_prod *= hf_fn;
+      sum_L3[qoi] += lf_prod; sum_H3[qoi] += hf_prod; 
+      sum_L3H3[qoi] += lf_prod * hf_prod;
+
+      lf_prod *= lf_fn; hf_prod *= hf_fn;
+      sum_L4[qoi] += lf_prod; sum_H4[qoi] += hf_prod;
+      sum_L4H4[qoi] += lf_prod * hf_prod;
+
+      lf_prod *= lf_fn * lf_fn; sum_L6[qoi] += lf_prod;
+      lf_prod *= lf_fn * lf_fn; sum_L8[qoi] += lf_prod;
     }
   }
-  avg_eval_ratio = 0.;
-  bias_corr = 1./((Real)(numSamples - 1));
+  // compute & store subset of stats needed to compute avg_eval_ratio
+  RealVector mean_L(numFunctions, false), mean_L2(numFunctions, false),
+    mean_L3(numFunctions, false),  mean_L4(numFunctions, false),
+    mean_H(numFunctions, false),   mean_H2(numFunctions, false),
+    mean_H3(numFunctions, false),  mean_H4(numFunctions, false),
+    var_L(numFunctions, false),    var_L2(numFunctions, false),
+    var_L3(numFunctions, false),   var_L4(numFunctions, false),
+    var_H(numFunctions, false),    cov_LH(numFunctions, false),
+    cov_L2H2(numFunctions, false), cov_L3H3(numFunctions, false),
+    cov_L4H4(numFunctions, false), rho2_LH(numFunctions, false);
+  Real mu_L, mu_H, cov, rho_sq,
+    avg_eval_ratio = 0., bias_corr = 1./(numSamples - 1);
   for (qoi=0; qoi<numFunctions; ++qoi) {
     // unbiased mean estimator X-bar = 1/N * sum
-    mu_L = mean_L[qoi] = sum_L[qoi] / (Real)numSamples;
-    mu_H = mean_H[qoi] = sum_H[qoi] / (Real)numSamples;
+    mu_L = mean_L[qoi] = sum_L[qoi] / numSamples;
+    mu_H = mean_H[qoi] = sum_H[qoi] / numSamples;
     // unbiased sample variance estimator = 1/(N-1) sum[(X_i - X-bar)^2]
     // = 1/(N-1) ( sum[X^2_i] - N X-bar^2 ) where Bessel's correction = 1/(N-1)
     var_L[qoi] = (sum_L2[qoi] - numSamples * mu_L * mu_L) * bias_corr;
     var_H[qoi] = (sum_H2[qoi] - numSamples * mu_H * mu_H) * bias_corr;
     cov = cov_LH[qoi] = (sum_LH[qoi] - numSamples * mu_L * mu_H) * bias_corr;
 
-    /*
-    // Higher order stats: treat as E[Y] for Y=LF^k or Y=HF^k
-    mu_L2 = mean_L2[qoi] = sum_L2[qoi] / (Real)numSamples;
-    mu_H2 = mean_H2[qoi] = sum_H2[qoi] / (Real)numSamples;
-    var_L2[qoi] = (sum_L4[qoi] - numSamples * mu_L2 * mu_L2) * bias_corr;
-    var_H2[qoi] = (sum_H4[qoi] - numSamples * mu_H2 * mu_H2) * bias_corr;
-    cov_L2H2[qoi] = (sum_L2H2[qoi] - numSamples * mu_L2 * mu_H2) * bias_corr;
-
-    mu_L3 = mean_L3[qoi] = sum_L3[qoi] / (Real)numSamples;
-    mu_H3 = mean_H3[qoi] = sum_H3[qoi] / (Real)numSamples;
-    var_L3[qoi] = (sum_L6[qoi] - numSamples * mu_L3 * mu_L3) * bias_corr;
-    var_H3[qoi] = (sum_H6[qoi] - numSamples * mu_H3 * mu_H3) * bias_corr;
-    cov_L3H3[qoi] = (sum_L3H3[qoi] - numSamples * mu_L3 * mu_H3) * bias_corr;
-
-    mu_L4 = mean_L4[qoi] = sum_L4[qoi] / (Real)numSamples;
-    mu_H4 = mean_H4[qoi] = sum_H4[qoi] / (Real)numSamples;
-    var_L4[qoi] = (sum_L8[qoi] - numSamples * mu_L4 * mu_L4) * bias_corr;
-    var_H4[qoi] = (sum_H8[qoi] - numSamples * mu_H4 * mu_H4) * bias_corr;
-    cov_L4H4[qoi] = (sum_L4H4[qoi] - numSamples * mu_L4 * mu_H4) * bias_corr;
-    */
-
-    // compute evaluation ratio which determines increment for LF samples; the
-    // sample increment optimizes the total computational budget and is not
-    // treated as a worst case accuracy requirement --> use the QoI average.
+    // compute evaluation ratio which determines increment for LF samples
+    // > the sample increment optimizes the total computational budget and is
+    //   not treated as a worst case accuracy reqmt --> use the QoI average
+    // > refinement based only on QoI mean statistics
     rho_sq = rho2_LH[qoi] = cov / var_L[qoi] * cov / var_H[qoi]; // bias cancels
     avg_eval_ratio += (rho_sq >= 1.) ? maxFunctionEvals : // should not happen
       std::sqrt(cost_ratio * rho_sq / (1. - rho_sq));
+
+    // bookkeeping for initial higher order stats
+    mu_L = mean_L2[qoi] = sum_L2[qoi] / numSamples;
+    mu_H = mean_H2[qoi] = sum_H2[qoi] / numSamples;
+    var_L2[qoi] = (sum_L4[qoi] - numSamples * mu_L * mu_L) * bias_corr;
+    //var_H2[qoi] = (sum_H4[qoi] - numSamples * mu_H * mu_H) * bias_corr;
+    cov_L2H2[qoi] = (sum_L2H2[qoi] - numSamples * mu_L * mu_H) * bias_corr;
+
+    mu_L = mean_L3[qoi] = sum_L3[qoi] / numSamples;
+    mu_H = mean_H3[qoi] = sum_H3[qoi] / numSamples;
+    var_L3[qoi] = (sum_L6[qoi] - numSamples * mu_L * mu_L) * bias_corr;
+    //var_H3[qoi] = (sum_H6[qoi] - numSamples * mu_H * mu_H) * bias_corr;
+    cov_L3H3[qoi] = (sum_L3H3[qoi] - numSamples * mu_L * mu_H) * bias_corr;
+
+    mu_L = mean_L4[qoi] = sum_L4[qoi] / numSamples;
+    mu_H = mean_H4[qoi] = sum_H4[qoi] / numSamples;
+    var_L4[qoi] = (sum_L8[qoi] - numSamples * mu_L * mu_L) * bias_corr;
+    //var_H4[qoi] = (sum_H8[qoi] - numSamples * mu_H * mu_H) * bias_corr;
+    cov_L4H4[qoi] = (sum_L4H4[qoi] - numSamples * mu_L * mu_H) * bias_corr;
   }
   avg_eval_ratio /= numFunctions;
+
+  // --------------------
+  // Compute LF increment
+  // --------------------
 
   // update LF samples based on evaluation ratio
   // r = m/n -> m = r*n -> delta = m-n = (r-1)*n
@@ -248,17 +276,20 @@ control_variate_mc(size_t lf_model_form, size_t hf_model_form,
     const RealVector& fn_vals = r_it->second.function_values();
     for (qoi=0; qoi<numFunctions; ++qoi) {
       // sum_* are running sums across all sample increments
-      lf_fn = fn_vals[qoi];
-      sum_L[qoi] += lf_fn; sum_L2[qoi] += lf_fn * lf_fn;
+      lf_prod  = lf_fn = fn_vals[qoi]; sum_L[qoi] += lf_fn;
+      lf_prod *= lf_fn; sum_L2[qoi] += lf_prod;
+      lf_prod *= lf_fn; sum_L3[qoi] += lf_prod;
+      lf_prod *= lf_fn; sum_L4[qoi] += lf_prod;
     }
   }
 
-  // aggregate expected value of estimators for Y, Y^2, Y^3, and Y^4. ... These
+  // aggregate expected value of estimators for E[Y] for Y=LF^k or Y=HF^k. These
   // uncentered raw moment estimates are then converted to standardized moments.
   if (momentStats.empty()) momentStats.shape/*Uninitialized*/(4, numFunctions);
-  Real orig_cntl, orig_mu_L, orig_var_L, new_mu_L, new_var_L,
-    mc_mse, cvmc_mse, mse_ratio;
-  bias_corr = 1./((Real)(N_lf - 1));
+  Real mu_L2, mu_L3, mu_L4, refined_mu_L, refined_mu_L2, refined_mu_L3,
+    refined_mu_L4, est_H, est_H2, est_H3, est_H4, H_cntl, H2_cntl, H3_cntl,
+    H4_cntl, mc_mse, cvmc_mse, mse_ratio, cm2, cm3, cm4;
+  bias_corr = 1. / (N_lf - 1);
   for (qoi=0; qoi<numFunctions; ++qoi) {
     // Compute ratio of MSE for high fidelity MC and multifidelity CVMC
     // (just a diagnostic prior to iteration)
@@ -267,18 +298,41 @@ control_variate_mc(size_t lf_model_form, size_t hf_model_form,
     Cout << "Mean square error reduced from " << mc_mse << " to " << cvmc_mse
 	 << " (relative factor of " << mse_ratio << ")\n";
 
-    // original and updated sample mean for LF:
-    orig_mu_L = mean_L[qoi];
-    new_mu_L  = mean_L[qoi] = sum_L[qoi] / (Real)N_lf;
-    // compute and apply control for HF mean estimate
-    orig_cntl = cov_LH[qoi] / var_L[qoi];
-    momentStats(0,qoi) = mean_H[qoi] - orig_cntl * (orig_mu_L - new_mu_L);
-    // Initial cut reuses the same control value, instead of recomputing from
-    // cov_L2H2[qoi] / var_L2[qoi]
-    orig_var_L = var_L[qoi];
-    new_var_L  = var_L[qoi]
-      = (sum_L2[qoi] - N_lf * new_mu_L * new_mu_L) * bias_corr;
-    momentStats(1,qoi) =  var_H[qoi] - orig_cntl * (orig_var_L - new_var_L);
+    // from original data set:
+    mu_L  = mean_L[qoi];      H_cntl = cov_LH[qoi]   / var_L[qoi];
+    mu_L2 = mean_L2[qoi];    H2_cntl = cov_L2H2[qoi] / var_L2[qoi];
+    mu_L3 = mean_L3[qoi];    H3_cntl = cov_L3H3[qoi] / var_L3[qoi];
+    mu_L4 = mean_L4[qoi];    H4_cntl = cov_L4H4[qoi] / var_L4[qoi];
+
+    // updated:
+    refined_mu_L  = mean_L[qoi]  =  sum_L[qoi] / N_lf;
+    refined_mu_L2 = mean_L2[qoi] = sum_L2[qoi] / N_lf;
+    refined_mu_L3 = mean_L3[qoi] = sum_L3[qoi] / N_lf;
+    refined_mu_L4 = mean_L4[qoi] = sum_L4[qoi] / N_lf;
+    //var_L2[qoi] = (sum_L4[qoi] - numSamples * mu_L2 * mu_L2) * bias_corr;
+    //var_H2[qoi] = (sum_H4[qoi] - numSamples * mu_H2 * mu_H2) * bias_corr;
+    //cov_L2H2[qoi] = (sum_L2H2[qoi] - numSamples * mu_L2 * mu_H2) * bias_corr;
+    //var_L3 = (sum_L6[qoi] - numSamples * mu_L3 * mu_L3) * bias_corr;
+    //var_H3 = (sum_H6[qoi] - numSamples * mu_H3 * mu_H3) * bias_corr;
+    //cov_L3H3 = (sum_L3H3[qoi] - numSamples * mu_L3 * mu_H3) * bias_corr;
+    //var_L4 = (sum_L8[qoi] - numSamples * mu_L4 * mu_L4) * bias_corr;
+    //var_H4 = (sum_H8[qoi] - numSamples * mu_H4 * mu_H4) * bias_corr;
+    //cov_L4H4 = (sum_L4H4[qoi] - numSamples * mu_L4 * mu_H4) * bias_corr;
+
+    // apply control for HF estimates (mean, std dev, skewness, kurtosis)
+    est_H  = mean_H[qoi]  -  H_cntl * (mu_L  - refined_mu_L);
+    est_H2 = mean_H2[qoi] - H2_cntl * (mu_L2 - refined_mu_L2);
+    est_H3 = mean_H3[qoi] - H3_cntl * (mu_L3 - refined_mu_L3);
+    est_H4 = mean_H4[qoi] - H4_cntl * (mu_L4 - refined_mu_L4);
+    // convert from uncentered to centered moments
+    cm2 = est_H2 - est_H*est_H; // variance
+    cm3 = est_H3 - est_H*(3.*cm2 + est_H*est_H);
+    cm4 = est_H4 - est_H*(4.*cm3 + est_H*(6.*cm2 + est_H*est_H));
+    // convert from centered to standardized moments
+    momentStats(0,qoi) = est_H;                    // mean
+    momentStats(1,qoi) = std::sqrt(cm2);           // std deviation
+    momentStats(2,qoi) = cm3 / std::pow(cm2, 1.5); // skewness
+    momentStats(3,qoi) = cm4 / (cm2 * cm2) - 3.;   // excess kurtosis
   }
   Cout << '\n';
 
@@ -532,6 +586,7 @@ void NonDMultilevelSampling::multilevel_mc(size_t model_form)
   // aggregate expected value of estimators for Y, Y^2, Y^3, and Y^4. Final
   // expected value result is sum of expected values from telescopic sum. These
   // uncentered raw moment estimates are then converted to standardized moments.
+  // There is no bias correction for small sample sizes as in NonDSampling::compute_moments().
   if (momentStats.empty()) momentStats.shapeUninitialized(4, numFunctions);
   for (qoi=0; qoi<numFunctions; ++qoi) {
     Real mu_Y1 = 0., mu_Y2 = 0., mu_Y3 = 0., mu_Y4 = 0.;
@@ -540,14 +595,14 @@ void NonDMultilevelSampling::multilevel_mc(size_t model_form)
       mu_Y1 += sum_Y1(qoi,lev) / Nl; mu_Y2 += sum_Y2(qoi,lev) / Nl;
       mu_Y3 += sum_Y3(qoi,lev) / Nl; mu_Y4 += sum_Y4(qoi,lev) / Nl;
     }
-    // convert uncentered raw moments to standardized moments. There is no bias
-    // correction for small sample sizes as in NonDSampling::compute_moments().
-    Real cm2 = mu_Y2 - mu_Y1*mu_Y1, cm3 = mu_Y3 - mu_Y1*(3.*cm2 + mu_Y1*mu_Y1);
-    momentStats(0,qoi) = mu_Y1;
-    momentStats(1,qoi) = std::sqrt(cm2);
-    momentStats(2,qoi) = cm3 / std::pow(cm2, 1.5);
-    momentStats(3,qoi) = (mu_Y4 - mu_Y1*(4.*cm3 + mu_Y1*(6.*cm2 + mu_Y1*mu_Y1)))
-                       / (cm2 * cm2) - 3.; // excess kurtosis
+    // convert from uncentered to centered moments
+    Real cm2 = mu_Y2 - mu_Y1*mu_Y1, cm3 = mu_Y3 - mu_Y1*(3.*cm2 + mu_Y1*mu_Y1),
+         cm4 = mu_Y4 - mu_Y1*(4.*cm3 + mu_Y1*(6.*cm2 + mu_Y1*mu_Y1));
+    // convert from centered to standardized moments
+    momentStats(0,qoi) = mu_Y1;                    // mean
+    momentStats(1,qoi) = std::sqrt(cm2);           // std deviation
+    momentStats(2,qoi) = cm3 / std::pow(cm2, 1.5); // skewness
+    momentStats(3,qoi) = cm4 / (cm2 * cm2) - 3.;   // excess kurtosis
   }
 }
 
