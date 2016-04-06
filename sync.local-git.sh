b406886aa3088277cf215f6136c249c7e87150a4
Russell Hooper <rhoope@sandia.gov>
2016-04-06 15:03:17 -0600

    Dakota git transition
     - Add optional -u to sync-local-git.sh script to provide a way to
       update the sha1 in external.local to the most recent version in
       origin/master

--- sync.local-git.sh
+++ sync.local-git.sh
@@ -2,8 +2,11 @@
 # Clone SRN repos
 git clone --recursive development.sandia.gov:/git/dakota local
 cd local
-#git log --pretty=format:"%H" -1 > ../external.local
-if [ -f ../external.local ]; then
+if [ $# == 1 ] && [ $1 == "-u" ]; then
+  git checkout master
+  git submodule update --init
+  git log --pretty=format:"%H" -1 > ../external.local
+elif [ -f ../external.local ]; then
   git checkout `cat ../external.local`
   git submodule update --init
 fi
