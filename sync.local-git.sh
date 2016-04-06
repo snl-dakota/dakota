e319077cfda56c38470e5969a75cffc1aa26089c
Russell Hooper <rhoope@sandia.gov>
2016-04-06 14:42:50 -0600

    Dakota git transition
     - changed the sync.local-git.sh script to clone local with submodules
       and then reset HEAD to the last stable version (indicated in external.local)
       instead of updating the sha1 contained in external.local to the most current
       version of local
    
    This is to support Jenkins testing and creation of distibution tarballs that
    are synchronized between core and local content.

--- sync.local-git.sh
+++ sync.local-git.sh
@@ -2,5 +2,9 @@
 # Clone SRN repos
 git clone --recursive development.sandia.gov:/git/dakota local
 cd local
-git log --pretty=format:"%H" -1 > ../external.local
+#git log --pretty=format:"%H" -1 > ../external.local
+if [ -f ../external.local ]; then
+  git checkout `cat ../external.local`
+  git submodule update --init
+fi
 cd -
