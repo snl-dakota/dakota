9620372a958b225bec6dc383622fd1bf9a4d9878
jamonsc <jamonsc@aa0779c3-6ec2-4b84-a212-a405c134cd8d>
2016-05-06 18:58:16 +0000

    The steady_state_diffusion_1d error checking should only be active for exponential kernels. This fixes the previous problem with failing tests.
    git-svn-id: svn+ssh://software.sandia.gov/svn/dakota/trunk@4261 aa0779c3-6ec2-4b84-a212-a405c134cd8d

--- src/TestDriverInterface.cpp
+++ src/TestDriverInterface.cpp
@@ -1545,15 +1545,16 @@ int TestDriverInterface::steady_state_diffusion_1d()
     abort_handler(INTERFACE_ERROR);
   }
 
-  if (order + 1 < xC.length()) {
-    Cerr << "Error: Mesh size must be greater than or equal "
-         << " to the number of random variables + 1." << std::endl;
-    abort_handler(INTERFACE_ERROR);
-  }
-
   // Get the kernel specification from the discrete string variables
   size_t kernel_index = find_index(xDSLabels, "kernel_type");
   String kernel = ( kernel_index == _NPOS ) ? "default" : xDS[kernel_index];
+
+  if (order + 1 < xC.length() && kernel == "exponential") {
+    Cerr << "Error: Mesh size must be greater than or equal "
+         << "to the number of random variables + 1 when using "
+         << "the exponential kernel." << std::endl;
+    abort_handler(INTERFACE_ERROR);
+  }
 
   // Get the positivity flag from the discrete string variables
   size_t pos_index = find_index(xDSLabels, "positivity");
