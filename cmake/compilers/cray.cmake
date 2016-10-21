b68866ce06076b473cc02845303a3b624f6daa38
J. Adam Stephens <jasteph@sandia.gov>
2016-10-21 13:46:55 -0600

    Put compiler settings in cache

--- cmake/compilers/cray.cmake
+++ cmake/compilers/cray.cmake
@@ -6,6 +6,6 @@
 
 set( CTEST_BUILD_NAME "${CTEST_BUILD_NAME}_cray" )
 
-set(CMAKE_C_COMPILER cc)
-set(CMAKE_CXX_COMPILER CC)
-set(CMAKE_Fortran_COMPILER ftn)
+set(CMAKE_C_COMPILER "cc" CACHE FILEPATH "Cray C compiler")
+set(CMAKE_CXX_COMPILER "CC" CACHE FILEPATH "Cray C++ compiler"  )
+set(CMAKE_Fortran_COMPILER "ftn" CACHE FILEPATH "Cray Fortran compiler" )
