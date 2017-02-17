d07b40ce94fc5eaacafe8d5a08f2c635dc04202e
Russell Hooper <rhoope@sandia.gov>
2017-02-17 10:00:02 -0700

    DAK-1309: change comment to capture clearer understanding of initialization values

--- src/NLPQLPOptimizer.cpp
+++ src/NLPQLPOptimizer.cpp
@@ -218,8 +218,7 @@ void NLPQLPOptimizer::allocate_workspace()
   ACTIVE = new  int [LACTIVE];
   // Need to initialize this data to avoid valgrind errors (TPL should do it but doesn't)
   for( int i=0; i<LACTIVE; ++i )
-    ACTIVE[i] = 1; // It is not clear what value(s) should be used to initialize this data,
-                   // eg what options the NLPQL solver should use by default. - RWH
+    ACTIVE[i] = 1; // These need to be non-zero so that all constraints are active. - RWH
 }
 
 
