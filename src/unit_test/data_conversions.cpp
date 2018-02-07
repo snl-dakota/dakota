93b052f4bf4dad900675a8dc45058bae786295fd
Russell Hooper <rhoope@sandia.gov>
2018-02-07 13:58:48 -0700

    DAK-1966: adjoint Jacobian for constraints
     - add some basic unit testing of the underlying utilities,
       apply_matrix and apply_matrix_transpose

--- src/unit_test/data_conversions.cpp
+++ src/unit_test/data_conversions.cpp
@@ -50,4 +50,123 @@ TEUCHOS_UNIT_TEST(data_conversion, rva2rm)
     for( int j=0; j<NCOLS; ++j )
     TEST_FLOATING_EQUALITY( test_rva[i][j], test_rm(i,j), 1.e-14 );
 }
+
+//----------------------------------------------------------------
+
+TEUCHOS_UNIT_TEST(data_conversion, apply_matrix)
+{
+  const int NROWS = 5;
+  const int NCOLS = 3;
+  RealMatrix mat(NROWS, NCOLS);
+  mat.random();
+
+  std::vector<Real> v1(NCOLS, 1.0);
+  std::vector<Real> v2;
+
+  /////////////////  What we want to test
+  apply_matrix(mat, v1, v2);
+  /////////////////  What we want to test
+
+  // Verify correct dimensions
+  TEST_EQUALITY( NROWS, v2.size() );
+
+  // Verify correct values
+  Real sum = 0.0;
+  for( size_t i=0; i<NROWS; ++i )
+  {
+    sum = 0.0;
+    for( int j=0; j<NCOLS; ++j )
+      sum += mat(i,j);
+    TEST_FLOATING_EQUALITY( sum, v2[i], 1.e-14 );
+  }
+
+  // Now test partial behavior
+
+  std::vector<Real> v3(2*NCOLS, 2.0);
+  std::vector<Real> v4(2*NROWS);
+
+  for( size_t i=0; i<v4.size(); ++i )
+    v4[i] = (Real)i;
+
+  /////////////////  What we want to test
+  apply_matrix(mat, v3, v4);
+  /////////////////  What we want to test
+
+  // Verify correct dimensions
+  TEST_EQUALITY( 2*NROWS, v4.size() );
+
+  // Verify correct values
+  for( size_t i=0; i<v4.size(); ++i )
+  {
+    if( i<NROWS )
+    {
+      sum = 0.0;
+      for( int j=0; j<NCOLS; ++j )
+        sum += mat(i,j);
+      TEST_FLOATING_EQUALITY( 2.0*sum, v4[i], 1.e-14 );
+    }
+    else
+      TEST_FLOATING_EQUALITY( (Real)i, v4[i], 1.e-14 );
+  }
+}
+
+//----------------------------------------------------------------
+
+TEUCHOS_UNIT_TEST(data_conversion, apply_matrix_transpose)
+{
+  const int NROWS = 5;
+  const int NCOLS = 3;
+  RealMatrix mat(NROWS, NCOLS);
+  mat.random();
+
+  std::vector<Real> v1(NROWS, 1.0);
+  std::vector<Real> v2;
+
+  /////////////////  What we want to test
+  apply_matrix_transpose(mat, v1, v2);
+  /////////////////  What we want to test
+
+  // Verify correct dimensions
+  TEST_EQUALITY( NCOLS, v2.size() );
+
+  // Verify correct values
+  Real sum = 0.0;
+  for( size_t i=0; i<NCOLS; ++i )
+  {
+    sum = 0.0;
+    for( int j=0; j<NROWS; ++j )
+      sum += mat(j,i);
+    TEST_FLOATING_EQUALITY( sum, v2[i], 1.e-14 );
+  }
+
+  // Now test partial behavior
+
+  std::vector<Real> v3(2*NROWS, 2.0);
+  std::vector<Real> v4(2*NCOLS);
+
+  for( size_t i=0; i<v4.size(); ++i )
+    v4[i] = (Real)i;
+
+  /////////////////  What we want to test
+  apply_matrix_transpose(mat, v3, v4);
+  /////////////////  What we want to test
+
+  // Verify correct dimensions
+  TEST_EQUALITY( 2*NCOLS, v4.size() );
+
+  // Verify correct values
+  for( size_t i=0; i<v4.size(); ++i )
+  {
+    if( i<NCOLS )
+    {
+      sum = 0.0;
+      for( int j=0; j<NROWS; ++j )
+        sum += mat(j,i);
+      TEST_FLOATING_EQUALITY( 2.0*sum, v4[i], 1.e-14 );
+    }
+    else
+      TEST_FLOATING_EQUALITY( (Real)i, v4[i], 1.e-14 );
+  }
+}
+
 //----------------------------------------------------------------
