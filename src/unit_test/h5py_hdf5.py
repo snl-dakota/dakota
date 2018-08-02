646c43871852646713fd09c19b71c00aa901e304
J. Adam Stephens <jasteph@sandia.gov>
2018-08-02 14:56:02 -0600

    DAK-2345: H5py tests - initial commit
    Not functioning

---
+++ src/unit_test/h5py_hdf5.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import subprocess
+import unittest
+import h5py
+
+TEST_NAME = "dakota_hdf5_test.in"
+
+# This test must:
+# 1. extract various results from console output
+# 2. compare them to results in an hdf5 file
+
+def extract_moments(lines):
+
+
+class HDF5Test(unittest.TestCase):
+
+    def test_moments(self):
+        
