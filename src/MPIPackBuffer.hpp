0778fcfc0290e80b7cad7540fabec6faa82e7324
Bill Bohnhoff <wjbohnh@sandia.gov>
2018-08-28 22:38:19 -0600

    DAK-2348: Fix for compiler errors Windows MSVC 19.0.x 64-bit
    Problematic MPIPackBuffer utility templates are no longer compiled using
    Windows MSVC since MPI is not enabled on the Windows platform.

--- src/MPIPackBuffer.hpp
+++ src/MPIPackBuffer.hpp
@@ -291,7 +291,7 @@ inline MPIUnpackBuffer& operator>> (MPIUnpackBuffer& buff, bool& data)
 
 
 /// Read a generic container (vector<T>, list<T>) from MPIUnpackBuffer, s
-//  WJB - ToDo: consider std::set<T> too (currently in data_io.hpp)
+#ifdef DAKOTA_HAVE_MPI
 template <class ContainerT>
 inline void container_read(ContainerT& c, MPIUnpackBuffer& s)
 {
@@ -317,6 +317,7 @@ inline void container_write(const ContainerT& c, MPIPackBuffer& s)
     s << entry;
   }
 }
+#endif
 
 
 /// stream insertion for BitArray
@@ -360,12 +361,20 @@ operator>>(MPIUnpackBuffer& s, boost::dynamic_bitset<Block, Allocator>& bs)
 /// global MPIUnpackBuffer extraction operator for generic container
 template <class ContainerT>
 inline MPIUnpackBuffer& operator>>(MPIUnpackBuffer& s, ContainerT& data)
+#ifdef DAKOTA_HAVE_MPI
 { container_read(data, s); return s; }
+#else
+{  return s; }
+#endif
 
 /// global MPIPackBuffer insertion operator for generic container
 template <class ContainerT>
 inline MPIPackBuffer& operator<<(MPIPackBuffer& s, const ContainerT& data)
+#ifdef DAKOTA_HAVE_MPI
 { container_write(data, s); return s; }
+#else
+{  return s; }
+#endif
 
 //---------------------------------------------------------------------
 //
