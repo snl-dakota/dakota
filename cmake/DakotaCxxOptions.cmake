8f47bcbf186b30fcb47dad05ecf7e9bbf6b0e641
Brian M. Adams <briadam@sandia.gov>
2020-04-20 16:53:16 -0600

    Remove warnings about C+11 for cmake < 3.10
    CMake 3.10 knows about C++11 compiler features for all compilers we
    care about, or at least those Dakota was warning about. So remove the
    unused code.

--- cmake/DakotaCxxOptions.cmake
+++ cmake/DakotaCxxOptions.cmake
@@ -20,42 +20,6 @@ macro(dakota_cxx_standard)
 endmacro()
 
 
-# Warn if we know the CMake version in use doesn't know how to add
-# compiler flags for C++11
-macro(dakota_check_cxx11)
-
-  # Based on CMake versions suppporting language feature, this is our
-  # best guess at support...
-  # CMake 3.1           Clang, GNU
-  # CMake 3.2           AppleClang, Clang, GNU, SunPro
-  # CMake 3.3 -- 3.5    AppleClang, Clang, GNU, MSVC, SunPro
-  # CMake 3.6 -- 3.10   AppleClang, Clang, GNU, Intel, MSVC, SunPro
-
-  # Compilers for which compiler features are supported by CMake 3.1
-  set(known_cxx11_compilers Clang GNU)
-  # CMake 3.1 doesn't have VERSION_GREATER_EQUAL, so we do NOT VERSION_LESS
-  if(NOT CMAKE_VERSION VERSION_LESS 3.2)
-    # Additional compilers supported by 3.2
-    list(APPEND known_cxx11_compilers AppleClang SunPro)
-  endif()
-  if(NOT CMAKE_VERSION VERSION_LESS 3.3)
-    # Additional compilers supported by 3.3
-    list(APPEND known_cxx11_compilers MSVC)
-  endif()
-  if(NOT CMAKE_VERSION VERSION_LESS 3.6)
-    # Additional compilers supported by 3.6
-    list(APPEND known_cxx11_compilers Intel)
-  endif()
-
-  list(FIND known_cxx11_compilers ${CMAKE_CXX_COMPILER_ID} cxx11_compiler_found)
-  if(cxx11_compiler_found EQUAL -1)
-    message(WARNING "CMake version ${CMAKE_VERSION} may not set "
-      "${CMAKE_CXX_COMPILER_ID} compiler flags for C++11.  It may be necessary "
-      "to add appropriate CMAKE_CXX_FLAGS, such as -std=c++11.")
-  endif()
-
-endmacro()
-
 # When compiling with GCC or Clang, enable stricter warnings on a per-target basis
 # Intel supports GCC -W options, but doesn't document which, so not enabling for now
 macro(dakota_strict_warnings target_name)
