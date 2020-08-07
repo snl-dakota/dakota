2edd6f4f15a751987b0b9ccef3da4e342d8ab7c1
Brian M. Adams <briadam@sandia.gov>
2020-08-07 14:31:31 -0600

    Surrogate Python module: comments on JAS feedback

--- src/surrogates/unit/test_surrogate.py
+++ src/surrogates/unit/test_surrogate.py
@@ -15,7 +15,22 @@ for i, x in enumerate(build_samples):
 
 # This doesn't work due to the way NumPy is passed to Eigen (by copy)
 # Working now with return value function
+
+
 pr = dakmod.PolynomialRegression(build_samples, build_response, {"max degree" : 2, "scaler type" : "none"})
+# JAS: keyword arguments would be more common than dict (and have to when providing defaults)
+# foo(pos1, pos2, i=4, j=5)
+# foo(P, Q, 3, 2)
+# foo(pos2=Q, pos1=P, i=3, j=2)
+##pr = dakmod.PolynomialRegression(build_samples, build_response,
+##                                 maxdegree = 2, scalertype = None)
+
+# JAS: Would be nice to call pr.fit() like sklearn...
+#      Causes a nice separation between what's different per model being in ctor
+#      vs. build which is generic
+##pr = dakmod.PolynomialRegression({"max degree" : 2, "scaler type" : "none"})
+##pr.build(build_samples, build_response);
+
 
 eval_samples = -1.0 + 2*np.random.random((10,2))
 eval_truth = np.zeros(10)
@@ -37,12 +52,21 @@ print("Surrogate Hessian:\n{0}\n".format(eval_hessian))
 
 
 # Save Poly to text and then reload
+# JAS: Save could be free function and a member on the model
+# JAS: Could we enable Python pickling by delegating to C++ serialization
 print("Saving Polynomial")
 dakmod.save_poly(pr, "poly.txt", False)
 
 # Demonstrate potential more Pythonic way to construct from file
 print("Loading Polynomial")
 prload = dakmod.PolynomialRegression(filename = "poly.txt", binary = False)
+
+# JAS: consider more generic load function for arbitrary surrogate that casts to right type
+#      could enable loading vector of surrogates of varying types and evaluate them
+# JAS: could we always save/load to binary? text is portable, still not human readable
+# DTS: What happens if you have pointer to Base, but some functions
+#      available on one derived type but not another
+##surrload = load("filename.txt", False)
 
 assert(np.allclose(pr.value(dpoint), prload.value(dpoint)))
 
