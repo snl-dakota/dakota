2518e96fc81ddf8df3400e920b26a75e25e2c04b
Brian M. Adams <briadam@sandia.gov>
2020-09-25 15:57:40 -0600

    DP-333: Cleanup Python options, use modern CMake
    Use new FindPython available in CMake 3.12 or later (but require 3.14
    due to use of NumPy component). Flexing between Python versions for
    simple cases can be achieved by setting Python_EXECUTABLE.
    
    Rework Dakota's Python options, for now to these proposed ones:
      DAKOTA_PYTHON (historically controlled linked/direct interface only)
      DAKOTA_PYTHON_DIRECT_INTERFACE
      DAKOTA_PYTHON_DIRECT_INTERFACE_NUMPY (formerly DAKOTA_PYTHON_NUMPY)
      ##DNE yet: DAKOTA_PYTHON_TOP_INTERFACE
      DAKOTA_PYTHON_SURROGATES (requires PYBIND11)
    
    Additional pass needed for other directories using Python features

--- cmake/DakotaOptions.cmake
+++ cmake/DakotaOptions.cmake
@@ -62,6 +62,29 @@ if(ENABLE_DAKOTA_DOCS AND NOT UNIX)
     "DAKOTA documentation build only available on UNIX platforms")
 endif()
 
+
+## Python options
+
+# Scripts and tests based on interpreter
+# NOTE: Historically controlled linked/direct Python interface only
+option(DAKOTA_PYTHON "Dakota Python scripts (Interpreter); default ON" ON)
+
+# ??? Better name: LINKED_INTERFACE; other???
+option(DAKOTA_PYTHON_DIRECT_INTERFACE
+  "Dakota Python direct interface (Development); default OFF" OFF)
+# Formerly called DAKOTA_PYTHON_NUMPY
+option(DAKOTA_PYTHON_DIRECT_INTERFACE_NUMPY
+  "Dakota Python direct interface uses NumPy; default ON)" ON)
+
+# DNE yet
+##option(DAKOTA_PYTHON_TOP_INTERFACE "Top-level Dakota interface (DNE)" OFF)
+
+# Requires Pybind11
+option(DAKOTA_PYTHON_SURROGATES
+  "Dakota Python interface to surrogates module; default ON when Python enabled"
+  ON
+  )
+
 # Option to build an unsupported Java wrapper for Dakota's library mode that 
 # has a Java callback for performing function evaluations.  This is a non-
 # functioning capability that should only be enabled by developers.  It does 
