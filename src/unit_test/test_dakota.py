a7eacbd2dc9896225b4fd3e9ea8ef9686ce2dc7c
Russell Hooper <rhoope@sandia.gov>
2020-12-22 17:04:40 -0700

    DP-372 : add python callback demo
     - implements a simple text_book driver
     - outputs to hdf5 and checks best vars and resp

--- src/unit_test/test_dakota.py
+++ src/unit_test/test_dakota.py
@@ -9,6 +9,7 @@
 
 # Tests of top-level Dakota Python interface
 
+import h5py
 import sys
 #import numpy as np
 
@@ -16,6 +17,37 @@ import sys
 if len(sys.argv) > 1:
     dakpy_lib_path = sys.argv[1]
     sys.path.append(dakpy_lib_path)
+
+
+def text_book(**kwargs):
+
+    num_fns = kwargs['functions']
+    x = kwargs['cv']
+    ASV = kwargs['asv']
+
+    retval = dict([])
+
+    if (ASV[0] & 1): # **** f:
+        fn = 0.0
+        for val in x:
+            fn += pow(val-1.0, 4)
+        f = [fn]
+        retval['fns'] = f
+
+    # The gradient format causes Dakota to complain that the matrix must have 1 row ...
+    #   I think it does. ? 
+    if (ASV[0] & 2): # **** df/dx:
+        g = []
+        for val in x:
+            g.append( 4.0*pow(val-1.0, 3) )
+        retval['fnGrads'] = g
+        print(g)
+
+    if (ASV[0] & 4): # **** d^2f/dx^2:
+        raise("Hessians not currently supported for this driver.")
+
+    return(retval)
+
 
 import dakpy
 
@@ -31,25 +63,29 @@ def test_cmd():
 # This should be a better path
 def test_lib():
     text_book_input = """
-method,
-  output silent
-  max_function_evaluations 500
-  ncsu_direct
-    min_boxsize_limit 1.e-20
-    volume_boxsize_limit 1.e-20
-variables,
-  continuous_design = 3
-    initial_point  -2.3    2.3   0.23
-    upper_bounds  10.0   10.0  10.0
-    lower_bounds     -10.0  -10.0 -10.0
-    descriptors 'x1'  'x2'  'x3'
-interface,
-  direct
-    analysis_driver = 'text_book'
-responses,
-  num_objective_functions = 1
-  no_gradients
-  no_hessians
+        environment,
+          results_output
+            hdf5
+            results_output_file = 'test.dakota'
+        method,
+          output silent
+          max_function_evaluations 500
+          ncsu_direct
+            min_boxsize_limit 1.e-20
+            volume_boxsize_limit 1.e-20
+        variables,
+          continuous_design = 3
+            initial_point  -2.3    2.3   0.23
+            upper_bounds   10.0   10.0   10.0
+            lower_bounds  -10.0  -10.0  -10.0
+            descriptors     'x1'  'x2'   'x3'
+        interface,
+          python
+            analysis_driver = 'test_dakota:text_book'
+        responses,
+          num_objective_functions = 1
+          no_gradients
+          no_hessians
 """
     print("\n+++ Constructing LibEnv...\n")
     daklib = dakpy.LibEnv(input = "", input_string = text_book_input)
@@ -61,19 +97,31 @@ responses,
     print("\tUsing wrapped objs: "+str(resp_res.function_value(0)))
     assert(resp_res.function_value(0) < 1.e-20)
     vars_res = daklib.variables_results()
-    print("\tNumber active continuous variables: "+str(vars_res.num_active_cv()))
+    #print("\tNumber active continuous variables: "+str(vars_res.num_active_cv()))
     assert(vars_res.num_active_cv() == 3)
-    # Requires numpy
+    # --- Requires numpy
     #dak_vars = dakpy.get_variable_values_np(daklib)
     #print(dak_vars)
+    # --- Default: uses python arrays
     dak_vars2 = dakpy.get_variable_values(daklib)
-    print(dak_vars2)
+    #print(dak_vars2)
     target = 1.0;
     max_tol = 1.e-4;
     assert(abs((dak_vars2[0] - target)/target) < max_tol)
     assert(abs((dak_vars2[1] - target)/target) < max_tol)
     assert(abs((dak_vars2[2] - target)/target) < max_tol)
     print("\n+++ Done LibEnv.\n")
+
+    # Test values written to the h5 file
+    with h5py.File("test.dakota.h5", "r") as h:
+        hresps = h["/methods/NO_METHOD_ID/results/execution:1/best_objective_functions"]
+        hvars =  h["/methods/NO_METHOD_ID/results/execution:1/best_parameters/continuous"]
+        print(hresps.value)
+        print(hvars.value)
+        assert(hresps.value[0] < 1.e-20)
+        assert(abs((hvars.value[0] - target)/target) < max_tol)
+        assert(abs((hvars.value[1] - target)/target) < max_tol)
+        assert(abs((hvars.value[2] - target)/target) < max_tol)
 
 
 if __name__ == "__main__":
