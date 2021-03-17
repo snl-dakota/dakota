f95c36605d4ffa89429256dce5e4305cede5a4f4
Brian M. Adams <briadam@sandia.gov>
2021-03-17 11:03:14 -0600

    Surrogates Java: Unit test depends on generated surrogate
    DP-430: Instead of a statically checked-in model file, the surrogates
    JNI unit test now depends on a surrogate exported by the polynomial
    regression unit test.

--- src/surrogates/java/gov.sandia.dakota.jni.test/src/gov/sandia/dakota/jni/test/DakotaJniTest.java
+++ src/surrogates/java/gov.sandia.dakota.jni.test/src/gov/sandia/dakota/jni/test/DakotaJniTest.java
@@ -22,6 +22,11 @@ public class DakotaJniTest {
 		if(message != null && !message.isEmpty()) {
 			throw new DakotaSurrogatesException(message);
 		}
+
+		message = instance.testGetValue(dakotaSurrogates);
+		if(message != null && !message.isEmpty()) {
+			throw new DakotaSurrogatesException(message);
+		}
 		
 		System.out.println("Dakota JNI interface was tested successfully!");
 	}
@@ -31,19 +36,13 @@ public class DakotaJniTest {
 		
 		//URL testDataFileUrl = DakotaJniTest.class.getResource("/surrogate/exp_poly.s12.response_fn_1.txt");
 		//String surrogateFileAbsPath = new File(testDataFileUrl.getFile()).getAbsolutePath();		
-		String surrogateFileAbsPath = new File("exp_poly.s12.response_fn_1.txt").getAbsolutePath();		
+		String surrogateFileAbsPath = new File("../unit/poly_test.surr.bin").getAbsolutePath();		
 		
 		List<String> variableLabels = dakotaSurrogates.getVariableLabels(surrogateFileAbsPath);
 		
-		if(variableLabels.size() != 8) errorMessage = "testGetVariableLabels failed - Expected 8 variable labels, but got " + variableLabels.size();
-		if(!variableLabels.contains("u4")) errorMessage = "testGetVariableLabels failed - Variable label list does not contain u4";
-		if(!variableLabels.contains("u5")) errorMessage = "testGetVariableLabels failed - Variable label list does not contain u5";
-		if(!variableLabels.contains("d1")) errorMessage = "testGetVariableLabels failed - Variable label list does not contain d1";
-		if(!variableLabels.contains("d2")) errorMessage = "testGetVariableLabels failed - Variable label list does not contain d2";
-		if(!variableLabels.contains("u6")) errorMessage = "testGetVariableLabels failed - Variable label list does not contain u6";
-		if(!variableLabels.contains("s7")) errorMessage = "testGetVariableLabels failed - Variable label list does not contain s7";
-		if(!variableLabels.contains("d3")) errorMessage = "testGetVariableLabels failed - Variable label list does not contain d3";
-		if(!variableLabels.contains("s8")) errorMessage = "testGetVariableLabels failed - Variable label list does not contain s8";
+		if(variableLabels.size() != 2) errorMessage += "\ntestGetVariableLabels failed - Expected 2 variable labels, but got " + variableLabels.size();
+		if(!variableLabels.contains("poly_x1")) errorMessage += "\ntestGetVariableLabels failed - Variable label list does not contain poly_x1";
+		if(!variableLabels.contains("poly_x2")) errorMessage += "\ntestGetVariableLabels failed - Variable label list does not contain poly_x2";
 		
 		return errorMessage;
 	}
@@ -53,12 +52,13 @@ public class DakotaJniTest {
 		
 		//URL testDataFileUrl = DakotaJniTest.class.getResource("/surrogate/exp_poly.s12.response_fn_1.txt");
 		//String surrogateFileAbsPath = new File(testDataFileUrl.getFile()).getAbsolutePath();		
-		String surrogateFileAbsPath = new File("exp_poly.s12.response_fn_1.txt").getAbsolutePath();		
+		// also test the .txt file as should be the same
+		String surrogateFileAbsPath = new File("../unit/poly_test.surr.txt").getAbsolutePath();		
 		
 		List<String> responseLabels = dakotaSurrogates.getResponseLabels(surrogateFileAbsPath);
 		
-		if(responseLabels.size() != 1) errorMessage = "testGetResponseLabels failed - Expected 1 response label, but got " + responseLabels.size();
-		if(!responseLabels.contains("response_fn_1")) errorMessage = "testGetResponseLabels failed - Response label list does not contain response_fn_1";
+		if(responseLabels.size() != 1) errorMessage += "\ntestGetResponseLabels failed - Expected 1 response label, but got " + responseLabels.size();
+		if(!responseLabels.contains("poly_f")) errorMessage += "\ntestGetResponseLabels failed - Response label list does not contain poly_f";
 		
 		return errorMessage;
 	}
@@ -68,21 +68,24 @@ public class DakotaJniTest {
 		
 		//URL testDataFileUrl = DakotaJniTest.class.getResource("/surrogate/exp_poly.s12.response_fn_1.txt");
 		//String surrogateFileAbsPath = new File(testDataFileUrl.getFile()).getAbsolutePath();		
-		String surrogateFileAbsPath = new File("exp_poly.s12.response_fn_1.txt").getAbsolutePath();		
+		String surrogateFileAbsPath = new File("../unit/poly_test.surr.bin").getAbsolutePath();		
 		
+
+		double x1 = 1.4;
+		double x2 = 2.1;
+
 		List<Double> inputValues = new ArrayList<>();
-		inputValues.add(1.0);
-		inputValues.add(2.0);
-		inputValues.add(3.0);
-		inputValues.add(4.0);
-		inputValues.add(5.0);
-		inputValues.add(6.0);
-		inputValues.add(7.0);
-		inputValues.add(8.0);
+		inputValues.add(x1);
+		inputValues.add(x2);
 		
 		Double value = dakotaSurrogates.getValue(surrogateFileAbsPath, inputValues);
-		
-		if(4484.84908 != value) errorMessage = "testGetValue failed - Expected 4484.84908, but got " + value +".";
+		double value_gold = 1.0 +
+		    Math.pow(2.0*x1-1.0, 2.0) + 2.0 * (2.0*x1-1.0) +
+		    Math.pow(2.0*x2-1.0, 2.0) + 2.0 * (2.0*x2-1.0);
+
+		double rel_err = Math.abs(value-value_gold)/Math.abs(value_gold);
+		// the gold value should be around 24.48
+		if(rel_err > 1.0e-12) errorMessage = "testGetValue failed - Expected " + value_gold + ", but got " + value + "; rel_error = " + rel_err + ".";
 		return errorMessage;
 	}	
 }
