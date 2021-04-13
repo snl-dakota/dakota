c10ea289e2025f00b067bcc83567fc762f72f88e
Michael Eldred <mseldre@sandia.gov>
2021-04-12 18:52:32 -0600

    non_hierarch: starting to flesh out ACV class.
    Need to synch with Friedrich's work so that I can start moving things around
    and sharing code in new base class(es).  For now, only adding a skeleton for
    NonDACVSampling, where this can be a peer to NonD{ML,MF,MLMF}Sampling.

---
+++ src/NonDACVSampling.hpp
@@ -0,0 +1,187 @@
+/*  _______________________________________________________________________
+
+    DAKOTA: Design Analysis Kit for Optimization and Terascale Applications
+    Copyright 2014-2020
+    National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+    This software is distributed under the GNU Lesser General Public License.
+    For more information, see the README file in the top Dakota directory.
+    _______________________________________________________________________ */
+
+//- Class:	 NonDACVSampling
+//- Description: Class for approximate control variate sampling
+//- Owner:       Mike Eldred
+//- Checked by:
+//- Version:
+
+#ifndef NOND_ACV_SAMPLING_H
+#define NOND_ACV_SAMPLING_H
+
+#include "NonDMLMFSampling.hpp"
+
+
+namespace Dakota {
+
+/// Perform Approximate Control Variate Monte Carlo sampling for UQ.
+
+/** Approximate Control Variate (ACV) is a variance-reduction technique
+    that utilitizes lower fidelity simulations that have response QoI
+    that are correlated with the high-fidelity response QoI. */
+
+class NonDACVSampling: public NonDMLMFSampling
+{
+public:
+
+  //
+  //- Heading: Constructors and destructor
+  //
+
+  /// standard constructor
+  NonDACVSampling(ProblemDescDB& problem_db, Model& model);
+  /// destructor
+  ~NonDACVSampling();
+
+  //
+  //- Heading: Virtual function redefinitions
+  //
+
+  bool resize();
+
+protected:
+
+  //
+  //- Heading: Virtual function redefinitions
+  //
+
+  void pre_run();
+  void core_run();
+  void post_run(std::ostream& s);
+  void print_results(std::ostream& s, short results_state = FINAL_RESULTS);
+
+private:
+
+  //
+  //- Heading: Helper functions
+  //
+
+  /// synchronize iteratedModel and activeSet on AGGREGATED_MODELS mode
+  void aggregated_models_mode();
+  /// synchronize iteratedModel and activeSet on BYPASS_SURROGATE mode
+  void bypass_surrogate_mode();
+  /// synchronize iteratedModel and activeSet on UNCORRECTED_SURROGATE mode
+  void uncorrected_surrogate_mode();
+
+  /// return (aggregate) level cost
+  Real level_cost(const RealVector& cost, size_t step);
+
+  /// advance any sequence specifications
+  void assign_specification_sequence(size_t index);
+  /// extract current random seed from randomSeedSeqSpec
+  int random_seed(size_t index) const;
+
+  /// manage response mode and active model key from {group,form,lev} triplet.
+  /// s_index is the sequence index that defines the active dimension for a
+  /// model sequence.
+  void configure_indices(unsigned short group, unsigned short form,
+			 size_t lev,           short seq_type);
+
+  /// export allSamples to tagged tabular file
+  void export_all_samples(String root_prepend, const Model& model,
+			  size_t iter, size_t lev);
+
+  //
+  //- Heading: Data
+  //
+
+  /// total number of successful sample evaluations (excluding faults)
+  /// for each model form, discretization level, and QoI
+  Sizet3DArray NLev;
+
+  /// store the pilot_samples input specification, prior to run-time
+  /// invocation of load_pilot_sample()
+  SizetArray pilotSamples;
+
+  /// user specification for seed_sequence
+  SizetArray randomSeedSeqSpec;
+
+  /// major iteration counter
+  size_t mlmfIter;
+
+  /// store the allocation_target input specification, prior to run-time
+  /// Options right now:
+  ///   - Mean = First moment (Mean)
+  ///   - Variance = Second moment (Variance or standard deviation depending on moments central or standard)
+  short allocationTarget;
+
+  /// store the qoi_aggregation_norm input specification, prior to run-time
+  /// Options right now:
+  ///   - sum = aggregate the variance over all QoIs, compute samples from that
+  ///   - max = take maximum sample allocation over QoIs for each level
+  short qoiAggregation;
+
+  /// if defined, export each of the sample increments in ML, CV, MLCV
+  /// using tagged tabular files
+  bool exportSampleSets;
+  /// format for exporting sample increments using tagged tabular files
+  unsigned short exportSamplesFormat;
+};
+
+
+inline void NonDACVSampling::aggregated_models_mode()
+{
+  if (iteratedModel.surrogate_response_mode() != AGGREGATED_MODELS) {
+    iteratedModel.surrogate_response_mode(AGGREGATED_MODELS); // set truth + LFs
+    // synch activeSet with iteratedModel.response_size()
+    activeSet.reshape(num_mf * numFunctions);
+    activeSet.request_values(1);
+  }
+}
+
+
+inline void NonDACVSampling::bypass_surrogate_mode()
+{
+  if (iteratedModel.surrogate_response_mode() != BYPASS_SURROGATE) {
+    iteratedModel.surrogate_response_mode(BYPASS_SURROGATE); // HF only
+    activeSet.reshape(numFunctions);// synch with model.response_size()
+  }
+}
+
+
+/*
+inline void NonDACVSampling::uncorrected_surrogate_mode()
+{
+  if (iteratedModel.surrogate_response_mode() != UNCORRECTED_SURROGATE) {
+    iteratedModel.surrogate_response_mode(UNCORRECTED_SURROGATE); // LF
+    activeSet.reshape(numFunctions);// synch with model.response_size()
+  }
+}
+
+
+inline Real NonDACVSampling::
+level_cost(const RealVector& cost, size_t step)
+{
+  // discrepancies incur two level costs
+  return (step) ?
+    cost[step] + cost[step-1] : // aggregated {HF,LF} mode
+    cost[step];                 //     uncorrected LF mode
+}
+*/
+
+
+/** extract an active seed from a seed sequence */
+inline int NonDACVSampling::random_seed(size_t index) const
+{
+  // return 0 for cases where seed is undefined or will not be updated
+
+  if (randomSeedSeqSpec.empty()) return 0; // no spec -> non-repeatable samples
+  else if (!varyPattern) // continually reset seed to specified value
+    return (index < randomSeedSeqSpec.size()) ?
+      randomSeedSeqSpec[index] : randomSeedSeqSpec.back();
+  // only set sequence of seeds for first pass, then let RNG state continue
+  else if (mlmfIter == 0 && index < randomSeedSeqSpec.size()) // pilot iter only
+    return randomSeedSeqSpec[index];
+  else return 0; // seed sequence exhausted, do not update
+}
+
+} // namespace Dakota
+
+#endif
