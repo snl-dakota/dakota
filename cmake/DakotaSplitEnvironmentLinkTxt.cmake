14268559974961901ab4430e10ba5ce557f1c9f5
Brian M. Adams <briadam@sandia.gov>
2021-04-27 15:37:37 -0600

    Fix link line splitting to fix packaging
    DP-543: Fix bug where CPack would fail due to not finding the
    anticipated RPATH in the build tree binaries when changing to install
    tree binaries. A double colon tripped it up. Still fragile, but may
    get us through for now.

--- cmake/DakotaSplitEnvironmentLinkTxt.cmake
+++ cmake/DakotaSplitEnvironmentLinkTxt.cmake
@@ -3,13 +3,13 @@
 file(READ ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/environment.dir/link.txt
   link_txt)
 
-string(REGEX MATCH "packages/external/DDACE/src: -Wl,-rpath,/"
+string(REGEX MATCH "packages/external/DDACE/src -Wl,-rpath,/"
   already_fixed ${link_txt})
 
 if(NOT already_fixed)
 
   #string(REPLACE <match-string> <replace-string> <out-var> <input>...)
-  string(REPLACE "packages/external/DDACE/src:/" "packages/external/DDACE/src: -Wl,-rpath,/" link_new ${link_txt})
+  string(REPLACE "packages/external/DDACE/src:/" "packages/external/DDACE/src -Wl,-rpath,/" link_new ${link_txt})
 
   file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/environment.dir/link.txt
     ${link_new})
