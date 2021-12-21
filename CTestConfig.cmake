ac3c67e26ff911dc524dc7c6c75eb31045c6fdc3
Brian M. Adams <briadam@sandia.gov>
2021-12-20 17:03:59 -0700

    Update CTest to use new drop location

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,8 +9,8 @@ set(CTEST_PROJECT_NAME "Dakota")
 set(CTEST_PROJECT_SUBPROJECTS UnitTest)
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "sems-cdash-srn.sandia.gov")
-set(CTEST_DROP_LOCATION "/sems/submit.php?project=Dakota")
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "dakota-cdash.sandia.gov")
+set(CTEST_DROP_LOCATION "/submit.php?project=Dakota")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
