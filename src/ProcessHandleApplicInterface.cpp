snl-dakota/dakota

commit d29d4843326b5facb1ef1c53ce7455bfed1fd23d

PluginInterface: Make Boost optional
DP-723: Remove dependence of user-compiled plugins on Boost, with an
optional fallback via CMake option DAKOTA_PLUGINS_USE_BOOST in case of
an unsupported compiler.

src/ProcessHandleApplicInterface.cpp
@@ -479,8 +479,14 @@ void ProcessHandleApplicInterface::check_wait(pid_t pid, int status)
     //   to exit or _exit or as the argument for a return statement in main().
     //   This macro should only be employed if WIFEXITED returned true.
     if ( WIFEXITED(status) == 0 || (signed char)WEXITSTATUS(status) == -1 ) {
-      Cerr << "Fork application failure, aborting.\nSystem error message: "
-	   << std::strerror(errno) << '\n';
+      Cerr << "Fork application failure. Aborting.\nOperating system error "
+	   << "message: " << std::strerror(errno) << '\n';
+      if (errno == EACCES) {
+	Cerr << "Note: Each analysis_driver must have read and execute permission.\n";
+#ifndef _WIN32
+	Cerr << "Note: Consider 'chmod u+rx driver_filename' or similar.\n";
+#endif
+      }
       // std::strerror() returns the system error message associated with errno
       // (a system constant defined in errno.h containing the number of the last
       // observed error).  Note: If it becomes an issue, it would also be

src/plugins/CMakeLists.txt
@@ -11,12 +11,13 @@ cmake_minimum_required(VERSION 3.12)
 project(DakotaPlugins CXX)
 
 
-# TODO: Decide if we want the dependence on boost or want to roll our
-# own export macro. This should work with most any version of Boost
-# and needn't match Dakota's
-find_package(Boost)
+option(DAKOTA_PLUGINS_USE_BOOST "Whether to use Boost to build plugins" OFF)
+if(DAKOTA_PLUGINS_USE_BOOST)
+  find_package(Boost REQUIRED)
+endif()
 
 
+# TODO: function(add_plugin PLUGIN_NAME SOURCES) spanning the following
 add_library(identity_map SHARED PluginIdentityMap.cpp)
 
 set_target_properties(identity_map PROPERTIES
@@ -24,7 +25,11 @@ set_target_properties(identity_map PROPERTIES
 		      CXX_STANDARD_REQUIRED TRUE
 		      CXX_VISIBILITY_PRESET hidden)
 
-target_link_libraries(identity_map Boost::boost)
+if(DAKOTA_PLUGINS_USE_BOOST)
+  target_compile_definitions(identity_map PRIVATE DAKOTA_PLUGINS_USE_BOOST=1)
+  target_link_libraries(identity_map Boost::boost)
+endif()
+
 
 # TODO: install whole plugin source dir to share/dakota/ for end users
 #install(...)

src/plugins/PluginIdentityMap.cpp
@@ -8,10 +8,7 @@
     _______________________________________________________________________ */
 
 #include "PluginIdentityMap.hpp"
-
-// TODO: Decide if we want the dependence on boost or want to roll our
-// own macro. This should work with most any version of Boost
-#include <boost/config.hpp>
+#include "dakota_symbol_visibility.hpp"
 
 
 namespace DP = DakotaPlugins;
@@ -25,5 +22,5 @@ DP::EvalResponse PluginIdentityMap::evaluate(const DP::EvalRequest& request)
 }
 
 
-extern "C" BOOST_SYMBOL_EXPORT PluginIdentityMap dakota_interface_plugin;
+extern "C" DAKOTA_SYMBOL_EXPORT PluginIdentityMap dakota_interface_plugin;
 PluginIdentityMap dakota_interface_plugin;

src/plugins/dakota_symbol_visibility.hpp
@@ -1,0 +1,32 @@
+/*  _______________________________________________________________________
+
+    DAKOTA: Design Analysis Kit for Optimization and Terascale Applications
+    Copyright 2014-2020
+    National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+    This software is distributed under the GNU Lesser General Public License.
+    For more information, see the README file in the top Dakota directory.
+    _______________________________________________________________________ */
+
+#ifndef DAKOTA_SYMBOL_VISIBILITY_H
+#define DAKOTA_SYMBOL_VISIBILITY_H
+
+// RATIONALE: Boost config.hpp offers a more general/portable
+// implementation than the basics herein, but Dakota plugins shouldn't
+// require Boost. If Boost used, most any version of Boost would
+// suffice.
+
+#ifdef DAKOTA_PLUGINS_USE_BOOST
+#  include <boost/config.hpp>
+#  define DAKOTA_SYMBOL_EXPORT BOOST_SYMBOL_EXPORT
+#else
+#  if (defined(_WIN32) || defined(__WIN32__) || defined(WIN32)) && !defined(__CYGWIN__)
+#    define DAKOTA_SYMBOL_EXPORT __attribute__((__dllexport__))
+#  elif defined(__INTEL_COMPILER)
+#    define DAKOTA_SYMBOL_EXPORT __attribute__((visibility("default")))
+#  else
+//   gcc, clang
+#    define DAKOTA_SYMBOL_EXPORT __attribute__((__visibility__("default")))
+#  endif
+#endif
+
+#endif

commit 880b13d48409f7409fe8ecc0d12fc4c8a14825b5

PluginInterface: Revert one file from d29d484
Accidentally committed an improved warning for fork interfaces.

src/ProcessHandleApplicInterface.cpp
@@ -479,14 +479,8 @@ void ProcessHandleApplicInterface::check_wait(pid_t pid, int status)
     //   to exit or _exit or as the argument for a return statement in main().
     //   This macro should only be employed if WIFEXITED returned true.
     if ( WIFEXITED(status) == 0 || (signed char)WEXITSTATUS(status) == -1 ) {
-      Cerr << "Fork application failure. Aborting.\nOperating system error "
-	   << "message: " << std::strerror(errno) << '\n';
-      if (errno == EACCES) {
-	Cerr << "Note: Each analysis_driver must have read and execute permission.\n";
-#ifndef _WIN32
-	Cerr << "Note: Consider 'chmod u+rx driver_filename' or similar.\n";
-#endif
-      }
+      Cerr << "Fork application failure, aborting.\nSystem error message: "
+	   << std::strerror(errno) << '\n';
       // std::strerror() returns the system error message associated with errno
       // (a system constant defined in errno.h containing the number of the last
       // observed error).  Note: If it becomes an issue, it would also be