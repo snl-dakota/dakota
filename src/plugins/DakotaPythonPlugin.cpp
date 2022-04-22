bdf1ec686a01846d50436124705a809f32cb8e96
Brian M. Adams <briadam@sandia.gov>
2022-04-22 16:05:34 -0600

    PluginInterface: Basic build system for demo plugins
    DP-751, DP-723: Given CMake option DAKOTA_PLUGIN_DEMOS (default OFF),
    build src/plugins as a CMake ExternalProject to emulate a user
    building them separately from Dakota.
    
    Enable test/dakota_plugins.in when demos are enabled and add
    baseline. Only working for Linux thus far.

--- src/plugins/DakotaPythonPlugin.cpp
+++ src/plugins/DakotaPythonPlugin.cpp
@@ -1,3 +1,12 @@
+/*  _______________________________________________________________________
+
+    DAKOTA: Design Analysis Kit for Optimization and Terascale Applications
+    Copyright 2014-2020
+    National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+    This software is distributed under the GNU Lesser General Public License.
+    For more information, see the README file in the top Dakota directory.
+    _______________________________________________________________________ */
+
 #include <boost/config.hpp>
 #include <boost/dll.hpp>
 #include <pybind11/embed.h>
