dff5b721e577c51ee75ab9e2d5c830addb8471c9
Michael Eldred <mseldre@sandia.gov>
2022-08-15 18:26:45 -0600

    online_debug_output: revert accidental commit that deactivates Sphinx docs

--- cmake/DakotaDev.cmake
+++ cmake/DakotaDev.cmake
@@ -2,7 +2,7 @@
 
 # Developer options
 set(DAKOTA_HAVE_MPI TRUE CACHE BOOL "Enable MPI in DAKOTA?")
-set(ENABLE_DAKOTA_DOCS FALSE CACHE BOOL "Enable DAKOTA documentation build")
+set(ENABLE_DAKOTA_DOCS TRUE CACHE BOOL "Enable DAKOTA documentation build")
 set(ENABLE_SPEC_MAINT TRUE CACHE BOOL 
   "Enable DAKOTA specification maintenance mode?")
 set(PECOS_ENABLE_TESTS TRUE CACHE BOOL "Enable Pecos-specific tests?")
