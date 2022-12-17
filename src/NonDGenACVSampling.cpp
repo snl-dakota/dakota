dc4442c1abd1461c817c4494d57e590d19d7b8db
Michael Eldred <mseldre@sandia.gov>
2022-12-16 20:49:18 -0700

    ensemble_select: refining G,g implementation and testing versus F for ACV-{MF,IS} for DAG = {0, ..., 0} case

--- src/NonDGenACVSampling.cpp
+++ src/NonDGenACVSampling.cpp
@@ -94,10 +94,13 @@ void NonDGenACVSampling::generalized_acv_online_pilot()
   generate_dags(model_dags);
 
   UShortArraySet::const_iterator dag_cit;
-  RealMatrix G;  RealVector g, N_vec(numApprox+1);
-  N_vec[numApprox] = numSamples;
-  for (int i=0; i<numApprox; ++i)
+  RealMatrix G;  RealVector g, N_vec(numApprox+1), avg_eval_ratios;
+  for (int i=0; i<=numApprox; ++i)
     N_vec[i] = (numApprox+1-i)*numSamples;
+  copy_data_partial(N_vec, 0, (int)numApprox, avg_eval_ratios);
+  avg_eval_ratios.scale(1./N_vec[numApprox]);
+  Cout << "N_vec:\n" << N_vec << "r_i:\n" << avg_eval_ratios << std::endl;
+
   for (dag_cit=model_dags.begin(); dag_cit!=model_dags.end(); ++dag_cit) {
     // for testing:
     const UShortArray& dag = *dag_cit;
@@ -106,6 +109,12 @@ void NonDGenACVSampling::generalized_acv_online_pilot()
     //set_active_dag(dag);
     //approximate_control_variate(); // *** must rely on virtual G/g definition
   }
+
+  RealSymMatrix F;
+  compute_F_matrix(avg_eval_ratios, F);
+  // Differs by a scaling...
+  F *= 1./(Real)numSamples;
+  Cout << "Scaled F:\n" << F << std::endl;
 }
 
 
@@ -160,60 +169,71 @@ compute_parameterized_G_g(const RealVector& N_vec, const UShortArray& dag,
 			  RealMatrix& G, RealVector& g)
                         //RealSymMatrix& G, RealVector& g) // *** TO DO
 {
-  // ************************************
-  // *** TO DO: invert N_vec ordering ***
-  // ************************************
+  // Invert N_vec ordering
+  // > Dakota r_i ordering is low-to-high --> reversed from Peherstorfer
+  //   (HF = 1, MF = 2, LF = 3) and Gorodetsky,Bomarito (HF = 0, MF = 1, LF = 2)
+  // > incoming N_vec is ordered as for Dakota r_i: LF = 0, MF = 1, HF = 2
+  // > incoming DAG is zero root as in Bomarito 2022 --> reverse DAG ordering
+  // See also NonDNonHierarchSampling::mfmc_analytic_solution()
+
+  // Here we use ACV notation: z_i is comprised of N_i incoming samples
+  // including z^1_i (less resolved "correlated mean estimator") and z^2_i
+  // (more resolved "control variate mean").  Mapping from GenACV notation:
+  // "z^*_i" --> z^1_i, "z_i" --> z^2_i, "z_{i* U i}" --> z_i
 
   size_t i, j;
   if (G.empty()) G.shapeUninitialized(numApprox, numApprox); // *** TO DO
   if (g.empty()) g.sizeUninitialized(numApprox);
 
+  Real bi, bj, z_i, z1_i, z2_i;
   switch (mlmfSubMethod) {
   case SUBMETHOD_ACV_IS: { // Bomarito Eqs. 21-22
-    Real bi, bj, N_i, N_bi, NpN_i, NpN_j, NpN_NpN;
+    Real z_j, zi_zj;//, z1_j, z2_j;
     for (i=0; i<numApprox; ++i) {
-      bi = dag[i];  N_i = N_vec[i];  N_bi = N_vec[bi];  NpN_i = N_bi + N_i;
-      g[i] = (bi == 0) ? 1./N_bi - 1./NpN_i : 0.;
-      for (j=0; j<numApprox; ++j) { // *** TO DO: VERIFY SYMMETRY --> j<=i
-	bj = dag[j];  NpN_j = N_vec[bj] + N_vec[j];  NpN_NpN = NpN_i * NpN_j;
-	G(i,j)  = 0.;
-	if (bi == bj)  G(i,j) += 1./N_bi - 1./NpN_i - 1./NpN_j + N_bi/NpN_NpN;
-	if (bi ==  j)  G(i,j) += N_bi/NpN_NpN - 1./NpN_j;
-	if ( i == bj)  G(i,j) += N_i /NpN_NpN - 1./NpN_i;
-	if ( i ==  j)  G(i,j) += N_i /NpN_NpN;
+      bi = numApprox - dag[i]; // reverse DAG ordering for sample ordering
+      z_i = N_vec[i];  z1_i = N_vec[bi];  z2_i = z_i - z1_i;
+      g[i] = (bi == numApprox) ? 1./z1_i - 1./z_i : 0.;
+      for (j=0; j<numApprox; ++j) {
+	bj = numApprox - dag[j]; // reverse DAG ordering for sample ordering
+	z_j = N_vec[j];  //z_1j = N_vec[bj];  z2_j = z_j - z1_j;
+	zi_zj = z_i * z_j;  G(i,j)  = 0.;
+	if (bi == bj)  G(i,j) += 1./z1_i - 1./z_i - 1./z_j + z1_i/zi_zj;
+	if (bi ==  j)  G(i,j) += z1_i/zi_zj - 1./z_j; // false for dag = 0
+	if (i  == bj)  G(i,j) += z2_i/zi_zj - 1./z_i; // false for dag = 0
+	if (i  ==  j)  G(i,j) += z2_i/zi_zj;
       }
     }
     break;
   }
   case SUBMETHOD_ACV_MF: { // Bomarito Eqs. 16-17
-    Real N_0 = N_vec[0], N_i, N_bi, N_j, N_bj;
+    Real z_H = N_vec[numApprox], z_j, z1_j, z2_j;
     for (i=0; i<numApprox; ++i) {
-      N_i = N_vec[i];  N_bi = N_vec[dag[i]];
-      g[i] = (std::min(N_bi, N_0) / N_bi - std::min(N_i, N_0) / N_i) / N_0;
-      for (j=0; j<numApprox; ++j) { // *** TO DO: VERIFY SYMMETRY --> j<=i
-	N_j = N_vec[j];  N_bj = N_vec[dag[j]];
-	G(i,j) =
-	  (std::min(N_bi, N_bj) / N_bj - std::min(N_bi, N_j) / N_j)  / N_bi +
-	  (std::min(N_i,  N_j)  / N_j  - std::min(N_i, N_bj) / N_bj) / N_i;
+      bi = numApprox - dag[i]; // reverse DAG ordering for sample ordering
+      z_i = N_vec[i];  z1_i = N_vec[bi];  z2_i = z_i; // *** CONFIRM z2_i
+      g[i] = (std::min(z1_i, z_H) / z1_i - std::min(z2_i, z_H) / z2_i) / z_H;
+      for (j=0; j<numApprox; ++j) {
+	bj = numApprox - dag[j]; // reverse DAG ordering for sample ordering
+	z_j = N_vec[j];  z1_j = N_vec[bj];  z2_j = z_j; // *** CONFIRM z2_j
+	G(i,j) = (std::min(z1_i, z1_j)/z1_j - std::min(z1_i, z2_j)/z2_j)/z1_i +
+	         (std::min(z2_i, z2_j)/z2_j - std::min(z2_i, z1_j)/z1_j)/z2_i;
       }
     }
     break;
   }
-  case SUBMETHOD_ACV_RD: { // Bomarito Eqs. 19-20
-    Real bi, bj, N_i, N_bi;
+  case SUBMETHOD_ACV_RD: // Bomarito Eqs. 19-20
     for (i=0; i<numApprox; ++i) {
-      bi = dag[i];  N_i = N_vec[i];  N_bi = N_vec[bi];
-      g[i] = (bi == 0) ? 1./N_bi : 0.;
-      for (j=0; j<numApprox; ++j) { // *** TO DO: VERIFY SYMMETRY --> j<=i
-	bj = dag[j];   G(i,j)  = 0.;
-	if (bi == bj)  G(i,j) += 1./N_bi;
-	if (bi ==  j)  G(i,j) -= 1./N_bi;
-	if ( i == bj)  G(i,j) -= 1./N_i;
-	if ( i ==  j)  G(i,j) += 1./N_i;
+      bi = numApprox - dag[i];
+      z_i = N_vec[i];  z1_i = N_vec[bi];  z2_i = z_i - z1_i;
+      g[i] = (bi == numApprox) ? 1./z1_i : 0.;
+      for (j=0; j<numApprox; ++j) {
+	bj = numApprox - dag[j];   G(i,j)  = 0.;
+	if (bi == bj)  G(i,j) += 1./z1_i;
+	if (bi ==  j)  G(i,j) -= 1./z1_i; // always false for dag = 0
+	if ( i == bj)  G(i,j) -= 1./z2_i; // always false for dag = 0
+	if ( i ==  j)  G(i,j) += 1./z2_i;
       }
     }
     break;
-  }
   default:
     Cerr << "Error: bad sub-method name (" << mlmfSubMethod << ") in NonDGen"
 	 << "ACVSampling::compute_parameterized_F_matrix()" << std::endl;
