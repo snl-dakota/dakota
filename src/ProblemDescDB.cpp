377f584f81bd8e1f5e8d88efb47acb0e5db3d81f
Michael Eldred <mseldre@sandia.gov>
2023-02-08 19:35:24 -0700

    ensemble_select: implement full and partial recursion for generating DAGs
    > Spec for graph recursion updated to support KL recursion, partial recursion
      with depth throttle, and full recursion
    > The latter two options above use new generate_trees() which recursively
      employs generate_sub_trees().  Implementation motivated by python code from
      stack overflow that John pointed me to.
    > Generated root list promoted to a class attribute and deployed more broadly
      (previous dependency unrolling in compute_parameterized_G_g() has been
      consolidated)
    
    TO DO: recursion appears correct, but still need to populate modelDAGs array
    during the recursion.

--- src/ProblemDescDB.cpp
+++ src/ProblemDescDB.cpp
@@ -2437,6 +2437,7 @@ unsigned short ProblemDescDB::get_ushort(const String& entry_name) const
       {"nond.export_corrected_variance_format", P_MET exportCorrVarFormat},
       {"nond.export_discrep_format", P_MET exportDiscrepFormat},
       {"nond.export_samples_format", P_MET exportSamplesFormat},
+      {"nond.graph_recursion_limit", P_MET dagRecursionLimit},
       {"nond.integration_refinement", P_MET integrationRefine},
       {"nond.numerical_solve_mode", P_MET numericalSolveMode},
       {"nond.opt_subproblem_solver", P_MET optSubProbSolver},
