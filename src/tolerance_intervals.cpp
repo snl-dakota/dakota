fd037f910c485e95510671b9ee42d0cbc7481e17
Ernesto Prudencio <eeprude@sandia.gov>
2023-03-07 13:44:48 -0700

    Implementation of double sided tolerance interval equivalent normal (DSTIEN). See Charles F. Jekel and Vicente Romero, "Conservative and Efficient Tail Probability Estimation from Sparse Sample Data", Sandia Report SAND2020-7572J, equations (4) and (5).
    Squashed commit of the following:
    
    commit 4d64135b87079ed388be24975a47c0b8a9b56e45
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Tue Mar 7 11:05:41 2023 -0700
    
        Forgot one line in the explanation header of the routine
    
    commit 6100dda4fa4bce4cb04c99f6b5fe7b9ee46d5785
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Tue Mar 7 10:12:56 2023 -0700
    
        Addressed the extra 3 feedbacks from Adam
    
    commit 110076174282dff1c71d80d7a2ab57437b3c6f2f
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Mon Mar 6 16:46:00 2023 -0700
    
        Addressed all the two feedbacks from J. Adam Stephens
    
    commit a26b6275896e7a0f13a0e437088094db827e7102
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Sun Mar 5 19:57:55 2023 -0700
    
        Finished adding the extra parameter 'coverage', per discussion with Vicente Romero. Unit tests were adapted and new unit tests were added as well.
    
    commit d3b922b6bad33683c2bb2ac42b7d9d14155501f9
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Wed Mar 1 20:57:39 2023 -0700
    
        Last commit. We will wait for after the meeting with Vicente Romero to merge the code.
    
    commit eccc8fae3efb412c429c72cb053bbb17a5b481b3
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Tue Feb 28 21:27:32 2023 -0700
    
        Final implementation and unit tests
    
    commit 6b3c281fd8c10cb03301df4e9aeead60610c3d62
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Tue Feb 28 16:48:20 2023 -0700
    
        Almost done with unit tests
    
    commit 69570242447dea952dab7c624eadf0e90b64027f
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Mon Feb 27 13:11:51 2023 -0700
    
        Added correction factor for TIEN sigma. Adding more unit tests.
    
    commit b9fe977f6464d410d77c96f4f44fb0f270af29eb
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Thu Feb 23 12:26:24 2023 -0700
    
        Improvements on implementation and on testing
    
    commit 7527716b9d733666f7162ae2be0417d96defb197
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Thu Feb 23 01:57:49 2023 -0700
    
        Forgot to add these 4 new files
    
    commit d074a38872749b09475c8ee67fecd40003821036
    Author: Ernesto Prudencio <eeprude@sandia.gov>
    Date:   Thu Feb 23 01:51:26 2023 -0700
    
        Backup of current state of implementation. There is a 'TO DO' on the implementation itself. Begun working on the test file. Cleaned up all unnecessary include files for the 'tolerance intervals' code.

---
+++ src/tolerance_intervals.cpp
@@ -0,0 +1,237 @@
+/*  _______________________________________________________________________
+
+    DAKOTA: Design Analysis Kit for Optimization and Terascale Applications
+    Copyright 2014-2022
+    National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+    This software is distributed under the GNU Lesser General Public License.
+    For more information, see the README file in the top Dakota directory.
+    _______________________________________________________________________ */
+
+//- Class:
+//- Description: Routines for computing tolerance intervals
+//- Owner:       
+//- Checked by:
+//- Version:
+
+#include "tolerance_intervals.hpp"
+#include "DakotaResponse.hpp"
+#include <boost/math/distributions/chi_squared.hpp>
+
+static const char rcsId[]="@(#) $Id: tolerance_intervals.cpp 9999 2010-10-22 23:20:24Z mseldre $";
+
+namespace Dakota {
+
+Real std_normal_coverage_inverse( const Real coverage )
+{
+  if ((0. <= coverage) && (coverage <= 1.)) {
+    // Ok
+  }
+  else {
+    Cerr << "Error in std_normal_coverage_inverse()"
+         << ": coverage (" << coverage
+         << ") must belong to the closed interval [0,1]"
+         << std::endl;
+    abort_handler(-1);
+  }
+
+  Real b = sqrt(2.) * boost::math::erf_inv(coverage);
+
+  return b;
+}
+
+Real computeDSTIEN_conversion_factor( const size_t number_of_samples
+                                    , const Real   coverage
+                                    , const Real   alpha
+                                    )
+{
+  if (number_of_samples < 2) {
+    Cerr << "Error in computeDSTIEN_conversion_factor()"
+         << ": the number of response samples (" << number_of_samples
+         << ") must be at least 2"
+         << std::endl;
+    abort_handler(-1);
+  }
+
+  if ((0. <= coverage) && (coverage <= 1.)) {
+    // Ok
+  }
+  else {
+    Cerr << "Error in computeDSTIEN_conversion_factor()"
+         << ": coverage (" << coverage
+         << ") must belong to the closed interval [0,1]"
+         << std::endl;
+    abort_handler(-1);
+  }
+
+  if ((0. <= alpha) && (alpha <= 1.)) {
+    // Ok
+  }
+  else {
+    Cerr << "Error in computeDSTIEN_conversion_factor()"
+         << ": alpha (" << alpha
+         << ") must belong to the closed interval [0,1]"
+         << std::endl;
+    abort_handler(-1);
+  }
+
+  Real conversionFactor = 0.;
+  
+  if (number_of_samples == 1) {
+    conversionFactor = std::numeric_limits<Real>::quiet_NaN();
+  }
+  else {
+    Real m = static_cast<Real>(number_of_samples);
+    boost::math::chi_squared chisq(m - 1.);
+    Real quant = boost::math::quantile(chisq, alpha);
+    Real tmpRatio = (m - 3. - quant) / (2. * (m + 1.) * (m + 1.));
+    conversionFactor = std_normal_coverage_inverse(coverage) * std::sqrt(1. + 1./m) * std::sqrt( (m - 1.)/quant ) * std::sqrt( 1. + tmpRatio );
+  }
+
+  return conversionFactor;
+}
+
+void computeDSTIEN( const IntResponseMap & resp_samples
+                  , const Real             coverage
+                  , const Real             alpha
+                  , size_t               & num_valid_samples
+                  , RealVector           & dstien_mus
+                  , RealVector           & dstien_sigmas
+                  )
+{
+  // Check input information
+  size_t num_samples = resp_samples.size();
+  if (num_samples < 2) {
+    Cerr << "Error in computeDSTIEN()"
+         << ": the number of response samples (" << num_samples
+         << ") must be at least 2"
+         << std::endl;
+    abort_handler(-1);
+  }
+
+  size_t num_responses = resp_samples.begin()->second.num_functions();
+  if (num_responses == 0) {
+    Cerr << "Error in computeDSTIEN()"
+         << ": the number of responses of the first sample (" << num_responses
+         << ") must be nonzero"
+         << std::endl;
+    abort_handler(-1);
+  }
+
+  {
+    ;
+    for (IntRespMCIter it = resp_samples.begin(); it != resp_samples.end(); ++it) {
+      if (it->second.num_functions() != num_responses) {
+        Cerr << "Error in computeDSTIEN()"
+             << ": all response samples must have the same size (" << num_responses
+             << ") as the first sample"
+             << std::endl;
+        abort_handler(-1);
+      }
+    }
+  }
+
+  if ((0. <= coverage) && (coverage <= 1.)) {
+    // Ok
+  }
+  else {
+    Cerr << "Error in computeDSTIEN()"
+         << ": coverage (" << coverage
+         << ") must belong to the closed interval [0,1]"
+         << std::endl;
+    abort_handler(-1);
+  }
+
+  if ((0. <= alpha) && (alpha <= 1.)) {
+    // Ok
+  }
+  else {
+    Cerr << "Error in computeDSTIEN()"
+         << ": alpha (" << alpha
+         << ") must belong to the closed interval [0,1]"
+         << std::endl;
+    abort_handler(-1);
+  }
+
+  if (dstien_mus.length() != num_responses) {
+    dstien_mus.resize(num_responses);
+  }
+
+  if (dstien_sigmas.length() != num_responses) {
+    dstien_sigmas.resize(num_responses);
+  }
+
+  // Initialize the output variables
+  num_valid_samples = 0;
+  dstien_mus        = 0.;
+  dstien_sigmas     = 0.;
+
+  // Determine the amount of valid samples
+  std::vector<bool> sample_valid_status(num_samples,false);
+  {
+    IntRespMCIter it = resp_samples.begin();
+    for (size_t j = 0; j < num_samples; ++j, ++it) {
+      bool sample_is_valid = true;
+      for (size_t k = 0; (k < num_responses) && sample_is_valid; ++k) {
+        sample_is_valid = std::isfinite(it->second.function_value(k));
+      } // for k
+      if (sample_is_valid) {
+        num_valid_samples += 1;
+        sample_valid_status[j] = true;
+      }
+    } // for j
+  }
+
+  if (num_valid_samples == 0) {
+    dstien_mus    = std::numeric_limits<Real>::quiet_NaN();
+    dstien_sigmas = std::numeric_limits<Real>::quiet_NaN();
+  }
+  else {
+    Real m = static_cast<Real>(num_valid_samples);
+
+    // Compute DSTIEN mus
+    {
+      IntRespMCIter it = resp_samples.begin();
+      for (size_t j = 0; j < num_samples; ++j, ++it) {
+        if (sample_valid_status[j]) {
+          for (size_t k = 0; k < num_responses; ++k) {
+            dstien_mus[k] += it->second.function_value(k);
+          } // for k
+        }
+      } // for j
+    }
+
+    dstien_mus *= 1./m;
+
+    // Compute DSTIEN sigmas
+    if (num_valid_samples == 1) {
+      dstien_sigmas = std::numeric_limits<Real>::quiet_NaN();
+    }
+    else {
+      {
+        IntRespMCIter it = resp_samples.begin();
+        for (size_t j = 0; j < num_samples; ++j, ++it) {
+          if (sample_valid_status[j]) {
+            for (size_t k = 0; k < num_responses; ++k) {
+              Real diff = it->second.function_value(k) - dstien_mus[k];
+              dstien_sigmas[k] += diff * diff;
+            } // for k
+          }
+        } // for j
+      }
+
+      Real conversionFactor = computeDSTIEN_conversion_factor( num_valid_samples
+                                                             , coverage
+                                                             , alpha
+                                                             );
+
+      dstien_sigmas *= 1./(m - 1.);
+      for (size_t k = 0; k < num_responses; ++k) {
+        dstien_sigmas[k] = std::sqrt( dstien_sigmas[k] );
+      } // for k
+      dstien_sigmas *= conversionFactor;
+
+    } // if (num_valid_samples == 1) else
+  } // if (num_valid_samples == 0) else
+} // void computeDSTIEN()
+
+} // namespace Dakota
