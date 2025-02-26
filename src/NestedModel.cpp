bc616ead187a99fbf4fc6656d732a10aa8a33139
J. Adam Stephens <jasteph@sandia.gov>
2025-02-26 08:27:06 -0700

    Check for default constructed iterator
    This commit fixes a segfault caused by a couple of
    interacting changes previously made.
    
    After making subIterator a shared_ptr<Iterator>, I naively
    converted a conditional that tests subIterator.is_null() to
    check whether subIterator is itself set. This results in a
    segfault in NestedModel when Dakota is run in parallel
    due changes in 1769ef09.
    
    That commit "default constructs" Iterators in
    IteratorScheduler::init__iterator() on non-zero ranks when
    Dakota is run in parallel in order to cache information
    in them that originally (on devel) was being stored in
    Iterator envelopes.
    
    Because the envelopes had no letter, the conditional
    on is_null() was bypassed, but now is executed because
    subIterator is no longer null.
    
    This change tests subIterator->method_id(), assuming that
    when it is empty, subIterator points to a default
    constructed object.
    
    I would very much welcome comments/suggestions on this
    approach. It feels pretty non-ideal.
    
    An alternative I considered was adding a new member variable
    and getter to Iterator, Iterator::default_constructed(). But
    I hesitated because I didn't want to add a whole new member
    function for this one unusual situation.

--- src/NestedModel.cpp
+++ src/NestedModel.cpp
@@ -609,7 +609,7 @@ derived_init_communicators(ParLevLIter pl_iter, int max_eval_concurrency,
   probDescDB.set_db_model_nodes(model_index);
 
   // > now that subIterator is constructed, perform downstream updates
-  if (subIterator) {
+  if (!subIterator->method_id().empty()) { // only do if subIterator was properly constructed
     init_sub_iterator(); // follow DB restore: extracts data from nested spec
     if (subIteratorSched.messagePass) {
       // msg lengths: vars from this model, set & final results from subIterator
