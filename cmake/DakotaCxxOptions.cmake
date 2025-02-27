138f14fb9c1be9ba740f9bcb060f0aa92d692f76
J. Adam Stephens <jasteph@sandia.gov>
2025-02-27 13:57:24 -0700

    Fix ordering issue again

--- cmake/DakotaCxxOptions.cmake
+++ cmake/DakotaCxxOptions.cmake
@@ -16,14 +16,6 @@ macro(dakota_cxx_standard)
   endif()
   set(CMAKE_CXX_STANDARD_REQUIRED TRUE CACHE BOOL
     "Dakota strictly requires C++14 or better")
-
-  if(WIN32)
-    if(MSVC AND MSVC_VERSION GREATER_EQUAL 1914)  # VS 2017 15.7+
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zc:__cplusplus" CACHE STRING 
-        "MSVC does not define __cplusplus by default" FORCE)
-    endif()
-  endif()
-
 endmacro()
 
 
@@ -36,4 +28,10 @@ macro(dakota_strict_warnings target_name)
   endif()
 endmacro()
 
-
+# Set platform-specific global compile options
+macro(dakota_platform_specific_compile_options)
+  if(WIN32 AND MSVC AND MSVC_VERSION GREATER_EQUAL 1914)  # VS 2017 15.7+
+     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zc:__cplusplus" CACHE STRING 
+        "MSVC does not define __cplusplus by default" FORCE)
+   endif()
+endmacro()
