e39825c07c1af7655241a0152718b1a6487bc9e5
Russell Hooper <rhoope@sandia.gov>
2025-04-28 20:33:16 -0600

    DP-1280 : Update release version and date

--- DakotaVersion.cmake
+++ DakotaVersion.cmake
@@ -1,16 +1,16 @@
 # Configure the Dakota version, stable build indicator, and release date 
 
 set(Dakota_VERSION_MAJOR 6)
-set(Dakota_VERSION_MINOR 21)
+set(Dakota_VERSION_MINOR 22)
 set(Dakota_VERSION_PATCH 0)
 
 # Toggle this to append to the version number for stable
-#set(Dakota_VERSION_APPEND_STABLE "")  # Versioned release
-set(Dakota_VERSION_APPEND_STABLE "+")  # Stable releases
+set(Dakota_VERSION_APPEND_STABLE "")  # Versioned release
+#set(Dakota_VERSION_APPEND_STABLE "+")  # Stable releases
 
 # If empty, the build date will be used as the release date (could consider 
 # using the Git rev date); use format Mmm dd yyyy, as in asctime()
-#set(Dakota_RELEASE_DATE "Nov. 14 2024")  # Versioned release date Mmm dd yyyy
-set(Dakota_RELEASE_DATE "")               # Stable release: empty -> build date
+set(Dakota_RELEASE_DATE "May 15 2025")  # Versioned release date Mmm dd yyyy
+#set(Dakota_RELEASE_DATE "")               # Stable release: empty -> build date
 
 
