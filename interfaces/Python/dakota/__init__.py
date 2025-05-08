9239262489cd850cf259251fbaebc17ff1041fee
Russell Hooper <rhoope@sandia.gov>
2025-05-08 10:07:14 -0600

    Update copyright

--- interfaces/Python/dakota/__init__.py
+++ interfaces/Python/dakota/__init__.py
@@ -3,6 +3,6 @@ The dakota package collects various Dakota-related Python functionality.
 """
 
 __author__ = 'J. Adam Stephens'
-__copyright__ = 'Copyright 2014-2024 National Technology & Engineering Solutions of Sandia, LLC (NTESS)'
+__copyright__ = 'Copyright 2014-2025 National Technology & Engineering Solutions of Sandia, LLC (NTESS)'
 __license__ = 'GNU Lesser General Public License'
 
