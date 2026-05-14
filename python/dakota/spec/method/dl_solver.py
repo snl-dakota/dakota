ad5b1b13d80d3e223b1e2f6c1160b901608a036e
J. Adam Stephens <jasteph@sandia.gov>
2026-05-14 16:08:31 -0600

    Make name an argument keyword

--- python/dakota/spec/method/dl_solver.py
+++ python/dakota/spec/method/dl_solver.py
@@ -22,10 +22,32 @@ class DlSolverConfig(
 ):
     "(Experimental) Dynamically-loaded solver"
 
-    name: str = DakotaField(description="(Experimental) Dynamically-loaded solver")
-
+    name: str = DakotaField(
+        description="(Experimental) Dynamically-loaded solver",
+        dakota={
+            "materialization": [
+                {
+                    "ir_key": "method.dl_solver.dlDetails",
+                    "storage_type": "DIRECT_VALUE",
+                    "ir_value_type": "String",
+                }
+            ]
+        }
+    )
 
 class DlSolverSelection(MethodSelection):
     "Generated model for DlSolverSelection"
 
-    dl_solver: DlSolverConfig
+    dl_solver: DlSolverConfig = DakotaField(
+        dakota={
+            "argument": "name",
+            "materialization": [
+                {
+                    "ir_key": "method.algorithm",
+                    "storage_type": "PRESENCE_ENUM",
+                    "stored_value": "DL_SOLVER",
+                    "ir_value_type": "unsigned short",
+                }
+            ]
+        }
+    )
